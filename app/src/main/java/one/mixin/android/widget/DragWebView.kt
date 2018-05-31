6a0dd15f1a531bb9741a39e10adf926a4f056b78
zhaitao <370985116@qq.com>
2018-05-31 21:23:27 +0800

    Fix accident

--- app/src/main/java/one/mixin/android/widget/DragWebView.kt
+++ app/src/main/java/one/mixin/android/widget/DragWebView.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.MotionEvent
 import android.webkit.WebView
+import org.jetbrains.anko.dip
 
 class DragWebView : WebView {
     constructor(context: Context) : super(context)
@@ -14,36 +15,48 @@ class DragWebView : WebView {
 
     constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int, defStyleRes: Int) : super(context, attrs, defStyleAttr, defStyleRes)
 
+    private val dp1 = context.dip(1)
+
     private var tempY = 0f
-    private var processed = false
+    private var handled = false
+
     override fun onTouchEvent(event: MotionEvent): Boolean {
         return if (event.action == MotionEvent.ACTION_DOWN) {
-            processed = false
             tempY = event.rawY
+            handled = false
             super.onTouchEvent(event)
         } else if (event.action == MotionEvent.ACTION_MOVE) {
             val dis = event.rawY - tempY
             tempY = event.rawY
             if (dis > 0 && canScrollVertically(-dis.toInt())) {
-                processed = false
                 super.onTouchEvent(event)
             } else {
-                if (!onDragListener.onScroll(dis)) {
-                    processed = false
-                    super.onTouchEvent(event)
-                } else {
-                    true
+                when {
+                    dis == 0f -> {
+                        true
+                    }
+                    onDragListener.onScroll(dis) -> {
+                        if (!handled) {
+                            handled = (dis > dp1 || dis < -dp1)
+                        }
+                        true
+                    }
+                    else -> {
+                        super.onTouchEvent(event)
+                    }
                 }
             }
-        } else if (event.action == MotionEvent.ACTION_UP) {
+        } else if (event.action == MotionEvent.ACTION_UP || event.action == MotionEvent.ACTION_CANCEL) {
             onDragListener.onUp()
-            if (processed) {
-                true
+            if (handled) {
+                super.onTouchEvent(MotionEvent.obtain(event).apply {
+                    action = MotionEvent.ACTION_CANCEL
+                })
+                return true
             } else {
                 super.onTouchEvent(event)
             }
         } else {
-            onDragListener.onUp()
             super.onTouchEvent(event)
         }
     }
