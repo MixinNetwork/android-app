7992adec342904912b50e6f2591bed8b835ee9b6
Crossle Song <crosslesong@gmail.com>
2018-06-03 15:06:08 +0800

    Remove sign out

--- app/src/main/java/one/mixin/android/ui/setting/SettingFragment.kt
+++ app/src/main/java/one/mixin/android/ui/setting/SettingFragment.kt
@@ -1,27 +1,16 @@
 package one.mixin.android.ui.setting
 
-import android.app.Dialog
 import android.arch.lifecycle.ViewModelProvider
 import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.fragment_setting.*
 import kotlinx.android.synthetic.main.view_title.view.*
-import one.mixin.android.MixinApplication
 import one.mixin.android.R
-import one.mixin.android.api.MixinResponse
 import one.mixin.android.extension.addFragment
 import one.mixin.android.ui.common.BaseFragment
-import one.mixin.android.ui.landing.LandingActivity
-import one.mixin.android.util.ErrorHandler
-import org.jetbrains.anko.cancelButton
-import org.jetbrains.anko.support.v4.alert
-import org.jetbrains.anko.support.v4.indeterminateProgressDialog
-import org.jetbrains.anko.support.v4.onUiThread
-import org.jetbrains.anko.yesButton
 import javax.inject.Inject
 
 class SettingFragment : BaseFragment() {
@@ -56,48 +45,6 @@ class SettingFragment : BaseFragment() {
         about_rl.setOnClickListener {
             activity?.addFragment(this@SettingFragment,
                 AboutFragment.newInstance(), AboutFragment.TAG)
-        }
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        logout_rl.setOnClickListener {
-            alert(
-                getString(R.string.setting_logout_message),
-                getString(R.string.setting_logout_confirm)
-            ) {
-                yesButton { alert ->
-                    alert.dismiss()
-
-                    val dialog = indeterminateProgressDialog(message = getString(R.string.pb_dialog_message),
-                        title = getString(R.string.logout))
-                    dialog.setCancelable(false)
-                    dialog.show()
-
-                    settingViewModel.logout().subscribeOn(Schedulers.io()).subscribe({ r: MixinResponse<Unit> ->
-                        if (r.isSuccess) {
-                            MixinApplication.get().closeAndClear(false)
-                            dismissDialog(dialog)
-                            LandingActivity.show(ctx)
-                        } else {
-                            dismissDialog(dialog)
-                            ErrorHandler.handleMixinError(r.errorCode)
-                        }
-                    }, { t: Throwable ->
-                        dismissDialog(dialog)
-                        ErrorHandler.handleError(t)
-                    })
-                }
-                cancelButton { alert -> alert.dismiss() }
-            }.show()
-        }
-    }
-
-    private fun dismissDialog(dialog: Dialog?) {
-        if (dialog != null && dialog.isShowing) {
-            onUiThread {
-                dialog.dismiss()
-            }
         }
     }
 }
