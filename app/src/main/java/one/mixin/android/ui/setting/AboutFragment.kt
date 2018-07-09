f315baedbce4d826d98b2ab1a6152621ad5b1e31
wangxin <wangxin@shou.tv>
2018-07-09 14:01:50 +0800

    Decorate about page

--- app/src/main/java/one/mixin/android/ui/setting/AboutFragment.kt
+++ app/src/main/java/one/mixin/android/ui/setting/AboutFragment.kt
@@ -6,13 +6,15 @@ import android.view.View
 import android.view.ViewGroup
 import kotlinx.android.synthetic.main.fragment_about.*
 import kotlinx.android.synthetic.main.view_title.view.*
+import nl.dionsegijn.konfetti.models.Shape
+import nl.dionsegijn.konfetti.models.Size
 import one.mixin.android.R
 import one.mixin.android.extension.openUrl
 import one.mixin.android.ui.common.BaseFragment
 
 class AboutFragment : BaseFragment() {
     companion object {
-        val TAG = "AboutFragment"
+        const val TAG = "AboutFragment"
 
         fun newInstance() = AboutFragment()
     }
@@ -29,5 +31,32 @@ class AboutFragment : BaseFragment() {
         facebook.setOnClickListener { context?.openUrl("https://fb.com/MixinMessenger") }
         terms.setOnClickListener { context?.openUrl(getString(R.string.landing_terms_url)) }
         privacy.setOnClickListener { context?.openUrl(getString(R.string.landing_privacy_policy_url)) }
+        konfetti_view.post {
+            konfetti_view.build()
+                .addColors(resources.getColor(R.color.lt_yellow, null), resources.getColor(R.color.lt_orange, null),
+                    resources.getColor(R.color.lt_purple, null), resources.getColor(R.color.lt_pink, null))
+                .setDirection(0.0, 359.0)
+                .setSpeed(1f, 5f)
+                .setFadeOutEnabled(true)
+                .setTimeToLive(2000L)
+                .addShapes(Shape.RECT, Shape.CIRCLE)
+                .addSizes(Size(12))
+                .setPosition(-50f, konfetti_view.width + 50f, -50f, -50f)
+                .stream(300, 5000L)
+        }
+        konfetti_view.postDelayed({
+            konfetti_view.build()
+                .addColors(resources.getColor(R.color.lt_yellow, null), resources.getColor(R.color.lt_orange, null),
+                    resources.getColor(R.color.lt_pink, null), resources.getColor(R.color.dk_cyan, null),
+                    resources.getColor(R.color.dk_green, null))
+                .setDirection(0.0, 359.0)
+                .setSpeed(1f, 8f)
+                .setFadeOutEnabled(true)
+                .setTimeToLive(4000)
+                .addShapes(Shape.RECT, Shape.CIRCLE)
+                .addSizes(Size(12), Size(16, 6f))
+                .setPosition(konfetti_view.x + konfetti_view.width / 2, konfetti_view.y + konfetti_view.height / 3)
+                .burst(100)
+        }, 1000)
     }
 }
