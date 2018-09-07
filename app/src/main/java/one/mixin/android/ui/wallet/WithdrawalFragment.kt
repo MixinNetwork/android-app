0ebb61049a0469dff757527a58499c3a67e1d064
SeniorZhai <developer.zhaitao@gmail.com>
2018-09-07 12:52:55 +0800

    Error alert

--- app/src/main/java/one/mixin/android/ui/wallet/WithdrawalFragment.kt
+++ app/src/main/java/one/mixin/android/ui/wallet/WithdrawalFragment.kt
@@ -36,6 +36,7 @@ import one.mixin.android.vo.AssetItem
 import one.mixin.android.widget.BottomSheet
 import org.jetbrains.anko.support.v4.defaultSharedPreferences
 import org.jetbrains.anko.support.v4.dip
+import org.jetbrains.anko.support.v4.toast
 import org.jetbrains.anko.textColor
 import javax.inject.Inject
 
@@ -106,6 +107,14 @@ class WithdrawalFragment : BaseFragment() {
         title_view.right_animator.isEnabled = false
         title_view.right_animator.setOnClickListener {
             currAddr?.let {
+                it.dust?.let {
+                    if (it.toFloat() > 0f && it.toFloat() > amount_et.text.toString().toFloat()) {
+                        toast(getString(R.string.error_minimum_withdraw_amount, "${currAddr?.dust} ${asset.symbol}"))
+                        addrBottomSheet.dismiss()
+                        return@setOnClickListener
+                    }
+                }
+
                 val amount = amount_et.text.toString()
                 val withdrawalItem = WithdrawalBottomSheetDialogFragment.WithdrawalItem(if (noPublicKey(it)) it.accountTag!! else it.publicKey!!,
                     amount.toDot(), memo_et.text.toString(), it.addressId, if (noPublicKey(it)) it.accountName!! else it.label!!)
