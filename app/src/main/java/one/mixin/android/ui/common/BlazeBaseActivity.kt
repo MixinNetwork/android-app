618c4572f7c64c30ca0b2615cdf0710676034b8a
SeniorZhai <developer.zhaitao@gmail.com>
2018-11-19 15:10:50 +0800

    Fix reconnect

--- app/src/main/java/one/mixin/android/ui/common/BlazeBaseActivity.kt
+++ app/src/main/java/one/mixin/android/ui/common/BlazeBaseActivity.kt
@@ -1,5 +1,6 @@
 package one.mixin.android.ui.common
 
+import one.mixin.android.MixinApplication
 import one.mixin.android.job.BlazeMessageService.Companion.ACTION_ACTIVITY_PAUSE
 import one.mixin.android.job.BlazeMessageService.Companion.ACTION_ACTIVITY_RESUME
 import one.mixin.android.job.BlazeMessageService.Companion.startService
@@ -9,14 +10,14 @@ abstract class BlazeBaseActivity : BaseActivity() {
 
     override fun onResume() {
         super.onResume()
-        if (Session.checkToken()) {
+        if (Session.checkToken() && MixinApplication.get().onlining.get()) {
             startService(this, ACTION_ACTIVITY_RESUME)
         }
     }
 
     override fun onPause() {
         super.onPause()
-        if (Session.checkToken()) {
+        if (Session.checkToken() && MixinApplication.get().onlining.get()) {
             startService(this, ACTION_ACTIVITY_PAUSE)
         }
     }
