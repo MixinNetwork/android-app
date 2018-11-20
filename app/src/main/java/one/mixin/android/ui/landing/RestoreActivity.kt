98c4c196d545ccadaaca0bf0e30fede2f454ac3c
SeniorZhai <developer.zhaitao@gmail.com>
2018-11-20 18:29:21 +0800

    Restore

---
+++ app/src/main/java/one/mixin/android/ui/landing/RestoreActivity.kt
@@ -0,0 +1,131 @@
+package one.mixin.android.ui.landing
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.text.format.DateUtils
+import androidx.appcompat.app.AlertDialog
+import com.drive.demo.backup.Result
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.drive.Drive
+import com.google.android.gms.drive.DriveResourceClient
+import kotlinx.android.synthetic.main.activity_restore.*
+import one.mixin.android.Constants
+import one.mixin.android.R
+import one.mixin.android.extension.fileSize
+import one.mixin.android.extension.toast
+import one.mixin.android.ui.common.BaseActivity
+import one.mixin.android.util.Session
+import one.mixin.android.util.backup.DataBaseBackupManager
+import java.util.Date
+
+class RestoreActivity : BaseActivity() {
+
+    private lateinit var driveResourceClient: DriveResourceClient
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestScopes(Drive.SCOPE_FILE, Drive.SCOPE_APPFOLDER)
+            .requestEmail()
+            .build()
+        val googleSignInClient = GoogleSignIn.getClient(this, gso)
+        val account = GoogleSignIn.getAccountForScopes(this, Drive.SCOPE_FILE, Drive.SCOPE_APPFOLDER)
+        if (account.displayName == null) {
+            AlertDialog.Builder(this, R.style.MixinAlertDialogTheme)
+                .setMessage(R.string.restore_message)
+                .setNegativeButton(R.string.restore_skip) { dialog, _ ->
+                    InitializeActivity.showLoading(this)
+                    dialog.dismiss()
+                    finish()
+                }
+                .setPositiveButton(R.string.restore_authorization) { dialog, _ ->
+                    startActivityForResult(googleSignInClient.signInIntent, RC_SIGN_IN)
+                    dialog.dismiss()
+                }
+                .show()
+        } else {
+            initialize(account)
+        }
+    }
+
+    private fun initialize(account: GoogleSignInAccount) {
+        driveResourceClient = Drive.getDriveResourceClient(this, account)
+        val manager = DataBaseBackupManager.getManager(driveResourceClient, "mixin.db",
+            Session.getAccount()!!.identity_number, { this.getDatabasePath("mixin.db") }, Constants.DataBase.MINI_VERSION, Constants.DataBase.CURRENT_VERSION)
+
+        manager.findBackup { result, metadata ->
+            when (result) {
+                Result.SUCCESS -> {
+                    metadata?.let { data ->
+                        setContentView(R.layout.activity_restore)
+                        restore_time.text = data.createdDate.run {
+                            val now = Date().time
+                            val createTime = data.createdDate.time
+                            DateUtils.getRelativeTimeSpanString(createTime, now, when {
+                                ((now - createTime) < 60000L) -> DateUtils.SECOND_IN_MILLIS
+                                ((now - createTime) < 3600000L) -> DateUtils.MINUTE_IN_MILLIS
+                                ((now - createTime) < 86400000L) -> DateUtils.HOUR_IN_MILLIS
+                                else -> DateUtils.DAY_IN_MILLIS
+                            })
+                        }
+                        restore_restore.setOnClickListener {
+                            manager.restoreDatabase {result
+                                if (result == Result.SUCCESS) {
+                                    InitializeActivity.showLoading(this)
+                                    finish()
+                                    toast("success")
+                                } else {
+                                    toast("failure")
+                                }
+                            }
+                        }
+                        restore_size.text = getString(R.string.restore_size, data.fileSize.fileSize())
+                        restore_name.text = getString(R.string.restore_account, account.email)
+                        restore_alert.text = getString(R.string.restore_alert, data.fileSize.fileSize())
+                    }
+                }
+                Result.FAILURE -> {
+                }
+                Result.NOT_FOUND -> {
+                }
+                Result.NOT_SUPPORT -> {
+                }
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RC_SIGN_IN) {
+            try {
+                val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+                val account = task.getResult(ApiException::class.java)
+                if (account == null) {
+                    InitializeActivity.showLoading(this)
+                    finish()
+                } else {
+                    initialize(account)
+                }
+            } catch (e: ApiException) {
+                InitializeActivity.showLoading(this)
+                finish()
+            }
+        }
+    }
+
+    // Todo test close
+    // override fun onBackPressed() {
+    // }
+
+    companion object {
+        fun show(context: Context) {
+            context.startActivity(Intent(context, RestoreActivity::class.java))
+        }
+
+        private const val RC_SIGN_IN = 9001
+    }
+}
--- app/src/main/java/one/mixin/android/util/backup/DataBaseBackupManager.kt
+++ app/src/main/java/one/mixin/android/util/backup/DataBaseBackupManager.kt
@@ -58,55 +58,82 @@ class DataBaseBackupManager private constructor(driveResourceClient: DriveResour
     fun backup(callback: (Result) -> Unit) {
         GlobalScope.launch {
             createDirectory()
-            deleteAllDb(dbName)
             val dbFile = getDbFile() ?: return@launch callback(NOT_FOUND)
             val metadata = isFolderExists(folderName)
             if (metadata != null) {
                 uploadDatabase(this.coroutineContext, metadata.driveId, dbFile, currentVersion, callback)
             } else {
-                callback(FAILURE)
+                withContext(Dispatchers.Main) {
+                    callback(FAILURE)
+                }
+            }
+        }
+    }
+
+    fun findBackup(callback: (Result, Metadata?) -> Unit) {
+        GlobalScope.launch {
+            val parentMetadata = isFolderExists(folderName)
+            if (parentMetadata == null) {
+                withContext(Dispatchers.Main) {
+                    callback(NOT_FOUND, null)
+                }
+            }
+            val metaData = findDbFiles(parentMetadata!!.driveId.asDriveFolder(), dbName) { title ->
+                if (currentVersion != null && minVersion != null) {
+                    val version = title.split("_")[1].toInt()
+                    version <= currentVersion && version >= minVersion
+                } else {
+                    title.contentEquals(dbName)
+                }
+            }?.run {
+                if (!this.isEmpty()) {
+                    this[0]
+                } else {
+                    null
+                }
+            }
+            if (metaData != null) {
+                withContext(Dispatchers.Main) {
+                    callback(SUCCESS, metaData)
+                }
+            } else {
+                withContext(Dispatchers.Main) {
+                    callback(NOT_FOUND, null)
+                }
             }
         }
     }
 
     fun restoreDatabase(callback: (Result) -> Unit) {
-        val parentMetadata = isFolderExists(folderName)
-        if (parentMetadata == null) {
-            callback(NOT_FOUND)
-        }
-        val metaData = findDbFiles(parentMetadata!!.driveId.asDriveFolder(), dbName) { title ->
-            if (currentVersion != null && minVersion != null) {
-                val version = title.split("_")[1].toInt()
-                version >= currentVersion && version <= minVersion
-            } else {
-                title.contentEquals(dbName)
+        GlobalScope.launch {
+            val parentMetadata = isFolderExists(folderName)
+            if (parentMetadata == null) {
+                withContext(Dispatchers.Main) {
+                    callback(NOT_FOUND)
+                }
             }
-        }?.run {
-            if (!this.isEmpty()) {
-                this[0]
-            } else {
-                null
+            val metaData = findDbFiles(parentMetadata!!.driveId.asDriveFolder(), dbName) { title ->
+                if (currentVersion != null && minVersion != null) {
+                    val version = title.split("_")[1].toInt()
+                    version <= currentVersion && version >= minVersion
+                } else {
+                    title.contentEquals(dbName)
+                }
+            }?.run {
+                if (!this.isEmpty()) {
+                    this[0]
+                } else {
+                    null
+                }
             }
-        }
-        if (metaData != null) {
-            val file = getDbFile() ?: return callback(NOT_FOUND)
-            file.deleteOnExit()
-            restore(metaData.driveId.asDriveFile(), file.toString(), callback)
-        } else {
-            callback(NOT_FOUND)
-        }
-    }
-
-    private fun deleteAllDb(dbName: String) {
-        val query = Query.Builder()
-            .addFilter(Filters.contains(SearchableField.TITLE, dbName))
-            .build()
-        val queryTask = resourceClient.query(query)
-        val metadataBuffer = Tasks.await(queryTask)
-        for (b in metadataBuffer) {
-            if (b.title == dbName) {
-                val deleteTask = deleteExistingFile(b.driveId.asDriveFile())
-                Tasks.await(deleteTask)
+            if (metaData != null) {
+                val file = getDbFile() ?: return@launch withContext(Dispatchers.Main) { callback(NOT_FOUND) }
+                file.deleteOnExit()
+                restore(metaData.driveId.asDriveFile(), file.toString(), callback)
+            } else {
+                withContext(Dispatchers.Main) {
+                    callback(NOT_FOUND)
+                }
             }
         }
     }
@@ -115,7 +142,7 @@ class DataBaseBackupManager private constructor(driveResourceClient: DriveResour
         attachCheck: ((String) -> Boolean)? = null): List<Metadata>? {
         val result = mutableListOf<Metadata>()
         val query = Query.Builder()
-            .addFilter(Filters.contains(SearchableField.TITLE, folderName))
+            .addFilter(Filters.contains(SearchableField.TITLE, dbName))
             .build()
         val queryTask = resourceClient.queryChildren(driveFile, query)
         try {
@@ -136,15 +163,36 @@ class DataBaseBackupManager private constructor(driveResourceClient: DriveResour
         return result
     }
 
-    private suspend fun uploadDatabase(context: CoroutineContext, driveId: DriveId, file: File, version: Int? = null,
+    private fun clearOtherDb(title: String) {
+        val query = Query.Builder()
+            .addFilter(Filters.contains(SearchableField.TITLE, "db"))
+            .build()
+        val queryTask = resourceClient.query(query)
+        val metadataBuffer = Tasks.await(queryTask)
+        for (b in metadataBuffer) {
+            if (b.title != title) {
+                val deleteTask = deleteExistingFile(b.driveId.asDriveFile())
+                Tasks.await(deleteTask)
+            }
+        }
+    }
+
+    private suspend fun uploadDatabase(context: CoroutineContext, driveId: DriveId, file: File,
+        version: Int? = null,
         callback: (Result) -> Unit): DriveFile? {
         val d = GlobalScope.async(context) {
-            val uploadTask = uploadBackup(driveId, file, version)
+            val title = if (version != null) {
+                "${file.name}_$version"
+            } else {
+                file.name
+            }
+            val uploadTask = uploadBackup(driveId, file, title)
             val driveFile = Tasks.await(uploadTask)
             if (driveFile != null && uploadTask.isSuccessful) {
                 withContext(Dispatchers.Main) {
                     callback(SUCCESS)
                 }
+                clearOtherDb(title)
             } else {
                 withContext(Dispatchers.Main) {
                     callback(FAILURE)
