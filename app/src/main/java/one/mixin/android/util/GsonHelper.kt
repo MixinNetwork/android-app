586072b4ee7b209f0a6e826cf19d8d16cda7d893
Crossle Song <crosslesong@gmail.com>
2018-12-05 13:15:40 +0800

    Turn GsonHelper

--- app/src/main/java/one/mixin/android/util/GsonHelper.kt
+++ app/src/main/java/one/mixin/android/util/GsonHelper.kt
@@ -2,8 +2,6 @@ package one.mixin.android.util
 
 import java.lang.reflect.Type
 
-import android.util.Base64
-
 import com.google.gson.GsonBuilder
 import com.google.gson.JsonDeserializationContext
 import com.google.gson.JsonDeserializer
@@ -11,17 +9,18 @@ import com.google.gson.JsonElement
 import com.google.gson.JsonPrimitive
 import com.google.gson.JsonSerializationContext
 import com.google.gson.JsonSerializer
+import one.mixin.android.crypto.Base64
 
 object GsonHelper {
     val customGson = GsonBuilder().registerTypeHierarchyAdapter(ByteArray::class.java, ByteArrayToBase64TypeAdapter()).create()
 
     private class ByteArrayToBase64TypeAdapter : JsonSerializer<ByteArray>, JsonDeserializer<ByteArray> {
         override fun deserialize(json: JsonElement, typeOfT: Type, context: JsonDeserializationContext): ByteArray {
-            return Base64.decode(json.asString, Base64.NO_WRAP)
+            return Base64.decode(json.asString)
         }
 
         override fun serialize(src: ByteArray, typeOfSrc: Type, context: JsonSerializationContext): JsonElement {
-            return JsonPrimitive(Base64.encodeToString(src, Base64.NO_WRAP))
+            return JsonPrimitive(Base64.encodeBytes(src))
         }
     }
 }
