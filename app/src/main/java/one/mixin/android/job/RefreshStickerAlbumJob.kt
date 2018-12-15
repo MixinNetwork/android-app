5dbb28707edabb94dc521a2d5a0ca6fffb59756f
SeniorZhai <developer.zhaitao@gmail.com>
2018-12-15 18:51:15 +0800

    Revert refresh sticker album

---
+++ app/src/main/java/one/mixin/android/job/RefreshStickerAlbumJob.kt
@@ -0,0 +1,42 @@
+package one.mixin.android.job
+
+import com.birbit.android.jobqueue.Params
+import one.mixin.android.db.insertUpdate
+import one.mixin.android.extension.defaultSharedPreferences
+import one.mixin.android.extension.putBoolean
+import one.mixin.android.vo.Sticker
+import one.mixin.android.vo.StickerAlbum
+import one.mixin.android.vo.StickerRelationship
+
+class RefreshStickerAlbumJob : BaseJob(Params(PRIORITY_UI_HIGH)
+    .addTags(RefreshStickerAlbumJob.GROUP).requireNetwork()) {
+
+    companion object {
+        private const val serialVersionUID = 1L
+        const val GROUP = "RefreshStickerAlbumJob"
+    }
+
+    override fun onRun() {
+        val response = accountService.getStickerAlbums().execute().body()
+        if (response != null && response.isSuccess && response.data != null) {
+            val albums = response.data as List<StickerAlbum>
+            for (a in albums) {
+                stickerAlbumDao.insert(a)
+
+                val r = accountService.getStickersByAlbumId(a.albumId).execute().body()
+                if (r != null && r.isSuccess && r.data != null) {
+                    val stickers = r.data as List<Sticker>
+                    for (s in stickers) {
+                        stickerDao.insertUpdate(s)
+                        stickerRelationshipDao.insert(StickerRelationship(a.albumId, s.stickerId))
+                    }
+                }
+            }
+            val sp = applicationContext.defaultSharedPreferences
+            if (!sp.getBoolean("UpgradeMessageSticker", false)) {
+                stickerRelationshipDao.updateMessageStickerId()
+                sp.putBoolean("UpgradeMessageSticker", true)
+            }
+        }
+    }
+}
