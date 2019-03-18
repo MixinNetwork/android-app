149d7e151fdf1d407def48323abfc5498d3fa399
Crossle Song <crosslesong@gmail.com>
2019-03-18 12:37:30 +0800

    Add package_name for some apis

--- app/src/main/java/one/mixin/android/api/request/AccountRequest.kt
+++ app/src/main/java/one/mixin/android/api/request/AccountRequest.kt
@@ -11,6 +11,7 @@ data class AccountRequest(
     val platform: String = "Android",
     val platform_version: String = Build.VERSION.RELEASE,
     val app_version: String = BuildConfig.VERSION_NAME,
+    val package_name: String = BuildConfig.APPLICATION_ID,
     var purpose: String = VerificationPurpose.SESSION.name,
     val pin: String? = null,
     val session_secret: String? = null
--- app/src/main/java/one/mixin/android/api/request/VerificationRequest.kt
+++ app/src/main/java/one/mixin/android/api/request/VerificationRequest.kt
@@ -1,13 +1,15 @@
 package one.mixin.android.api.request
 
 import com.google.gson.annotations.SerializedName
+import one.mixin.android.BuildConfig
 
 data class VerificationRequest(
     val phone: String?,
     val invitation: String?,
     val purpose: String,
     @SerializedName("g_recaptcha_response")
-    val gRecaptchaResponse: String?
+    val gRecaptchaResponse: String?,
+    val package_name: String = BuildConfig.APPLICATION_ID
 )
 
 enum class VerificationPurpose {
