90f76d85cde3c8e88300ebab9ff48da2c8db2db8
Crossle Song <crosslesong@gmail.com>
2019-05-06 21:16:32 +0800

    Revert "Notification title color"
    This reverts commit 6890ee8a92d2dbb6a68c2865350020be4ba5c165.

--- app/src/main/java/one/mixin/android/job/BlazeMessageService.kt
+++ app/src/main/java/one/mixin/android/job/BlazeMessageService.kt
@@ -157,7 +157,7 @@ class BlazeMessageService : Service(), NetworkEventProvider.Listener, ChatWebSoc
             .setSound(null)
             .setDefaults(0)
             .setOnlyAlertOnce(true)
-            .setColor(ContextCompat.getColor(this, R.color.colorDarkBlue))
+            .setColor(ContextCompat.getColor(this, R.color.gray_light))
             .setSmallIcon(R.drawable.ic_msg_default)
             .addAction(R.drawable.ic_close_black_24dp, getString(R.string.exit), exitPendingIntent)
 
