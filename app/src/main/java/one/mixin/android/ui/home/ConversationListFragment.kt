cceb200ef0eb9f8da07ffdd4cceb7275586757d2
SeniorZhai <developer.zhaitao@gmail.com>
2019-05-17 21:02:41 +0800

    Change size

--- app/src/main/java/one/mixin/android/ui/home/ConversationListFragment.kt
+++ app/src/main/java/one/mixin/android/ui/home/ConversationListFragment.kt
@@ -493,7 +493,7 @@ class ConversationListFragment : LinkFragment() {
                         AppCompatResources.getDrawable(itemView.context, R.drawable.ic_status_sending)
                     }
                 }.also {
-                    it?.setBounds(0, 0, itemView.context.dpToPx(12f), itemView.context.dpToPx(12f))
+                    it?.setBounds(0, 0, itemView.context.dpToPx(14f), itemView.context.dpToPx(14f))
                     itemView.msg_flag.setImageDrawable(it)
                     itemView.msg_flag.visibility = VISIBLE
                 }
