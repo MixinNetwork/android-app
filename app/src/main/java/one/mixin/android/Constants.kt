42679c03d9107134838dc9e78f3e4e332483c803
SeniorZhai <developer.zhaitao@gmail.com>
2019-05-29 14:25:43 +0800

    Increase page size

--- app/src/main/java/one/mixin/android/Constants.kt
+++ app/src/main/java/one/mixin/android/Constants.kt
@@ -77,7 +77,7 @@ object Constants {
 
     const val BATCH_SIZE = 700
 
-    const val PAGE_SIZE = 20
+    const val PAGE_SIZE = 80
 
     const val BIOMETRICS_ALIAS = "biometrics_alias"
     const val BIOMETRICS_PIN = "biometrics_pin"
