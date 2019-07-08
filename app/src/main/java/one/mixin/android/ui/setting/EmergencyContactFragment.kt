e569d86500292ea19e45e76fc1172e922ada815b
wangxin <wangxin@shou.tv>
2019-07-08 15:21:43 +0800

    Remove useless tip

--- app/src/main/java/one/mixin/android/ui/setting/EmergencyContactFragment.kt
+++ app/src/main/java/one/mixin/android/ui/setting/EmergencyContactFragment.kt
@@ -37,7 +37,7 @@ class EmergencyContactFragment : BaseViewModelFragment<EmergencyViewModel>() {
         title_view.left_ib.setOnClickListener {
             activity?.onBackPressed()
         }
-        title_view.right_animator.setOnClickListener { context?.openUrl("") }
+        title_view.right_animator.setOnClickListener { context?.openUrl("https://mixinmessenger.zendesk.com/hc/articles/360029154692") }
         enable_rl.setOnClickListener {
             EmergencyContactTipBottomSheetDialogFragment.newInstance()
                 .showNow(requireFragmentManager(), EmergencyContactTipBottomSheetDialogFragment.TAG)
