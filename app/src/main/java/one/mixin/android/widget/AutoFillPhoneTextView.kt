997104ca1cd668dbe453cf530095fa0e30fcef30
wangxin <wangxin@shou.tv>
2019-07-09 12:05:35 +0800

    Use auto fill hint instead of Google account hint

---
+++ app/src/main/java/one/mixin/android/widget/AutoFillPhoneTextView.kt
@@ -0,0 +1,28 @@
+package one.mixin.android.widget
+
+import android.annotation.TargetApi
+import android.content.Context
+import android.os.Build
+import android.util.AttributeSet
+import android.view.autofill.AutofillValue
+import android.widget.EditText
+import com.google.i18n.phonenumbers.NumberParseException
+import com.google.i18n.phonenumbers.PhoneNumberUtil
+import java.util.Locale
+
+class AutoFillPhoneText(
+    context: Context,
+    attributeSet: AttributeSet
+): EditText(context, attributeSet) {
+
+    @TargetApi(Build.VERSION_CODES.O)
+    override fun autofill(value: AutofillValue) {
+        val autoFilledText = value.textValue
+        val phoneNum = try {
+            PhoneNumberUtil.getInstance().parse(autoFilledText, Locale.getDefault().country).nationalNumber.toString()
+        } catch (e: NumberParseException) {
+            ""
+        }
+        super.autofill(AutofillValue.forText(phoneNum))
+    }
+}
