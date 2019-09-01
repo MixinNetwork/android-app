681b164ac6fd2b5dbaf972491ac3fbd9990d97ef
wangxin <touge1027@gmail.com>
2019-09-01 13:06:55 +0800

    Fix fiats set concurrent modification bug

--- app/src/main/java/one/mixin/android/vo/Fiat.kt
+++ app/src/main/java/one/mixin/android/vo/Fiat.kt
@@ -1,6 +1,9 @@
 package one.mixin.android.vo
 
+import android.os.Build
 import com.google.gson.Gson
+import java.util.Collections
+import java.util.concurrent.ConcurrentHashMap
 import one.mixin.android.Constants.Account.PREF_CURRENCY
 import one.mixin.android.Constants.Account.PREF_CURRENCY_SYMBOL
 import one.mixin.android.Constants.Account.PREF_FIAT_SET
@@ -14,7 +17,11 @@ data class Fiat(val code: String, val rate: Double)
 object Fiats {
     private val gson = Gson()
 
-    private val fiatSet = mutableSetOf<Fiat>()
+    private val fiatSet = if (Build.VERSION.SDK_INT > Build.VERSION_CODES.M) {
+        ConcurrentHashMap.newKeySet<Fiat>()
+    } else {
+        Collections.newSetFromMap(ConcurrentHashMap<Fiat, Boolean>())
+    }
 
     var currency: String = MixinApplication.appContext.defaultSharedPreferences.getString(PREF_CURRENCY, "USD") ?: "USD"
         set(value) {
