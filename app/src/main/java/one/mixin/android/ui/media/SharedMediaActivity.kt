b4a8b7fe405b3abd2da207ba3e21bd0536e81c30
wangxin <wangxin@shou.tv>
2019-10-11 16:47:01 +0800

    Fix share element animation position offset

--- app/src/main/java/one/mixin/android/ui/media/SharedMediaActivity.kt
+++ app/src/main/java/one/mixin/android/ui/media/SharedMediaActivity.kt
@@ -3,6 +3,7 @@ package one.mixin.android.ui.media
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import one.mixin.android.Constants.ARGS_CONVERSATION_ID
 import one.mixin.android.R
 import one.mixin.android.extension.replaceFragment
@@ -23,5 +24,6 @@ class SharedMediaActivity : BaseActivity() {
         setContentView(R.layout.activity_contact)
         val conversationId = intent.getStringExtra(ARGS_CONVERSATION_ID)
         replaceFragment(SharedMediaFragment.newInstance(conversationId), R.id.container, SharedMediaFragment.TAG)
+        window.decorView.systemUiVisibility = window.decorView.systemUiVisibility or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
     }
 }
