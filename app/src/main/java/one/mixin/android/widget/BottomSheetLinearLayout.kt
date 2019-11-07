02b9478f646a2293aff4f010cef80a217f24dcf1
wangxin <wangxin@shou.tv>
2019-11-07 20:14:15 +0800

    Fix currency bottom bug

---
+++ app/src/main/java/one/mixin/android/widget/BottomSheetLinearLayout.kt
@@ -0,0 +1,19 @@
+package one.mixin.android.widget
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.LinearLayout
+import one.mixin.android.extension.statusBarHeight
+
+class BottomSheetLinearLayout(context: Context, attributeSet: AttributeSet) : LinearLayout(context, attributeSet) {
+
+    private val statusBarHeight = context.statusBarHeight()
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        val parentHeightSpec = MeasureSpec.makeMeasureSpec(
+            MeasureSpec.getSize(heightMeasureSpec) - statusBarHeight,
+            MeasureSpec.getMode(heightMeasureSpec)
+        )
+        super.onMeasure(widthMeasureSpec, parentHeightSpec)
+    }
+}
