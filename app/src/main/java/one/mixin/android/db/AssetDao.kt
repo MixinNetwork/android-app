46a2ab4e78deeeecf01f49a332e732b04140fae2
SeniorZhai <developer.zhaitao@gmail.com>
2019-12-18 16:20:57 +0800

    Code format

--- app/src/main/java/one/mixin/android/db/AssetDao.kt
+++ app/src/main/java/one/mixin/android/db/AssetDao.kt
@@ -76,7 +76,7 @@ interface AssetDao : BaseDao<Asset> {
     suspend fun findAssetItemById(assetId: String): AssetItem?
 
     @Query("SELECT asset_id FROM assets WHERE balance > 0")
-    suspend fun findAllAssetIdSuspend():List<String>
+    suspend fun findAllAssetIdSuspend(): List<String>
 
     @Query("UPDATE assets SET balance = 0 WHERE asset_id IN (:assetIds)")
     suspend fun zeroClearSuspend(assetIds: List<String>)
