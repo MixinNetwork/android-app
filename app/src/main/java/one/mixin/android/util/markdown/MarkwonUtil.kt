1b9efa2267adffa8a79d59be4d373c1037d27c40
Crossle Song <crosslesong@gmail.com>
2020-02-13 19:05:59 +0800

    Fix markdown image load

--- app/src/main/java/one/mixin/android/util/markdown/MarkwonUtil.kt
+++ app/src/main/java/one/mixin/android/util/markdown/MarkwonUtil.kt
@@ -1,10 +1,8 @@
 package one.mixin.android.util.markdown
 
 import android.content.Context
-import android.graphics.drawable.Drawable
 import android.view.View
 import com.bumptech.glide.Glide
-import com.bumptech.glide.RequestBuilder
 import io.noties.markwon.AbstractMarkwonPlugin
 import io.noties.markwon.LinkResolverDef
 import io.noties.markwon.Markwon
@@ -15,8 +13,7 @@ import io.noties.markwon.core.MarkwonTheme
 import io.noties.markwon.ext.strikethrough.StrikethroughPlugin
 import io.noties.markwon.ext.tables.TablePlugin
 import io.noties.markwon.ext.tables.TableTheme
-import io.noties.markwon.image.AsyncDrawable
-import io.noties.markwon.image.glide.GlideImagesPlugin
+import io.noties.markwon.image.ImagesPlugin
 import io.noties.markwon.recycler.table.TableEntryPlugin
 import io.noties.markwon.syntax.Prism4jThemeDarkula
 import io.noties.markwon.syntax.Prism4jThemeDefault
@@ -46,17 +43,7 @@ class MarkwonUtil {
                 .usePlugin(StrikethroughPlugin.create())
                 .usePlugin(SyntaxHighlightPlugin.create(prism4j, prism4jTheme))
                 .usePlugin(TableEntryPlugin.create(context))
-                .usePlugin(GlideImagesPlugin.create(context))
-                .usePlugin(GlideImagesPlugin.create(Glide.with(context)))
-                .usePlugin(GlideImagesPlugin.create(object : GlideImagesPlugin.GlideStore {
-                    override fun cancel(target: com.bumptech.glide.request.target.Target<*>) {
-                        requestManager.clear(target)
-                    }
-
-                    override fun load(drawable: AsyncDrawable): RequestBuilder<Drawable> {
-                        return requestManager.load(drawable.destination)
-                    }
-                }))
+                .usePlugin(ImagesPlugin.create())
                 .usePlugin(object : AbstractMarkwonPlugin() {
                     override fun configureTheme(builder: MarkwonTheme.Builder) {
                         builder.headingBreakHeight(0)
@@ -104,17 +91,7 @@ class MarkwonUtil {
                 .usePlugin(StrikethroughPlugin.create())
                 .usePlugin(SyntaxHighlightPlugin.create(prism4j, prism4jTheme))
                 .usePlugin(TablePlugin.create(getTheme()))
-                .usePlugin(GlideImagesPlugin.create(context))
-                .usePlugin(GlideImagesPlugin.create(Glide.with(context)))
-                .usePlugin(GlideImagesPlugin.create(object : GlideImagesPlugin.GlideStore {
-                    override fun cancel(target: com.bumptech.glide.request.target.Target<*>) {
-                        requestManager.clear(target)
-                    }
-
-                    override fun load(drawable: AsyncDrawable): RequestBuilder<Drawable> {
-                        return requestManager.load(drawable.destination)
-                    }
-                }))
+                .usePlugin(ImagesPlugin.create())
                 .usePlugin(object : AbstractMarkwonPlugin() {
                     override fun configureTheme(builder: MarkwonTheme.Builder) {
                         builder.headingBreakHeight(0)
