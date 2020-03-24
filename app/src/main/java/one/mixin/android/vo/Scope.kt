9bfe98dbcd5d28eeb62889c702d98455d32059f0
Crossle Song <crosslesong@gmail.com>
2020-03-24 17:07:03 +0800

    Revert "Add read favorite apps scope"
    This reverts commit a9ae01eb19ad6adedd670d8470ff8dcbbc11092f.

--- app/src/main/java/one/mixin/android/vo/Scope.kt
+++ app/src/main/java/one/mixin/android/vo/Scope.kt
@@ -17,17 +17,8 @@ data class Scope(val name: String, val desc: String) : Parcelable {
             override fun areContentsTheSame(oldItem: Scope, newItem: Scope) = oldItem == newItem
         }
 
-        val SCOPES = arrayListOf(
-            "PROFILE:READ",
-            "PHONE:READ",
-            "MESSAGES:REPRESENT",
-            "CONTACTS:READ",
-            "ASSETS:READ",
-            "SNAPSHOTS:READ",
-            "APPS:READ",
-            "APPS:WRITE",
-            "APPS:FAVORITE:READ"
-        )
+        val SCOPES = arrayListOf("PROFILE:READ", "PHONE:READ", "MESSAGES:REPRESENT",
+            "CONTACTS:READ", "ASSETS:READ", "SNAPSHOTS:READ", "APPS:READ", "APPS:WRITE")
     }
 }
 
@@ -41,7 +32,6 @@ fun Scope.convertName(ctx: Context): String {
         Scope.SCOPES[5] -> R.string.auth_snapshot_read
         Scope.SCOPES[6] -> R.string.auth_app_read
         Scope.SCOPES[7] -> R.string.auth_apps_write
-        Scope.SCOPES[8] -> R.string.auth_apps_favorite_read
         else -> R.string.auth_public_profile
     }
     return ctx.getString(id)
