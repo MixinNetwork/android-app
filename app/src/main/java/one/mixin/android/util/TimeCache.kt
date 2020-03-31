cf19d31b9e70704c72f5fbd150515e0bbb4e85eb
wangxin <touge1027@gmail.com>
2020-03-31 15:53:39 +0800

    New time format in chat

--- app/src/main/java/one/mixin/android/util/TimeCache.kt
+++ app/src/main/java/one/mixin/android/util/TimeCache.kt
@@ -1,13 +1,15 @@
 package one.mixin.android.util
 
 import androidx.collection.LruCache
+import one.mixin.android.util.language.Lingver
 
 class TimeCache private constructor(maxSize: Int) : LruCache<String, Any>(maxSize) {
 
     companion object {
         private const val TYPE_DATE = "date"
         private const val TYPE_TIME_AGO = "time_ago"
-        private const val TYPE_TIME_AGO_DATE = "time_ago_date"
+        private const val TYPE_TIME_AGO_DATE_EN = "time_ago_date_en"
+        private const val TYPE_TIME_AGO_DATE_CN = "time_ago_date_cn"
         private const val TYPE_HASH = "hash"
         private const val TYPE_CLOCK = "clock"
 
@@ -31,11 +33,19 @@ class TimeCache private constructor(maxSize: Int) : LruCache<String, Any>(maxSiz
     }
 
     fun getTimeAgoDate(key: String): Any? {
-        return get(key + TYPE_TIME_AGO_DATE)
+        return if (Lingver.getInstance().isCurrChinese()) {
+            get(key + TYPE_TIME_AGO_DATE_CN)
+        } else {
+            get(key + TYPE_TIME_AGO_DATE_EN)
+        }
     }
 
     fun putTimeAgoDate(key: String, value: String): Any? {
-        return put(key + TYPE_TIME_AGO_DATE, value)
+        return if (Lingver.getInstance().isCurrChinese()) {
+            put(key + TYPE_TIME_AGO_DATE_CN, value)
+        } else {
+            put(key + TYPE_TIME_AGO_DATE_EN, value)
+        }
     }
 
     fun getHashForDate(key: String): Any? {
