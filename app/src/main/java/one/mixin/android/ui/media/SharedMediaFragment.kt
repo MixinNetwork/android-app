43799d87ee32ff72c80ffd5ace257a7fc3567d9e
wangxin <touge1027@gmail.com>
2020-04-08 23:13:35 +0800

    Share media support post

--- app/src/main/java/one/mixin/android/ui/media/SharedMediaFragment.kt
+++ app/src/main/java/one/mixin/android/ui/media/SharedMediaFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 import kotlinx.android.synthetic.main.fragment_shared_media.*
 import kotlinx.android.synthetic.main.view_title.view.*
 import one.mixin.android.Constants.ARGS_CONVERSATION_ID
@@ -26,7 +27,7 @@ class SharedMediaFragment : BaseViewModelFragment<SharedMediaViewModel>() {
     }
 
     private val adapter: SharedMediaAdapter by lazy {
-        SharedMediaAdapter(parentFragmentManager, requireContext(), conversationId)
+        SharedMediaAdapter(requireActivity(), conversationId)
     }
 
     override fun getModelClass() = SharedMediaViewModel::class.java
@@ -41,7 +42,16 @@ class SharedMediaFragment : BaseViewModelFragment<SharedMediaViewModel>() {
         super.onViewCreated(view, savedInstanceState)
         title_view.left_ib.setOnClickListener { activity?.onBackPressed() }
         view_pager.adapter = adapter
-        shared_tl.setupWithViewPager(view_pager)
+        TabLayoutMediator(shared_tl, view_pager, TabLayoutMediator.TabConfigurationStrategy { tab, position ->
+            tab.text = getString(when (position) {
+                0 -> R.string.media
+                1 -> R.string.audio
+                2 -> R.string.post
+                3 -> R.string.links
+                else -> R.string.files
+            })
+            view_pager.setCurrentItem(tab.position, true)
+        }).attach()
         shared_tl.tabMode = TabLayout.MODE_FIXED
         view_pager.currentItem = 0
     }
