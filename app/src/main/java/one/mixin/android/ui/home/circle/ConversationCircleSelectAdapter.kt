c9af486e894709f8feddf612d53d684e5f3d9503
Crossle Song <crosslesong@gmail.com>
2020-04-13 11:16:44 +0800

    Fix circle selection item

--- app/src/main/java/one/mixin/android/ui/home/circle/ConversationCircleSelectAdapter.kt
+++ app/src/main/java/one/mixin/android/ui/home/circle/ConversationCircleSelectAdapter.kt
@@ -3,14 +3,12 @@ package one.mixin.android.ui.home.circle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.item_group_select.view.*
 import one.mixin.android.R
 import one.mixin.android.extension.notNullWithElse
 import one.mixin.android.vo.ConversationItem
 import one.mixin.android.vo.User
-import one.mixin.android.vo.showVerifiedOrBot
 
 class ConversationCircleSelectAdapter(
     val removeItem: (Any) -> Unit
@@ -34,7 +32,6 @@ class ConversationCircleSelectAdapter(
         if (item is User) {
             holder.itemView.avatar_view.setInfo(item.fullName, item.avatarUrl, item.userId)
             holder.itemView.name_tv.text = item.fullName
-            item.showVerifiedOrBot(holder.itemView.verified_iv, holder.itemView.bot_iv)
         } else if (item is ConversationItem) {
             holder.itemView.apply {
                 if (item.isGroup()) {
@@ -44,8 +41,6 @@ class ConversationCircleSelectAdapter(
                     avatar_view.setInfo(item.getConversationName(), item.iconUrl(), item.ownerId)
                     name_tv.text = item.name
                 }
-                bot_iv.isVisible = false
-                verified_iv.isVisible = false
             }
         }
         holder.itemView.setOnClickListener {
