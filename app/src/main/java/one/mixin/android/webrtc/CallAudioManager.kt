4d36b78a3f15abe7a1c62c7a5b8e94f4ce99530c
Crossle Song <crosslesong@gmail.com>
2020-04-15 10:45:58 +0800

    Revert "Support switch headset when making a audio call"
    This reverts commit 5e82f87b9a5d734b50b5a8b98f695c5df7510176.

--- app/src/main/java/one/mixin/android/webrtc/CallAudioManager.kt
+++ app/src/main/java/one/mixin/android/webrtc/CallAudioManager.kt
@@ -1,9 +1,6 @@
 package one.mixin.android.webrtc
 
-import android.content.BroadcastReceiver
 import android.content.Context
-import android.content.Intent
-import android.content.IntentFilter
 import android.media.AudioAttributes
 import android.media.AudioManager
 import android.media.MediaPlayer
@@ -13,24 +10,18 @@ import android.os.VibrationEffect
 import android.os.Vibrator
 import androidx.core.content.getSystemService
 import one.mixin.android.R
-import one.mixin.android.extension.isHeadsetOn
 import timber.log.Timber
 
 class CallAudioManager(private val context: Context) {
     private val savedSpeakerOn: Boolean
     private val saveMode: Int
     private val savedMicrophoneMute: Boolean
-
-    private var isHeadsetOn = false
-
     private val audioManager: AudioManager = context.getSystemService<AudioManager>()!!.apply {
         savedSpeakerOn = isSpeakerphoneOn
         saveMode = mode
         savedMicrophoneMute = isMicrophoneMute
-
-        isHeadsetOn = isHeadsetOn()
     }
-    private val vibrator: Vibrator? = context.getSystemService()
+    private val vibrator: Vibrator? = context.getSystemService<Vibrator>()
 
     private var mediaPlayer: MediaPlayer? = MediaPlayer().apply {
         isLooping = true
@@ -49,20 +40,7 @@ class CallAudioManager(private val context: Context) {
     private var isInitiator = false
     private var changedByUser = false
 
-    private val wiredHeadsetReceiver = object : BroadcastReceiver() {
-        override fun onReceive(context: Context?, intent: Intent) {
-            val state = intent.getIntExtra("state", STATE_UNPLUGGED)
-            val newState = state == STATE_PLUGGED
-            if (newState != isHeadsetOn) {
-                isHeadsetOn = newState
-                updateAudioManager()
-            }
-        }
-    }
-
     fun start(isInitiator: Boolean) {
-        context.registerReceiver(wiredHeadsetReceiver, IntentFilter(Intent.ACTION_HEADSET_PLUG))
-
         this.isInitiator = isInitiator
 
         if (!isInitiator && vibrator != null && audioManager.ringerMode != AudioManager.RINGER_MODE_SILENT) {
@@ -75,16 +53,30 @@ class CallAudioManager(private val context: Context) {
             }
         }
 
+        val audioAttributes = AudioAttributes.Builder()
+            .setLegacyStreamType(
+                if (isInitiator) AudioManager.STREAM_VOICE_CALL else AudioManager.STREAM_RING)
+            .build()
+        if (isInitiator) {
+            audioManager.mode = AudioManager.MODE_IN_COMMUNICATION
+        } else {
+            audioManager.mode = AudioManager.MODE_RINGTONE
+        }
+        mediaPlayer?.setAudioAttributes(audioAttributes)
+        audioManager.isSpeakerphoneOn = !isInitiator
         audioManager.isMicrophoneMute = false
-        updateAudioManager()
+        val uri = Uri.parse("android.resource://${context.packageName}/${R.raw.call}")
+        try {
+            mediaPlayer?.setDataSource(context, uri)
+            mediaPlayer?.prepare()
+            mediaPlayer?.start()
+        } catch (e: Exception) {
+            Timber.w("mediaPlayer start, $e")
+        }
     }
 
     fun stop() {
-        audioManager.mode = if (isHeadsetOn) {
-            AudioManager.MODE_NORMAL
-        } else {
-            AudioManager.MODE_IN_COMMUNICATION
-        }
+        audioManager.mode = AudioManager.MODE_IN_COMMUNICATION
         if (mediaPlayer != null) {
             mediaPlayer?.release()
             mediaPlayer = null
@@ -100,50 +92,5 @@ class CallAudioManager(private val context: Context) {
         audioManager.mode = saveMode
         audioManager.isMicrophoneMute = savedMicrophoneMute
         vibrator?.cancel()
-        context.unregisterReceiver(wiredHeadsetReceiver)
-    }
-
-    private fun updateAudioManager() {
-        if (isHeadsetOn) {
-            audioManager.isSpeakerphoneOn = false
-            audioManager.mode = AudioManager.MODE_NORMAL
-        } else {
-            audioManager.isSpeakerphoneOn = !isInitiator
-            if (isInitiator) {
-                audioManager.mode = AudioManager.MODE_IN_COMMUNICATION
-            } else {
-                audioManager.mode = AudioManager.MODE_RINGTONE
-            }
-        }
-
-        mediaPlayer?.reset()
-        val audioAttributes = AudioAttributes.Builder()
-            .setLegacyStreamType(
-                if (isHeadsetOn) {
-                    AudioManager.STREAM_MUSIC
-                } else {
-                    if (isInitiator) {
-                        AudioManager.STREAM_VOICE_CALL
-                    } else {
-                        AudioManager.STREAM_RING
-                    }
-                }
-            )
-            .build()
-        mediaPlayer?.setAudioAttributes(audioAttributes)
-
-        val uri = Uri.parse("android.resource://${context.packageName}/${R.raw.call}")
-        try {
-            mediaPlayer?.setDataSource(context, uri)
-            mediaPlayer?.prepare()
-            mediaPlayer?.start()
-        } catch (e: Exception) {
-            Timber.w("mediaPlayer start, $e")
-        }
-    }
-
-    companion object {
-        private const val STATE_UNPLUGGED = 0
-        private const val STATE_PLUGGED = 1
     }
 }
