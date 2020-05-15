eee4079c3a62bb271f306f55b300f7ba295518bb
SeniorZhai <developer.zhaitao@gmail.com>
2020-05-15 19:00:09 +0800

    Fix flag display bug

--- app/src/main/java/one/mixin/android/widget/FlagLayout.kt
+++ app/src/main/java/one/mixin/android/widget/FlagLayout.kt
@@ -3,39 +3,43 @@ package one.mixin.android.widget
 import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
-import android.widget.LinearLayout
+import android.view.ViewGroup
 import androidx.core.view.isVisible
+import kotlin.math.max
 import kotlinx.android.synthetic.main.view_flag.view.*
 import one.mixin.android.R
-import one.mixin.android.extension.dpToPx
+import one.mixin.android.extension.dp
 import one.mixin.android.extension.translationY
 
 class FlagLayout @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyle: Int = 0
-) : LinearLayout(context, attrs, defStyle) {
+) : ViewGroup(context, attrs, defStyle) {
     var bottomFlag = false
         set(value) {
             if (field != value) {
-                down_flag_layout.isVisible = value
                 field = value
+                requestLayout()
                 update()
             }
         }
     var bottomCountFlag = false
         set(value) {
             if (field != value) {
+                requestLayout()
                 down_unread.isVisible = value
                 field = value
                 update()
             }
         }
+
     var mentionCount = 0
         set(value) {
             if (field != value) {
-                mention_flag_layout.isVisible = value != 0
+                requestLayout()
                 field = value
+                down_unread.isVisible = value == 0
                 mention_count.text = "$field"
                 update()
             }
@@ -45,7 +49,7 @@ class FlagLayout @JvmOverloads constructor(
         if (!bottomCountFlag && !bottomFlag && mentionCount == 0) {
             hide()
         } else {
-            show(100)
+            show()
         }
     }
 
@@ -53,13 +57,36 @@ class FlagLayout @JvmOverloads constructor(
         LayoutInflater.from(context).inflate(R.layout.view_flag, this, true)
     }
 
-    private fun show(duration: Long = 100) {
+    override fun onLayout(changed: Boolean, l: Int, t: Int, r: Int, b: Int) {
+        var bottom = measuredHeight - paddingBottom
+        for (i in childCount - 1 downTo 0) {
+            val child = getChildAt(i)
+            if ((i == 1 && bottomFlag) || (i == 0 && mentionCount != 0)) {
+                child.layout(paddingStart, bottom - child.measuredHeight, paddingStart + child.measuredWidth, bottom)
+                bottom -= child.measuredHeight
+            }
+        }
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        measureChildren(widthMeasureSpec, heightMeasureSpec)
+        var width = 0
+        var height = 0
+        for (i in 0 until childCount) {
+            val child = getChildAt(i)
+            width = max(child.measuredWidth, width)
+            height += child.measuredHeight
+        }
+        setMeasuredDimension(width + paddingStart + paddingEnd, height + paddingTop + paddingBottom)
+    }
+
+    private fun show() {
         if (this.translationY != 0f)
-            translationY(0f, duration)
+            translationY(0f, 100)
     }
 
     private fun hide() {
         if (this.translationY == 0f)
-            translationY(context.dpToPx(130f).toFloat(), 100)
+            translationY(130.dp.toFloat(), 100)
     }
 }
