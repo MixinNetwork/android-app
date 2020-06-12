ccbe3a21b7db55df3b9cbc3b04711fb88d777495
Crossle Song <crosslesong@gmail.com>
2020-06-12 16:14:19 +0800

    Error extra

--- app/src/main/java/one/mixin/android/api/ResponseError.kt
+++ app/src/main/java/one/mixin/android/api/ResponseError.kt
@@ -1,11 +1,13 @@
 package one.mixin.android.api
 
+import com.google.gson.JsonElement
 import java.io.Serializable
 
 data class ResponseError(
     val status: Int,
     val code: Int,
-    val description: String
+    val description: String,
+    val extra: JsonElement?
 ) : Serializable {
     companion object {
         private const val serialVersionUID: Long = 2L
