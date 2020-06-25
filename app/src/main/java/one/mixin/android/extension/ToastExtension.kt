f130c22b4772cc16b8f60f5b75aa8496c9f9a62f
SeniorZhai <developer.zhaitao@gmail.com>
2020-06-25 13:29:16 +0800

    Update style

--- app/src/main/java/one/mixin/android/extension/ToastExtension.kt
+++ app/src/main/java/one/mixin/android/extension/ToastExtension.kt
@@ -4,16 +4,19 @@ package one.mixin.android.extension
 
 import android.content.Context
 import android.view.Gravity
+import android.view.LayoutInflater
 import androidx.annotation.StringRes
 import com.dovar.dtoast.DToast
 import one.mixin.android.R
 
 inline fun Context.toast(text: String, duration: Int = DToast.DURATION_LONG) {
-    DToast.make(this).setText(R.id.tv_content_default, text).setGravity(Gravity.BOTTOM or Gravity.CENTER, 0, 36.dp)
+    DToast.make(this).setView(LayoutInflater.from(this).inflate(R.layout.view_toast, null))
+        .setText(R.id.tv_content_default, text).setGravity(Gravity.BOTTOM or Gravity.CENTER, 0, 128.dp)
         .setDuration(duration).show()
 }
 
 inline fun Context.toast(@StringRes resId: Int, duration: Int = DToast.DURATION_LONG) {
-    DToast.make(this).setText(R.id.tv_content_default, getString(resId)).setGravity(Gravity.BOTTOM or Gravity.CENTER, 0, 36.dp)
+    DToast.make(this).setView(LayoutInflater.from(this).inflate(R.layout.view_toast, null))
+        .setText(R.id.tv_content_default, getString(resId)).setGravity(Gravity.BOTTOM or Gravity.CENTER, 0, 128.dp)
         .setDuration(duration).show()
 }
