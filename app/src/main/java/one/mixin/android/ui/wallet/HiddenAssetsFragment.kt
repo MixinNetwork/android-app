6f275281e86c60acac9e95f1d3acc0960cfa6495
touge <touge1027@gmail.com>
2020-07-09 10:57:33 +0800

    Fix async show snackbar

--- app/src/main/java/one/mixin/android/ui/wallet/HiddenAssetsFragment.kt
+++ app/src/main/java/one/mixin/android/ui/wallet/HiddenAssetsFragment.kt
@@ -56,20 +56,22 @@ class HiddenAssetsFragment : BaseFragment(), HeaderAdapter.OnItemListener {
                     val hiddenPos = viewHolder.absoluteAdapterPosition
                     val asset = assetsAdapter.data!![assetsAdapter.getPosition(hiddenPos)]
                     val deleteItem = assetsAdapter.removeItem(hiddenPos)!!
-                    lifecycleScope.launch(Dispatchers.IO) {
+                    lifecycleScope.launch {
                         walletViewModel.updateAssetHidden(asset.assetId, false)
+                        val anchorView = assets_rv ?: return@launch
+
+                        Snackbar.make(anchorView, getString(R.string.wallet_already_shown, asset.symbol), Snackbar.LENGTH_LONG)
+                            .setAction(R.string.undo_capital) {
+                                assetsAdapter.restoreItem(deleteItem, hiddenPos)
+                                lifecycleScope.launch(Dispatchers.IO) {
+                                    walletViewModel.updateAssetHidden(asset.assetId, true)
+                                }
+                            }.setActionTextColor(ContextCompat.getColor(requireContext(), R.color.wallet_blue)).apply {
+                                this.view.setBackgroundResource(R.color.call_btn_icon_checked)
+                                (this.view.findViewById(R.id.snackbar_text) as TextView)
+                                    .setTextColor(ContextCompat.getColor(requireContext(), R.color.white))
+                            }.show()
                     }
-                    Snackbar.make(assets_rv, getString(R.string.wallet_already_shown, asset.symbol), Snackbar.LENGTH_LONG)
-                        .setAction(R.string.undo_capital) {
-                            assetsAdapter.restoreItem(deleteItem, hiddenPos)
-                            lifecycleScope.launch(Dispatchers.IO) {
-                                walletViewModel.updateAssetHidden(asset.assetId, true)
-                            }
-                        }.setActionTextColor(ContextCompat.getColor(requireContext(), R.color.wallet_blue)).apply {
-                            this.view.setBackgroundResource(R.color.call_btn_icon_checked)
-                            (this.view.findViewById(R.id.snackbar_text) as TextView)
-                                .setTextColor(ContextCompat.getColor(requireContext(), R.color.white))
-                        }.show()
                 }
             })
         ).apply { attachToRecyclerView(assets_rv) }
