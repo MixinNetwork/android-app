0d51179544e95a8080c6412c59faade9227937f3
touge <touge1027@gmail.com>
2020-10-28 20:04:36 +0800

    More tests for transactions page

--- app/src/androidTest/java/one/mixin/android/ui/wallet/AllTransactionsFragmentTest.kt
+++ app/src/androidTest/java/one/mixin/android/ui/wallet/AllTransactionsFragmentTest.kt
@@ -3,17 +3,14 @@ package one.mixin.android.ui.wallet
 import androidx.navigation.NavController
 import androidx.recyclerview.widget.RecyclerView
 import androidx.test.core.app.ActivityScenario
-import androidx.test.espresso.Espresso
-import androidx.test.espresso.IdlingRegistry
-import androidx.test.espresso.action.ViewActions
-import androidx.test.espresso.contrib.RecyclerViewActions
-import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import one.mixin.android.R
-import one.mixin.android.util.EspressoIdlingResource
-import org.junit.After
 import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Rule
@@ -22,7 +19,7 @@ import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 @HiltAndroidTest
-class AllTransactionsFragmentTest {
+class AllTransactionsFragmentTest : BaseTransactionsFragmentTest() {
 
     @get:Rule
     var hiltRule = HiltAndroidRule(this)
@@ -32,37 +29,29 @@ class AllTransactionsFragmentTest {
         hiltRule.inject()
     }
 
-    @Before
-    fun registerIdlingResource() {
-        IdlingRegistry.getInstance().register(EspressoIdlingResource.countingIdlingResource)
-    }
-
-    @After
-    fun unregisterIdlingResource() {
-        IdlingRegistry.getInstance().unregister(EspressoIdlingResource.countingIdlingResource)
-    }
-
     @Test
     fun testClickItem() {
-        go2AllTransactions { navController ->
-            Espresso.onView(ViewMatchers.withId(R.id.transactions_rv))
-                .perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, ViewActions.click()))
+        go2Transactions { navController, _ ->
+            onView(withId(R.id.transactions_rv))
+                .perform(actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click()))
             assertTrue(navController?.currentDestination?.id == R.id.transaction_fragment)
         }
     }
 
-    private fun go2AllTransactions(action: (NavController?) -> Unit) {
+    override fun go2Transactions(action: (NavController?, ActivityScenario<WalletActivity>) -> Unit) {
         var navController: NavController? = null
         val activityScenario = ActivityScenario.launch(WalletActivity::class.java).onActivity {
             navController = it.navController
         }
 
-        Espresso.onView(ViewMatchers.withId(R.id.right_animator)).perform(ViewActions.click())
-        Espresso.onView(ViewMatchers.withId(R.id.transactions_tv)).perform(ViewActions.click())
+        onView(withId(R.id.right_animator)).perform(click())
+        onView(withId(R.id.transactions_tv)).perform(click())
         assertTrue(navController?.currentDestination?.id == R.id.all_transactions_fragment)
 
-        action.invoke(navController)
+        action.invoke(navController, activityScenario)
 
         activityScenario.close()
     }
+
+    override fun isAllTransactions() = true
 }
--- app/src/androidTest/java/one/mixin/android/ui/wallet/TransactionsFragmentTest.kt
+++ app/src/androidTest/java/one/mixin/android/ui/wallet/TransactionsFragmentTest.kt
@@ -1,36 +1,39 @@
 package one.mixin.android.ui.wallet
 
 import android.content.Context
-import android.widget.TextView
 import androidx.navigation.NavController
 import androidx.recyclerview.widget.RecyclerView
 import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.IdlingRegistry
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition
 import androidx.test.espresso.matcher.RootMatchers.isDialog
+import androidx.test.espresso.matcher.RootMatchers.withDecorView
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
-import kotlinx.android.synthetic.main.item_wallet_transactions.view.*
 import one.mixin.android.R
-import one.mixin.android.util.EspressoIdlingResource
-import org.junit.After
+import one.mixin.android.mock.mockAsset
+import one.mixin.android.mock.mockAssetWithDestination
+import one.mixin.android.mock.mockAssetWithDestinationAndTag
+import one.mixin.android.util.ErrorHandler
+import one.mixin.android.vo.toAssetItem
+import org.hamcrest.core.Is.`is`
+import org.hamcrest.core.IsNot.not
 import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import kotlin.math.absoluteValue
 
 @RunWith(AndroidJUnit4::class)
 @HiltAndroidTest
-class TransactionsFragmentTest {
+class TransactionsFragmentTest : BaseTransactionsFragmentTest() {
 
     @get:Rule
     var hiltRule = HiltAndroidRule(this)
@@ -38,16 +41,6 @@ class TransactionsFragmentTest {
     @Before
     fun init() {
         hiltRule.inject()
-    }
-
-    @Before
-    fun registerIdlingResource() {
-        IdlingRegistry.getInstance().register(EspressoIdlingResource.countingIdlingResource)
-    }
-
-    @After
-    fun unregisterIdlingResource() {
-        IdlingRegistry.getInstance().unregister(EspressoIdlingResource.countingIdlingResource)
     }
 
     @Test
@@ -90,6 +83,13 @@ class TransactionsFragmentTest {
     fun testTransactionsRv() {
         go2Transactions { navController, activityScenario ->
             // open first transaction item
+            var itemCount = 0
+            activityScenario.onActivity {
+                val rv = it.findViewById<RecyclerView>(R.id.transactions_rv)
+                itemCount = rv.adapter?.itemCount ?: 0
+            }
+            if (itemCount < 1) return@go2Transactions
+
             onView(withId(R.id.transactions_rv))
                 .perform(actionOnItemAtPosition<RecyclerView.ViewHolder>(1, click()))
             assertTrue(navController?.currentDestination?.id == R.id.transaction_fragment)
@@ -100,48 +100,74 @@ class TransactionsFragmentTest {
     }
 
     @Test
-    fun testFilter() {
-        val ctx: Context = ApplicationProvider.getApplicationContext()
+    fun testOpenTransferOut() {
         go2Transactions { navController, activityScenario ->
-            // sort by amount
-            onView(withId(R.id.group_info_member_title_sort)).perform(click())
-            onView(withId(R.id.apply_tv))
-                .inRoot(isDialog())
-                .check(matches(isDisplayed()))
-            onView(withId(R.id.sort_amount)).perform(click())
-            onView(withId(R.id.apply_tv)).perform(click())
-            assertTrue(navController?.currentDestination?.id == R.id.transactions_fragment)
-
+            // open send contact
+            openSendBottom(navController, R.id.contact, R.id.single_friend_select_fragment)
             activityScenario.onActivity {
-                val rv = it.findViewById<RecyclerView>(R.id.transactions_rv)
-                val itemCount = rv.adapter?.itemCount ?: 1
-                if (itemCount > 2) {
-                    // ignore head
-                    val first = rv.findViewHolderForAdapterPosition(1)
-                    val second = rv.findViewHolderForAdapterPosition(2)
-                    val firstAmount = first?.itemView?.findViewById<TextView>(R.id.value)?.text?.toString()?.toDoubleOrNull()?.absoluteValue
-                    val secondAmount = second?.itemView?.findViewById<TextView>(R.id.value)?.text?.toString()?.toDoubleOrNull()?.absoluteValue
-                    if (firstAmount != null && secondAmount != null) {
-                        assertTrue(firstAmount >= secondAmount)
-                    }
-                }
+                navController?.navigateUp()
             }
 
-            // filter by transfer
-            onView(withId(R.id.group_info_member_title_sort)).perform(click())
-            onView(withId(R.id.apply_tv))
-                .inRoot(isDialog())
-                .check(matches(isDisplayed()))
-            onView(withId(R.id.filters_radio_transfer)).perform(click())
-            onView(withId(R.id.apply_tv)).perform(click())
-            assertTrue(navController?.currentDestination?.id == R.id.transactions_fragment)
-            // val name = ctx.getString(R.string.filters_transfer)
-            // onView(allOf(withId(R.id.name)))
-            //     .check(matches(withText(name)))
+            // open send contact
+            openSendBottom(navController, R.id.address, R.id.address_management_fragment)
+            activityScenario.onActivity {
+                navController?.navigateUp()
+            }
+
+            // cancel send bottom
+            openSendBottom(navController, R.id.send_cancel, R.id.transactions_fragment)
         }
     }
 
-    private fun go2Transactions(action: (NavController?, ActivityScenario<WalletActivity>) -> Unit) {
+    @Test
+    fun testOpenDepositAccount() {
+        val ctx: Context = ApplicationProvider.getApplicationContext()
+        go2Transactions { navController, activityScenario ->
+            var fragment: TransactionsFragment? = null
+            activityScenario.onActivity { activity ->
+                fragment = activity.supportFragmentManager.primaryNavigationFragment?.childFragmentManager?.fragments?.first() as TransactionsFragment
+            }
+
+            fragment?.asset = mockAsset().toAssetItem()
+            onView(withId(R.id.receive_tv)).perform(click())
+            onView(withText(ctx.getString(R.string.error_bad_data, ErrorHandler.BAD_DATA)))
+                .inRoot(withDecorView(not(`is`(fragment?.activity?.window?.decorView))))
+                .check(matches(isDisplayed()))
+
+            fragment?.asset = mockAssetWithDestinationAndTag().toAssetItem()
+            onView(withId(R.id.receive_tv)).perform(click())
+            assertTrue(navController?.currentDestination?.id == R.id.deposit_account_fragment)
+        }
+    }
+
+    @Test
+    fun testOpenDepositPublicKey() {
+        go2Transactions { navController, activityScenario ->
+            var fragment: TransactionsFragment? = null
+            activityScenario.onActivity { activity ->
+                fragment = activity.supportFragmentManager.primaryNavigationFragment?.childFragmentManager?.fragments?.first() as TransactionsFragment
+            }
+
+            fragment?.asset = mockAssetWithDestination().toAssetItem()
+            onView(withId(R.id.receive_tv)).perform(click())
+            assertTrue(navController?.currentDestination?.id == R.id.deposit_public_key_fragment)
+        }
+    }
+
+    private fun openSendBottom(
+        navController: NavController?,
+        clickId: Int,
+        expectFragmentId: Int,
+    ) {
+        onView(withId(R.id.send_tv)).perform(click())
+        onView(withId(R.id.send_cancel))
+            .inRoot(isDialog())
+            .check(matches(isDisplayed()))
+        onView(withId(clickId)).perform(click())
+        assertTrue(navController?.currentDestination?.id == expectFragmentId)
+    }
+
+    override fun go2Transactions(action: (NavController?, ActivityScenario<WalletActivity>) -> Unit) {
         var navController: NavController? = null
         val activityScenario = ActivityScenario.launch(WalletActivity::class.java).onActivity {
             navController = it.navController
@@ -153,4 +179,6 @@ class TransactionsFragmentTest {
 
         activityScenario.close()
     }
+
+    override fun isAllTransactions() = false
 }
