fff9c32fa4c8dfb89c49ffceb542a214fba95cdd
SeniorZhai <developer.zhaitao@gmail.com>
2020-12-18 13:09:00 +0800

    Support reply

--- app/src/main/java/one/mixin/android/util/mention/MentionTextView.kt
+++ app/src/main/java/one/mixin/android/util/mention/MentionTextView.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.text.SpannableStringBuilder
 import android.util.AttributeSet
 import androidx.appcompat.widget.AppCompatTextView
+import one.mixin.android.extension.notNullWithElse
 import one.mixin.android.util.markdown.MarkwonUtil
 import org.commonmark.node.Node
 
@@ -12,14 +13,14 @@ class MentionTextView(context: Context, attrs: AttributeSet?) :
     var mentionRenderContext: MentionRenderContext? = null
 
     override fun setText(text: CharSequence?, type: BufferType) {
-        if (text.isNullOrBlank() || mentionRenderContext == null) {
+        if (text.isNullOrBlank()) {
             super.setText(text, type)
             return
         } else {
             val sp = SpannableStringBuilder()
             renderMarkdown(sp, MarkwonUtil.getSimpleMarkwon(context).parse(text.toString()))
-
-            super.setText(renderMention(sp), type)
+            mentionRenderContext.notNullWithElse({ super.setText(renderMention(sp), type) },
+                { super.setText(sp, type) })
         }
     }
 
