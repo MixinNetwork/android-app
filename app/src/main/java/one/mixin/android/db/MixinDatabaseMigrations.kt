6f15ea6d13b1055721df67077663e9925cdd0b79
touge <touge1027@gmail.com>
2021-01-07 13:15:18 +0800

    Add index_users_relationship_full_name

--- app/src/main/java/one/mixin/android/db/MixinDatabaseMigrations.kt
+++ app/src/main/java/one/mixin/android/db/MixinDatabaseMigrations.kt
@@ -249,6 +249,7 @@ class MixinDatabaseMigrations private constructor() {
         val MIGRATION_35_36: Migration = object : Migration(35, 36) {
             override fun migrate(database: SupportSQLiteDatabase) {
                 database.execSQL("CREATE INDEX IF NOT EXISTS `index_conversations_category_status_pin_time_created_at` ON `conversations` (`category`, `status`, `pin_time`, `created_at`)")
+                database.execSQL("CREATE INDEX IF NOT EXISTS `index_users_relationship_full_name` ON `users` (`relationship`, `full_name`)")
             }
         }
     }
