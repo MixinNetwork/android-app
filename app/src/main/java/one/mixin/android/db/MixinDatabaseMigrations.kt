3f641ae970d5638505eef317f3c5d45068eccd89
SeniorZhai <developer.zhaitao@gmail.com>
2021-01-09 13:00:21 +0800

    Job index

--- app/src/main/java/one/mixin/android/db/MixinDatabaseMigrations.kt
+++ app/src/main/java/one/mixin/android/db/MixinDatabaseMigrations.kt
@@ -239,5 +239,17 @@ class MixinDatabaseMigrations private constructor() {
                 database.execSQL("DROP INDEX IF EXISTS `index_messages_user_id`")
             }
         }
+
+        val MIGRATION_34_35: Migration = object : Migration(34, 35) {
+            override fun migrate(database: SupportSQLiteDatabase) {
+                database.execSQL("CREATE INDEX IF NOT EXISTS `index_jobs_action` ON `jobs` (`action`)")
+            }
+        }
+
+        val MIGRATION_35_34: Migration = object : Migration(35, 34) {
+            override fun migrate(database: SupportSQLiteDatabase) {
+                database.execSQL("DROP INDEX IF EXISTS `index_jobs_action`")
+            }
+        }
     }
 }
