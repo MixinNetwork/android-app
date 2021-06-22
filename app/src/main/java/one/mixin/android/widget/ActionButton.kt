9026f8b8355512e61e36a5137b2a7cb5f894d9ec
SeniorZhai <developer.zhaitao@gmail.com>
2021-06-22 17:15:49 +0800

    App group button style

--- app/src/main/java/one/mixin/android/widget/ActionButton.kt
+++ app/src/main/java/one/mixin/android/widget/ActionButton.kt
@@ -1,9 +1,12 @@
 package one.mixin.android.widget
 
 import android.content.Context
+import android.content.res.ColorStateList
+import android.util.TypedValue
 import android.view.ViewGroup
 import androidx.appcompat.widget.AppCompatTextView
 import one.mixin.android.R
+import one.mixin.android.extension.colorFromAttribute
 import one.mixin.android.extension.round
 import org.jetbrains.anko.dip
 
@@ -14,6 +17,10 @@ class ActionButton(context: Context) : AppCompatTextView(context) {
             ViewGroup.LayoutParams.WRAP_CONTENT
         )
         setBackgroundResource(R.drawable.bg_action_button)
+        val outValue = TypedValue()
+        context.theme.resolveAttribute(android.R.attr.selectableItemBackground, outValue, true)
+        foreground = resources.getDrawable(outValue.resourceId, context.theme)
+        backgroundTintList = ColorStateList.valueOf(context.colorFromAttribute(R.attr.bg_bubble))
         setPaddingRelative(dip(8), dip(8), dip(8), dip(8))
         round(dip(8f))
     }
