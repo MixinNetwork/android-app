b66a4e08af16b927bd44759b4903e66df26493c2
SeniorZhai <developer.zhaitao@gmail.com>
2021-07-27 18:32:08 +0800

    Replace text test

---
+++ app/src/test/java/one/mixin/android/markdown/TextTest.kt
@@ -0,0 +1,48 @@
+package one.mixin.android.markdown
+
+import junit.framework.TestCase.assertEquals
+import org.junit.Test
+
+class TextTest {
+
+    private val MARKDOWN_BOLD = Regex("(\\*\\*|__)(.*?)\\1")
+    private val MARKDOWN_SLANT = Regex("(\\*|_)(.*?)\\1")
+    private val MARKDOWN_STRIKETHROUGH = Regex("(\\~\\~)(.*?)\\~\\~")
+    private val MARKDOWN_INLINE = Regex("`(.*?)`")
+
+    @Test
+    fun testReplaceText() {
+        val source = "123 __粗体__ ~~删除~~ *斜* **粗** 123 `code` _斜体_ mixin"
+        var content = source
+        for (regex in arrayOf(
+            MARKDOWN_BOLD,
+            MARKDOWN_SLANT,
+            MARKDOWN_STRIKETHROUGH,
+            MARKDOWN_INLINE
+        )) {
+            content = content.replace(regex) { matchResult ->
+                val str = matchResult.value
+                val replaceSize = replaceSize(matchResult.value)
+                if (str.length <= replaceSize) {
+                    str
+                } else {
+                    str.substring(replaceSize, str.length - replaceSize)
+                }
+            }
+        }
+        println(content)
+        assertEquals(content, "123 粗体 删除 斜 粗 123 code 斜体 mixin")
+    }
+
+    private fun replaceSize(str: String): Int {
+        return if (str.startsWith("**") && str.endsWith("**")) {
+            2
+        } else if (str.startsWith("__") && str.endsWith("__")) {
+            2
+        } else if (str.startsWith("~~") && str.endsWith("~~")) {
+            2
+        } else {
+            1
+        }
+    }
+}
