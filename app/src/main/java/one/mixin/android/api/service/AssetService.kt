5b01d3a8dafbc94376285e6e2b3543f14af7d326
Crossle Song <crosslesong@gmail.com>
2021-10-09 09:47:52 +0800

    Filter lp token on top asset

--- app/src/main/java/one/mixin/android/api/service/AssetService.kt
+++ app/src/main/java/one/mixin/android/api/service/AssetService.kt
@@ -80,7 +80,7 @@ interface AssetService {
     suspend fun queryAssets(@Path("query") query: String): MixinResponse<List<Asset>>
 
     @GET("network/assets/top")
-    fun topAssets(): Call<MixinResponse<List<TopAsset>>>
+    fun topAssets(@Query("kind") kind: String = "NORMAL"): Call<MixinResponse<List<TopAsset>>>
 
     @GET("/snapshots/trace/{id}")
     suspend fun getTrace(@Path("id") traceId: String): MixinResponse<Snapshot>
