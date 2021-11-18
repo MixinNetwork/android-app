20e7227bf070012bd6e357a37ec0f4aa5cf045b3
SeniorZhai <developer.zhaitao@gmail.com>
2021-11-18 15:27:25 +0800

    Fix forward attachment

--- app/src/main/java/one/mixin/android/vo/AttachmentExtra.kt
+++ app/src/main/java/one/mixin/android/vo/AttachmentExtra.kt
@@ -1,12 +1,18 @@
 package one.mixin.android.vo
 
 import com.google.gson.annotations.SerializedName
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
 
+@JsonClass(generateAdapter = true)
 class AttachmentExtra(
     @SerializedName("attachment_id")
+    @Json(name = "attachment_id")
     var attachmentId: String,
     @SerializedName("message_id")
+    @Json(name = "message_id")
     var messageId: String?,
     @SerializedName("created_at")
+    @Json(name = "created_at")
     var createdAt: String? = null,
 )
