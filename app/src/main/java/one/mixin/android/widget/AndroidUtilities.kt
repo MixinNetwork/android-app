5633e90a767921ffae9d8a47ef8e21c9a8209695
SeniorZhai <developer.zhaitao@gmail.com>
2021-12-27 10:24:34 +0800

    Surprise layout (#2726)

--- app/src/main/java/one/mixin/android/widget/AndroidUtilities.kt
+++ app/src/main/java/one/mixin/android/widget/AndroidUtilities.kt
@@ -5,7 +5,7 @@ import one.mixin.android.MixinApplication
 import org.jetbrains.anko.dip
 
 object AndroidUtilities {
-    var displayMetrics = DisplayMetrics()
+    private var displayMetrics = DisplayMetrics()
     var screenRefreshRate = 60
     fun getPixelsInCM(cm: Float, isX: Boolean): Float {
         return cm / 2.54f * if (isX) displayMetrics.xdpi else displayMetrics.ydpi
