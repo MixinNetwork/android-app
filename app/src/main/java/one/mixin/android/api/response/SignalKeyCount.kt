8a5412a302b2a712c47205344926ee0cf36db669
SeniorZhai <developer.zhaitao@gmail.com>
2021-12-28 15:01:56 +0800

    Lost json adapter

--- app/src/main/java/one/mixin/android/api/response/SignalKeyCount.kt
+++ app/src/main/java/one/mixin/android/api/response/SignalKeyCount.kt
@@ -1,8 +1,12 @@
 package one.mixin.android.api.response
 
 import com.google.gson.annotations.SerializedName
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
 
+@JsonClass(generateAdapter = true)
 data class SignalKeyCount(
     @SerializedName("one_time_pre_keys_count")
+    @Json(name = "one_time_pre_keys_count")
     val preKeyCount: Int
 )
