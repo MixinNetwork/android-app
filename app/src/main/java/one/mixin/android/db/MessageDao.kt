7101b4b9b605ce03be6220db2c95b7604e3f275c
SeniorZhai <developer.zhaitao@gmail.com>
2022-01-19 09:49:31 +0800

    Fix category usage

--- app/src/main/java/one/mixin/android/db/MessageDao.kt
+++ app/src/main/java/one/mixin/android/db/MessageDao.kt
@@ -510,7 +510,7 @@ interface MessageDao : BaseDao<Message> {
     )
     fun findAttachmentMigration(rowId: Long, limit: Int, offset: Long): List<AttachmentMigration>
 
-    @Query("SELECT rowid IS NOT NULL FROM messages WHERE category IN ('SIGNAL_IMAGE','PLAIN_IMAGE', 'SIGNAL_VIDEO', 'PLAIN_VIDEO', 'SIGNAL_DATA', 'PLAIN_DATA', 'SIGNAL_AUDIO', 'PLAIN_AUDIO') AND media_status = 'DONE' LIMIT 1")
+    @Query("SELECT rowid IS NOT NULL FROM messages WHERE category IN ('SIGNAL_IMAGE','PLAIN_IMAGE', 'ENCRYPTED_IMAGE', 'SIGNAL_VIDEO', 'PLAIN_VIDEO', 'ENCRYPTED_VIDEO', 'SIGNAL_DATA', 'PLAIN_DATA', 'ENCRYPTED_DATA', 'SIGNAL_AUDIO', 'PLAIN_AUDIO', 'ENCRYPTED_AUDIO') AND media_status = 'DONE' LIMIT 1")
     fun hasDoneAttachment(): Boolean
 
     @Query("SELECT rowid FROM messages ORDER BY rowid DESC LIMIT 1")
