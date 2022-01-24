b68d5d3cddd39eeb6ba41da54f30b8d8358f5a32
SeniorZhai <developer.zhaitao@gmail.com>
2022-01-24 16:37:19 +0800

    Update url regex

--- app/src/main/java/one/mixin/android/widget/linktext/RegexParser.java
+++ app/src/main/java/one/mixin/android/widget/linktext/RegexParser.java
@@ -9,7 +9,7 @@ class RegexParser {
     static final String EMAIL_PATTERN = Patterns.EMAIL_ADDRESS.pattern();
     static final String HASHTAG_PATTERN = "(?:^|\\s|$)#[\\p{L}0-9_]*";
     static final String MENTION_PATTERN = "@(\\S|\\b)+(?:\\s|$)";
-    static final String URL_PATTERN = "[a-zA-z]+://[^\\s]*(?<!\\))";
+    static final String URL_PATTERN = "[a-zA-z]+:\\/\\/[^\\s].*?((?=[\"\\s，）)(（。：])|$)";
     static final String BOT_PATTERN = "7000\\d{6}";
 
 }
