2d1efbe203837af31926a00fe56c542e07ad3b67
touge <touge1027@gmail.com>
2022-04-21 21:02:08 +0800

    Remove PinView animation

--- app/src/main/java/one/mixin/android/widget/PinView.kt
+++ app/src/main/java/one/mixin/android/widget/PinView.kt
@@ -9,10 +9,6 @@ import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup.LayoutParams.MATCH_PARENT
-import android.view.animation.AlphaAnimation
-import android.view.animation.AnimationSet
-import android.view.animation.OvershootInterpolator
-import android.view.animation.TranslateAnimation
 import android.widget.LinearLayout
 import android.widget.TextView
 import one.mixin.android.R
@@ -95,15 +91,10 @@ class PinView : LinearLayout {
             binding.tipTv.visibility = View.INVISIBLE
         }
 
-        if (index > 0) {
-            val preItem = views[index - 1]
-            toStar(preItem)
-        }
-
         val curItem = views[index]
-        animIn(curItem, s)
+        curItem.text = STAR
+        curItem.textSize = textSize
         codes.append(index, s)
-        toStar(curItem, 1500)
         index++
 
         listener?.onUpdate(index)
@@ -113,7 +104,9 @@ class PinView : LinearLayout {
         if (s.length != count) return
         for (i in 0 until count) {
             val c = s[i]
-            toStar(views[i])
+            val v = views[i]
+            v.text = STAR
+            v.textSize = textSize
             codes.append(i, c.toString())
         }
         listener?.onUpdate(count)
@@ -160,37 +153,6 @@ class PinView : LinearLayout {
     }
 
     fun getCount() = count
-
-    private fun animIn(codeView: TextView, s: String) {
-        val translateIn = TranslateAnimation(0f, 0f, codeView.height.toFloat(), 0f)
-        translateIn.interpolator = OvershootInterpolator()
-        translateIn.duration = 500
-
-        val fadeIn = AlphaAnimation(0f, 1f)
-        fadeIn.duration = 200
-
-        val animationSet = AnimationSet(false)
-        animationSet.addAnimation(fadeIn)
-        animationSet.addAnimation(translateIn)
-        animationSet.reset()
-        animationSet.startTime = 0
-
-        codeView.text = s
-        codeView.textSize = textSize
-        codeView.clearAnimation()
-        codeView.startAnimation(animationSet)
-    }
-
-    private fun toStar(codeView: TextView, delay: Long = 0) {
-        codeView.postDelayed(
-            {
-                if (codeView.text.isNotEmpty()) {
-                    codeView.text = STAR
-                }
-            },
-            delay
-        )
-    }
 
     interface OnPinListener {
         fun onUpdate(index: Int)
