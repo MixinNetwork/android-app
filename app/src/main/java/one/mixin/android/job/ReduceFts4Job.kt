ac5ed041fb7d312bc9cb6548e7eeedf963493654
SeniorZhai <developer.zhaitao@gmail.com>
2022-04-26 23:39:12 +0800

    Do nothing

--- app/src/main/java/one/mixin/android/job/ReduceFts4Job.kt
+++ app/src/main/java/one/mixin/android/job/ReduceFts4Job.kt
@@ -3,7 +3,6 @@ package one.mixin.android.job
 import com.birbit.android.jobqueue.Params
 import kotlinx.coroutines.runBlocking
 import one.mixin.android.Constants.Account.PREF_FTS4_REDUCE
-import one.mixin.android.Constants.DB_DELETE_LIMIT
 import one.mixin.android.util.PropertyHelper
 import timber.log.Timber
 
@@ -14,21 +13,7 @@ class ReduceFts4Job :
     }
 
     override fun onRun() = runBlocking {
-        val list = messageFts4Dao.excludeIds(DB_DELETE_LIMIT)
-        delete(list)
-        if (list.size < DB_DELETE_LIMIT) {
-            PropertyHelper.updateKeyValue(PREF_FTS4_REDUCE, false.toString())
-            Timber.e("Delete message fts4 completed!!!")
-        } else {
-            PropertyHelper.updateKeyValue(PREF_FTS4_REDUCE, true.toString())
-            jobManager.addJobInBackground(ReduceFts4Job())
-        }
-    }
-
-    private suspend fun delete(list: List<String>) {
-        if (list.isEmpty()) return
-        val startTime = System.currentTimeMillis()
-        messageFts4Dao.deleteByMessageIds(list)
-        Timber.e("Delete message fts4 ${list.size}, cost: ${System.currentTimeMillis() - startTime} ms")
+        PropertyHelper.updateKeyValue(PREF_FTS4_REDUCE, false.toString())
+        Timber.e("Delete message fts4 completed!!!")
     }
 }
