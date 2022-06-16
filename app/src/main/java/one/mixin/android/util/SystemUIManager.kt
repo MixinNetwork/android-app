61c8a38c89e8f9eaa978c93bb558aa2844475cec
SeniorZhai <developer.zhaitao@gmail.com>
2022-06-16 13:41:11 +0800

    Replace deprecated funcation

--- app/src/main/java/one/mixin/android/util/SystemUIManager.kt
+++ app/src/main/java/one/mixin/android/util/SystemUIManager.kt
@@ -3,7 +3,6 @@ package one.mixin.android.util
 import android.annotation.SuppressLint
 import android.view.Window
 import androidx.annotation.ColorInt
-import androidx.core.view.ViewCompat
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
@@ -26,31 +25,31 @@ object SystemUIManager {
     fun fullScreen(window: Window) {
         WindowCompat.setDecorFitsSystemWindows(window, false)
 
-        ViewCompat.getWindowInsetsController(window.decorView)?.apply {
+        WindowCompat.getInsetsController(window,window.decorView).apply {
             hide(WindowInsetsCompat.Type.systemBars())
             systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
         }
     }
 
     fun lightUI(window: Window, light: Boolean) {
-        val controller = ViewCompat.getWindowInsetsController(window.decorView)
-        controller?.apply {
+        val controller = WindowCompat.getInsetsController(window,window.decorView)
+        controller.apply {
             isAppearanceLightStatusBars = light
             isAppearanceLightNavigationBars = light
         }
     }
 
     fun hideSystemUI(window: Window) {
-        val controller = ViewCompat.getWindowInsetsController(window.decorView)
-        controller?.apply {
+        val controller = WindowCompat.getInsetsController(window,window.decorView)
+        controller.apply {
             hide(WindowInsetsCompat.Type.systemBars())
             systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
         }
     }
 
     fun showSystemUI(window: Window) {
-        val controller = ViewCompat.getWindowInsetsController(window.decorView)
-        controller?.apply {
+        val controller = WindowCompat.getInsetsController(window,window.decorView)
+        controller.apply {
             show(WindowInsetsCompat.Type.systemBars())
         }
     }
