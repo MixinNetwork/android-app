8ea95388574a8ae3e2e8a2d29edc6307ef6a5e49
touge <touge1027@gmail.com>
2022-06-17 21:07:33 +0800

    Add request node sig

--- app/src/main/java/one/mixin/android/api/service/TipNodeService.kt
+++ app/src/main/java/one/mixin/android/api/service/TipNodeService.kt
@@ -1,10 +1,18 @@
 package one.mixin.android.api.service
 
 import one.mixin.android.api.MixinResponse
+import one.mixin.android.api.request.TipSignRequest
 import one.mixin.android.api.response.TipConfig
+import one.mixin.android.api.response.TipSignResponse
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Url
 
 interface TipNodeService {
     @GET("config.json")
     suspend fun tipConfig(): MixinResponse<TipConfig>
+
+    @POST("/")
+    suspend fun postSign(@Body tipSignRequest: TipSignRequest, @Url baseUrl: String): MixinResponse<TipSignResponse>
 }
--- app/src/main/java/one/mixin/android/tip/Tip.kt
+++ app/src/main/java/one/mixin/android/tip/Tip.kt
@@ -1,17 +1,30 @@
 package one.mixin.android.tip
 
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.coroutineScope
 import one.mixin.android.api.handleMixinResponse
+import one.mixin.android.api.request.TipSignData
+import one.mixin.android.api.request.TipSignRequest
 import one.mixin.android.api.response.TipSigner
 import one.mixin.android.api.service.TipNodeService
 import one.mixin.android.crypto.blst.aggregateSignatures
 import one.mixin.android.crypto.blst.sign
+import one.mixin.android.extension.currentTimeSeconds
 import one.mixin.android.extension.decodeBase64
 import one.mixin.android.extension.toHex
+import one.mixin.android.extension.toLeByteArray
+import one.mixin.android.util.GsonHelper
+import timber.log.Timber
 import java.security.MessageDigest
 import javax.inject.Inject
+import kotlin.time.Duration.Companion.days
 
 class Tip @Inject internal constructor(private val tipNodeService: TipNodeService) {
+    private val ephemeralGrace = 128.days.inWholeMilliseconds.toString()
+
+    private val gson = GsonHelper.customGson
 
     suspend fun genPriv(identityPub: String, ephemeral: String): String? {
         val tipConfig = handleMixinResponse(
@@ -27,7 +40,7 @@ class Tip @Inject internal constructor(private val tipNodeService: TipNodeServic
 
         val nodeSeeds = getNodeSeeds(tipConfig.signers, identityPubBytes, ephemeralBytes)
 
-        val nodeSigs = getNodeSigs(tipConfig.signers, nodeSeeds)
+        val nodeSigs = getNodeSigs(tipConfig.signers, identityPubBytes, nodeSeeds, ephemeral)
 
         val aggSig = aggregateSignatures(nodeSigs)
         return aggSig.compress().toHex()
@@ -47,18 +60,61 @@ class Tip @Inject internal constructor(private val tipNodeService: TipNodeServic
         return nodeSeeds
     }
 
-    private suspend fun getNodeSigs(tipSigners: List<TipSigner>, nodeSeeds: List<ByteArray>): List<String> {
+    private suspend fun getNodeSigs(tipSigners: List<TipSigner>, identityPub: ByteArray, nodeSeeds: List<ByteArray>, ephemeral: String): List<String> {
         require(tipSigners.size == nodeSeeds.size) { "Required tipSigners size equals nodeSeeds size failed." }
-        return emptyList()
+        val result = mutableListOf<String>()
+        coroutineScope {
+            tipSigners.mapIndexed { i, signer ->
+                async(Dispatchers.IO) {
+                    var success = false
+                    var retryCount = 0
+
+                    while (!success) {
+                        val sign = fetchTipNodeSigs(signer, identityPub, ephemeral)
+                        success = sign != null
+                        if (success) {
+                            require(sign != null)
+                            result.add(i, sign)
+                            Timber.d("fetch tip node $i sign success")
+                            return@async
+                        }
+
+                        retryCount++
+                        Timber.d("fetch tip node $i failed, retry $retryCount")
+                    }
+                }
+            }.awaitAll()
+        }
+        return result
     }
 
-    private suspend fun fetchTipNodeSigns(tipSigner: TipSigner, nodeSeed: ByteArray): String {
-        return ""
+    private suspend fun fetchTipNodeSigs(tipSigner: TipSigner, identityPub: ByteArray, ephemeral: String): String? {
+        val tipSignRequest = genTipSignRequest(tipSigner, identityPub, ephemeral)
+        return handleMixinResponse(
+            invokeNetwork = {
+                tipNodeService.postSign(tipSignRequest, tipSigner.api)
+            },
+            switchContext = Dispatchers.IO,
+            successBlock = {
+                val resp = it.data
+                requireNotNull(resp)
+                resp.signature
+            }
+        )
     }
 
-    private fun genRequestSignature(signerIdentity: String, identityPub: String, nonce: String, grace: String): String {
-        val m = identityPub.toByteArray() + signerIdentity.toByteArray() + nonce.toByteArray() + grace.toByteArray()
-        val sig = sign(m, identityPub.decodeBase64())
+    private fun genTipSignRequest(tipSigner: TipSigner, identityPub: ByteArray, ephemeral: String): TipSignRequest {
+        val nonce = currentTimeSeconds()
+        val grace = ephemeralGrace
+        val sig = genRequestSig(tipSigner.identity, identityPub, nonce, grace)
+        val data = TipSignData(tipSigner.identity, null, ephemeral, grace, nonce.toString(), null)
+        val dataJson = gson.toJson(data)
+        return TipSignRequest(sig, tipSigner.identity, dataJson)
+    }
+
+    private fun genRequestSig(signerIdentity: String, identityPub: ByteArray, nonce: Long, grace: String): String {
+        val m = identityPub + signerIdentity.toByteArray() + nonce.toLeByteArray() + grace.toByteArray()
+        val sig = sign(m, identityPub)
         return sig.compress().toHex()
     }
 }
