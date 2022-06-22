8c5b3e198d503dc0132fa68d04c7c9bacf690ea0
SeniorZhai <developer.zhaitao@gmail.com>
2022-06-22 16:14:10 +0800

    Select background

--- app/src/main/java/one/mixin/android/ui/setting/SettingChatFragment.kt
+++ app/src/main/java/one/mixin/android/ui/setting/SettingChatFragment.kt
@@ -1,20 +1,26 @@
 package one.mixin.android.ui.setting
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.DrawableRes
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import dagger.hilt.android.AndroidEntryPoint
 import one.mixin.android.R
 import one.mixin.android.databinding.FragmentSettingChatBinding
+import one.mixin.android.databinding.ItemBackgroudBinding
 import one.mixin.android.databinding.ItemChatTextBinding
 import one.mixin.android.databinding.ItemChatTimeBinding
 import one.mixin.android.extension.booleanFromAttribute
+import one.mixin.android.extension.dp
 import one.mixin.android.extension.nowInUtc
+import one.mixin.android.extension.round
 import one.mixin.android.ui.common.BaseFragment
 import one.mixin.android.ui.conversation.chathistory.holder.TextHolder
 import one.mixin.android.ui.conversation.holder.TimeHolder
@@ -29,6 +35,9 @@ class SettingChatFragment : BaseFragment(R.layout.fragment_setting_chat) {
     }
 
     private val binding by viewBinding(FragmentSettingChatBinding::bind)
+
+    private var currentSelected = 1
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         if (requireContext().booleanFromAttribute(R.attr.flag_night)) {
@@ -44,6 +53,38 @@ class SettingChatFragment : BaseFragment(R.layout.fragment_setting_chat) {
         binding.titleView.rightTv.setOnClickListener {
             // Todo save background
             requireActivity().onBackPressed()
+        }
+        binding.backgroundRv.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL,false)
+        binding.backgroundRv.adapter = object :RecyclerView.Adapter<BackgroundHolder>() {
+            override fun onCreateViewHolder(
+                parent: ViewGroup,
+                viewType: Int
+            ): BackgroundHolder {
+                return BackgroundHolder(
+                    ItemBackgroudBinding.inflate(
+                        LayoutInflater.from(parent.context),
+                        parent,
+                        false
+                    )
+                )
+            }
+
+            @SuppressLint("NotifyDataSetChanged")
+            override fun onBindViewHolder(holder: BackgroundHolder, position: Int) {
+                val p = position
+                holder.bind(R.drawable.bg_chat_night, position == currentSelected)
+                holder.itemView.setOnClickListener {
+                    currentSelected = p
+                    notifyDataSetChanged()
+                    binding.backgroundRv.layoutManager?.smoothScrollToPosition(
+                        binding.backgroundRv,
+                        null,
+                        position
+                    )
+                }
+            }
+
+            override fun getItemCount(): Int = 4
         }
         binding.chatRv.adapter = object : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
             override fun onCreateViewHolder(
@@ -128,6 +169,14 @@ class SettingChatFragment : BaseFragment(R.layout.fragment_setting_chat) {
             override fun getItemViewType(position: Int) = position
 
             override fun getItemCount(): Int = 3
+        }
+    }
+
+    class BackgroundHolder constructor(val binding: ItemBackgroudBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(@DrawableRes resId:Int,selected:Boolean) {
+            binding.image.setImageResource(resId)
+            binding.image.round(3.dp)
+            binding.selected.isVisible = selected
         }
     }
 }
