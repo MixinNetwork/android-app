4ccf4775b75e16eda3928c6c67800a80b770d30c
SeniorZhai <developer.zhaitao@gmail.com>
2022-06-29 15:47:49 +0800

    Replace camera featrure

--- app/src/main/java/one/mixin/android/widget/gallery/internal/utils/MediaStoreCompat.java
+++ app/src/main/java/one/mixin/android/widget/gallery/internal/utils/MediaStoreCompat.java
@@ -7,16 +7,17 @@ import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
-import androidx.fragment.app.Fragment;
 import androidx.core.content.FileProvider;
 import androidx.core.os.EnvironmentCompat;
+import androidx.fragment.app.Fragment;
+import one.mixin.android.widget.gallery.internal.entity.CaptureStrategy;
+
 import java.io.File;
 import java.io.IOException;
 import java.lang.ref.WeakReference;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
-import one.mixin.android.widget.gallery.internal.entity.CaptureStrategy;
 
 public class MediaStoreCompat {
 
@@ -38,7 +39,7 @@ public class MediaStoreCompat {
 
     public static boolean hasCameraFeature(Context context) {
         PackageManager pm = context.getApplicationContext().getPackageManager();
-        return pm.hasSystemFeature(PackageManager.FEATURE_CAMERA);
+        return pm.hasSystemFeature(PackageManager.FEATURE_CAMERA_ANY);
     }
 
     public void setCaptureStrategy(CaptureStrategy strategy) {
