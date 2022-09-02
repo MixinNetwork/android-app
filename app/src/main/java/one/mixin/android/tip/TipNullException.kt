ff8bbdce95ddf3273af506e81fb7087fde1f4a34
touge <touge1027@gmail.com>
2022-09-02 15:38:06 +0800

    Tweak tip exception

--- app/src/main/java/one/mixin/android/tip/TipNullException.kt
+++
@@ -1,7 +0,0 @@
-package one.mixin.android.tip
-
-class TipNullException(message: String) : TipException(message) {
-    companion object {
-        private const val serialVersionUID: Long = 1L
-    }
-}
