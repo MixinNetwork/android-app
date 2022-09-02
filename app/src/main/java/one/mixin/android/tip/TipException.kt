1af3c261e838d04121723fc994f5a1d1503d9270
touge <touge1027@gmail.com>
2022-09-02 15:38:06 +0800

    Tweak tip exception

--- app/src/main/java/one/mixin/android/tip/TipException.kt
+++ app/src/main/java/one/mixin/android/tip/TipException.kt
@@ -1,5 +1,7 @@
 package one.mixin.android.tip
 
+import one.mixin.android.api.ResponseError
+
 open class TipException : Exception {
 
     constructor(message: String) : super(message)
@@ -14,5 +16,17 @@ open class TipException : Exception {
 class PinIncorrectException : TipException() {
     companion object {
         private const val serialVersionUID: Long = 1L
+    }
+}
+
+class TipNullException(message: String) : TipException(message) {
+    companion object {
+        private const val serialVersionUID: Long = 1L
+    }
+}
+
+data class TipNetworkException(val error: ResponseError) : TipException(error.description) {
+    companion object {
+        private const val serialVersionUID = 1L
     }
 }
--- app/src/main/java/one/mixin/android/tip/TipNetworkException.kt
+++
@@ -1,9 +0,0 @@
-package one.mixin.android.tip
-
-import one.mixin.android.api.ResponseError
-
-data class TipNetworkException(val error: ResponseError) : TipException(error.description) {
-    companion object {
-        private const val serialVersionUID = 1L
-    }
-}
