3b806b8f77c01fb0b43b3723e998ea2557d3b42b
SeniorZhai <developer.zhaitao@gmail.com>
2022-09-14 12:54:32 +0800

    Scan view

---
+++ app/src/main/java/one/mixin/android/widget/compose/ScanView.kt
@@ -0,0 +1,90 @@
+
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.TileMode
+import androidx.compose.ui.graphics.drawscope.translate
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import one.mixin.android.R
+
+@Composable
+fun ScanView() {
+
+    val infiniteTransition = rememberInfiniteTransition()
+    val percent by infiniteTransition.animateFloat(
+        initialValue = 0f,
+        targetValue = 1f,
+        animationSpec = infiniteRepeatable(
+
+            animation = tween(
+                1500, easing = FastOutSlowInEasing
+            ), repeatMode = RepeatMode.Restart
+        )
+    )
+    val frameVector = ImageVector.vectorResource(id = R.drawable.scan_frame)
+    val framePainter = rememberVectorPainter(image = frameVector)
+
+    val gridVector = ImageVector.vectorResource(id = R.drawable.scan_grid)
+    val gridPainter = rememberVectorPainter(image = gridVector)
+    Canvas(
+        modifier = Modifier
+            .width(236.dp)
+            .aspectRatio(1f)
+            .clip(RoundedCornerShape(32.dp))
+    ) {
+        val canvasWidth = size.width
+        val canvasHeight = size.height
+        val offsetY = 3.dp.toPx()
+
+        val colorList: List<Color> =
+            listOf(Color.Transparent, Color(0x99aaffe0))
+        val startY = percent * canvasHeight
+        val brushHeight = canvasHeight / 4
+        val brush = Brush.verticalGradient(
+            colors = colorList,
+            startY = startY,
+            endY = startY + brushHeight,
+            tileMode = TileMode.Decal
+        )
+
+        drawRect(
+            brush = brush,
+            topLeft = Offset(offsetY, startY),
+            size = Size(canvasWidth - offsetY * 2, brushHeight)
+        )
+        translate(top = startY - brushHeight - offsetY, left = offsetY) {
+            with(gridPainter) {
+                draw(intrinsicSize)
+            }
+        }
+
+        with(framePainter) {
+            draw(intrinsicSize)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun ScanViewPreview() {
+    ScanView()
+}
