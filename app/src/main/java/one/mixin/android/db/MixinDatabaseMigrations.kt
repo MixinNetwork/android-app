d7714e82c3e1e581e2636b2cd46ff365c4e573f0
SeniorZhai <developer.zhaitao@gmail.com>
2022-09-21 16:13:59 +0800

    Migrate data

--- app/src/main/java/one/mixin/android/db/MixinDatabaseMigrations.kt
+++ app/src/main/java/one/mixin/android/db/MixinDatabaseMigrations.kt
@@ -346,6 +346,7 @@ class MixinDatabaseMigrations private constructor() {
         val MIGRATION_46_47: Migration = object : Migration(46, 47) {
             override fun migrate(database: SupportSQLiteDatabase) {
                 database.execSQL("CREATE TABLE IF NOT EXISTS `conversation_ext` (`conversation_id` TEXT NOT NULL, `count` INTEGER NOT NULL DEFAULT 0, PRIMARY KEY(`conversation_id`))")
+                database.execSQL("INSERT OR REPLACE INTO conversation_ext (`conversation_id`,`count`) SELECT conversation_id, count(*) FROM messages m INNER JOIN users u ON m.user_id = u.user_id GROUP BY conversation_id")
             }
         }
 
