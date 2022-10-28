e82c280ddd762778800c614bb26949900f7d5e1a
SeniorZhai <developer.zhaitao@gmail.com>
2022-10-28 21:10:08 +0800

    Auth bottom sheet

--- app/src/main/java/one/mixin/android/ui/auth/AuthBottomSheetDialogFragment.kt
+++ app/src/main/java/one/mixin/android/ui/auth/AuthBottomSheetDialogFragment.kt
@@ -2,48 +2,42 @@ package one.mixin.android.ui.auth
 
 import android.annotation.SuppressLint
 import android.app.Dialog
-import android.content.DialogInterface
 import android.content.Intent
 import android.net.Uri
 import android.os.Build
+import android.os.Bundle
+import android.view.Gravity
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
-import androidx.collection.ArraySet
-import androidx.lifecycle.lifecycleScope
-import androidx.recyclerview.widget.RecyclerView
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.core.view.doOnPreDraw
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 import one.mixin.android.R
-import one.mixin.android.api.handleMixinResponse
 import one.mixin.android.api.response.AuthorizationResponse
-import one.mixin.android.databinding.FragmentAuthBinding
-import one.mixin.android.databinding.ItemThirdLoginScopeBinding
+import one.mixin.android.extension.booleanFromAttribute
+import one.mixin.android.extension.dp
+import one.mixin.android.extension.isNightMode
 import one.mixin.android.extension.isWebUrl
-import one.mixin.android.extension.loadCircleImage
 import one.mixin.android.extension.withArgs
-import one.mixin.android.ui.common.MixinBottomSheetDialogFragment
-import one.mixin.android.ui.common.PinInputBottomSheetDialogFragment
-import one.mixin.android.util.viewBinding
+import one.mixin.android.util.SystemUIManager
 import one.mixin.android.vo.Scope
-import one.mixin.android.vo.Scope.Companion.SCOPES
-import one.mixin.android.vo.convertName
-import one.mixin.android.widget.BottomSheet
 import timber.log.Timber
 
 @AndroidEntryPoint
-class AuthBottomSheetDialogFragment : MixinBottomSheetDialogFragment() {
+class AuthBottomSheetDialogFragment : BottomSheetDialogFragment() {
 
     companion object {
         const val TAG = "AuthBottomSheetDialogFragment"
 
-        const val POS_TEXT = 0
-        const val POS_PB = 1
-
         const val ARGS_SCOPES = "args_scopes"
         const val ARGS_AUTHORIZATION = "args_authorization"
 
-        fun newInstance(scopes: ArrayList<Scope>, auth: AuthorizationResponse) =
+        fun newInstance(scopes: ArrayList<Scope>, auth: AuthorizationResponse?) =
             AuthBottomSheetDialogFragment().withArgs {
                 putParcelableArrayList(ARGS_SCOPES, scopes)
                 putParcelable(ARGS_AUTHORIZATION, auth)
@@ -58,87 +52,98 @@ class AuthBottomSheetDialogFragment : MixinBottomSheetDialogFragment() {
         requireArguments().getParcelable(ARGS_AUTHORIZATION)!!
     }
 
-    private val scopeAdapter: ScopeAdapter by lazy {
-        ScopeAdapter(scopes)
-    }
-
     private var success = false
 
-    private val binding by viewBinding(FragmentAuthBinding::inflate)
+    private var behavior: BottomSheetBehavior<*>? = null
+    override fun getTheme() = R.style.AppTheme_Dialog
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = ComposeView(requireContext()).apply {
+        setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+        setContent {
+            AuthBottomSheetDialogCompose(
+                name =
+                "Hacker News",
+                iconUrl = "https://mixin-images.zeromesh.net/wRNZyklATas2I_f7QqoZzzi3MQd8GhaEG9guYh3tfFL5xRNoPuVjSo9yDwqlRMv_2PSPPdIEI3Iqya6U2d_0HsY=s256",
+                scopes,
+                {
+
+                }, {
+
+                }
+            )
+        }
+        doOnPreDraw{
+            val params = (it.parent as View).layoutParams as? CoordinatorLayout.LayoutParams
+            behavior = params?.behavior as? BottomSheetBehavior<*>
+            behavior?.peekHeight = 560.dp
+            behavior?.isDraggable = false
+            behavior?.addBottomSheetCallback(bottomSheetBehaviorCallback)
+
+        }
+    }
 
     @SuppressLint("RestrictedApi")
     override fun setupDialog(dialog: Dialog, style: Int) {
-        super.setupDialog(dialog, style)
-        contentView = binding.root
-        dialog as BottomSheet
-        dialog.setCustomView(contentView)
+        super.setupDialog(dialog, R.style.MixinBottomSheet)
+        dialog.window?.let { window ->
+            SystemUIManager.lightUI(window, requireContext().isNightMode())
+        }
+        dialog.window?.setGravity(Gravity.BOTTOM)
+        dialog.window?.setLayout(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.MATCH_PARENT
+        )
+    }
 
-        binding.apply {
-            titleView.rightIv.setOnClickListener { dismiss() }
-            avatar.loadCircleImage(auth.app.iconUrl, R.mipmap.ic_launcher_round)
-            scopeRv.adapter = scopeAdapter
-            scopeAdapter.onScopeListener = object : OnScopeListener {
-                override fun onScope(binding: ItemThirdLoginScopeBinding, position: Int) {
-                    binding.cb.isChecked = !binding.cb.isChecked
-                    if (binding.cb.isChecked) {
-                        scopeAdapter.checkedScopes.add(scopes[position].name)
-                    } else {
-                        scopeAdapter.checkedScopes.remove(scopes[position].name)
-                    }
-                }
-            }
-            confirmAnim.setOnClickListener {
-                PinInputBottomSheetDialogFragment.newInstance().setOnPinComplete { pin ->
-                    confirmAnim.displayedChild = POS_PB
-                    confirmAnim.isEnabled = false
-                    lifecycleScope.launch {
-                        bottomViewModel // init on main thread
-                        handleMixinResponse(
-                            invokeNetwork = {
-                                bottomViewModel.authorize(auth.authorizationId, scopeAdapter.checkedScopes.toList(), pin)
-                            },
-                            switchContext = Dispatchers.IO,
-                            successBlock = {
-                                val data = it.data ?: return@handleMixinResponse
-                                val redirectUri = data.app.redirectUri
-                                redirect(redirectUri, data.authorization_code)
-                                success = true
-                                dismiss()
-                            },
-                            doAfterNetworkSuccess = {
-                                confirmAnim.displayedChild = POS_TEXT
-                                confirmAnim.isEnabled = true
-                            },
-                            exceptionBlock = {
-                                confirmAnim.displayedChild = POS_TEXT
-                                confirmAnim.isEnabled = true
-                                return@handleMixinResponse false
-                            }
-                        )
-                    }
-                }.showNow(parentFragmentManager, PinInputBottomSheetDialogFragment.TAG)
-            }
+    override fun onStart() {
+        super.onStart()
+        dialog?.window?.let { window ->
+            SystemUIManager.lightUI(
+                window,
+                !requireContext().booleanFromAttribute(R.attr.flag_night)
+            )
         }
     }
 
-    override fun onDismiss(dialog: DialogInterface) {
-        if (!success && isAdded) {
-            lifecycleScope.launch {
-                bottomViewModel // init on main thread
-                handleMixinResponse(
-                    invokeNetwork = {
-                        bottomViewModel.authorize(auth.authorizationId, listOf(), null)
-                    },
-                    switchContext = Dispatchers.IO,
-                    successBlock = {
-                        val data = it.data ?: return@handleMixinResponse
-                        redirect(data.app.redirectUri, data.authorization_code)
-                    }
-                )
+    override fun dismiss() {
+        dismissAllowingStateLoss()
+    }
+
+    private val bottomSheetBehaviorCallback = object : BottomSheetBehavior.BottomSheetCallback() {
+
+        override fun onStateChanged(bottomSheet: View, newState: Int) {
+            when (newState) {
+                BottomSheetBehavior.STATE_HIDDEN -> dismissAllowingStateLoss()
+                else -> {}
             }
         }
-        super.onDismiss(dialog)
+
+        override fun onSlide(bottomSheet: View, slideOffset: Float) {
+        }
     }
+
+    // override fun onDismiss(dialog: DialogInterface) {
+    //     if (!success && isAdded) {
+    //         lifecycleScope.launch {
+    //             bottomViewModel // init on main thread
+    //             handleMixinResponse(
+    //                 invokeNetwork = {
+    //                     bottomViewModel.authorize(auth.authorizationId, listOf(), null)
+    //                 },
+    //                 switchContext = Dispatchers.IO,
+    //                 successBlock = {
+    //                     val data = it.data ?: return@handleMixinResponse
+    //                     redirect(data.app.redirectUri, data.authorization_code)
+    //                 }
+    //             )
+    //         }
+    //     }
+    //     super.onDismiss(dialog)
+    // }
 
     private fun redirect(uri: String, code: String?) {
         if (!uri.isWebUrl()) {
@@ -160,36 +165,5 @@ class AuthBottomSheetDialogFragment : MixinBottomSheetDialogFragment() {
                 Timber.e(e)
             }
         }
-    }
-
-    private class ScopeAdapter(val scopes: List<Scope>) : RecyclerView.Adapter<ScopeViewHolder>() {
-        val checkedScopes = ArraySet<String>().apply {
-            addAll(scopes.map { it.name })
-        }
-        var onScopeListener: OnScopeListener? = null
-
-        override fun getItemCount(): Int = scopes.size
-
-        override fun onBindViewHolder(holder: ScopeViewHolder, position: Int) {
-            val scope = scopes[position]
-            holder.bind.title.text = scope.convertName(holder.itemView.context)
-            holder.bind.desc.text = scope.desc
-            holder.bind.cb.isChecked = checkedScopes.contains(scope.name)
-            if (scope.name == SCOPES[0]) {
-                holder.bind.cb.isEnabled = false
-            } else {
-                holder.bind.cb.isEnabled = true
-                holder.itemView.setOnClickListener { onScopeListener?.onScope(holder.bind, position) }
-            }
-        }
-
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ScopeViewHolder =
-            ScopeViewHolder(ItemThirdLoginScopeBinding.inflate(LayoutInflater.from(parent.context), parent, false))
-    }
-
-    class ScopeViewHolder(val bind: ItemThirdLoginScopeBinding) : RecyclerView.ViewHolder(bind.root)
-
-    interface OnScopeListener {
-        fun onScope(binding: ItemThirdLoginScopeBinding, position: Int)
     }
 }
