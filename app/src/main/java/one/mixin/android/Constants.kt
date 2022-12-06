3e8ebd33e7a72a5fca8fd6c0ac8a94c992560a2b
SeniorZhai <developer.zhaitao@gmail.com>
2022-12-06 15:05:24 +0800

    BSC chain name

--- app/src/main/java/one/mixin/android/Constants.kt
+++ app/src/main/java/one/mixin/android/Constants.kt
@@ -271,6 +271,7 @@ object Constants {
             "43d61dcd-e413-450d-80b8-101d5e903357" to "Ethereum (ERC-20)",
             "cbc77539-0a20-4666-8c8a-4ded62b36f0a" to "Avalanche X-Chain",
             "17f78d7c-ed96-40ff-980c-5dc62fecbc85" to "BNB Beacon Chain (BEP-2)",
+            "1949e683-6a08-49e2-b087-d6b72398588f" to "BNB Smart Chain (BEP-20)",
             "25dabac5-056a-48ff-b9f9-f67395dc407c" to "Tron (TRC-20)",
             "05891083-63d2-4f3d-bfbe-d14d7fb9b25a" to "BitShares"
         )
