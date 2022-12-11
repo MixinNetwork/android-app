00241f4a1fe310db8d1bd7423c3e88abd436d923
SeniorZhai <developer.zhaitao@gmail.com>
2022-12-11 18:26:26 +0800

    Flood flow

--- app/src/main/java/one/mixin/android/db/FloodMessageDao.kt
+++ app/src/main/java/one/mixin/android/db/FloodMessageDao.kt
@@ -3,13 +3,14 @@ package one.mixin.android.db
 import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
 import one.mixin.android.vo.FloodMessage
 
 @Dao
 interface FloodMessageDao : BaseDao<FloodMessage> {
 
     @Query("SELECT * FROM flood_messages ORDER BY created_at ASC limit 10")
-    suspend fun findFloodMessages(): List<FloodMessage>
+    fun findFloodMessages(): Flow<List<FloodMessage>>
 
     @Query("SELECT * FROM flood_messages ORDER BY created_at ASC limit 100")
     fun limit100(): List<FloodMessage>
