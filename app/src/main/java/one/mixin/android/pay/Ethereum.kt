afcc48a9ddbecf493dfd3a4253f9d789968061be
touge <touge1027@gmail.com>
2022-12-30 21:44:53 +0800

    Tweak amount usage

--- app/src/main/java/one/mixin/android/pay/Ethereum.kt
+++ app/src/main/java/one/mixin/android/pay/Ethereum.kt
@@ -3,10 +3,8 @@ package one.mixin.android.pay
 import one.mixin.android.Constants
 import one.mixin.android.api.response.AddressFeeResponse
 import one.mixin.android.pay.erc681.parseERC681
-import one.mixin.android.pay.erc681.scientificNumberRegEx
 import timber.log.Timber
 import java.math.BigDecimal
-import java.math.BigInteger
 
 data class EthereumURI(val uri: String)
 
@@ -22,57 +20,32 @@ internal suspend fun parseEthereum(
     val chainId = erc681.chainId?.toInt() ?: 1
     val assetId = ethereumChainIdMap[chainId] ?: return null
 
-    var value = erc681.value
+    val value = erc681.value
     var address: String? = null
+    var amount: BigDecimal? = null
     if (value == null) {
         if (erc681.function != "transfer") return null
 
         val fp = erc681.functionParams
-        var amountExists = false
-        // value > amount > uint256
         fp.forEach { pair ->
             if (pair.first == "address") {
                 address = pair.second
-            } else if (value == null) {
+            } else {
                 if (pair.first == "amount") {
-                    value = pair.second.toBigInteger()
-                    amountExists = true
-                } else if (!amountExists && pair.first == "uint256") {
-                    value = pair.second.toBigInteger()
+                    amount = BigDecimal(pair.second)
                 }
             }
         }
     } else {
         address = erc681.address
+        amount = Convert.fromWei(BigDecimal(value), Convert.Unit.ETHER)
     }
     val destination = address ?: return null
-    if (value == null) return null
+    val amountBD = amount ?: return null
 
-    val amount = Convert.fromWei(BigDecimal(value), Convert.Unit.ETHER)
     val addressFeeResponse = getAddressFee(assetId, destination) ?: return null
 
-    return ExternalTransfer(addressFeeResponse.destination, amount, assetId, addressFeeResponse.fee.toBigDecimalOrNull())
-}
-
-internal fun String?.toBigInteger(): BigInteger? {
-    if (this == null) {
-        return null
-    }
-
-    if (!scientificNumberRegEx.matches(this)) {
-        return null
-    }
-
-    return when {
-        contains("e") -> {
-            val split = split("e")
-            BigDecimal(split.first()).multiply(BigDecimal.TEN.pow(split[1].toIntOrNull() ?: 1)).toBigInteger()
-        }
-        contains(".") -> {
-            null
-        }
-        else -> BigInteger(this)
-    }
+    return ExternalTransfer(addressFeeResponse.destination, amountBD, assetId, addressFeeResponse.fee.toBigDecimalOrNull())
 }
 
 private val ethereumChainIdMap by lazy {
