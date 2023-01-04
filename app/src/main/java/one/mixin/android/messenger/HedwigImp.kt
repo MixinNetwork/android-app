b2dcf411551a7ea007e79d51f91a8c1e3621db0e
SeniorZhai <developer.zhaitao@gmail.com>
2023-01-04 18:23:36 +0800

    Sync conversation (#3485)

--- app/src/main/java/one/mixin/android/messenger/HedwigImp.kt
+++ app/src/main/java/one/mixin/android/messenger/HedwigImp.kt
@@ -4,25 +4,41 @@ import androidx.room.InvalidationTracker
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
+import one.mixin.android.api.service.CircleService
+import one.mixin.android.api.service.ConversationService
 import one.mixin.android.db.MixinDatabase
+import one.mixin.android.db.insertUpdate
 import one.mixin.android.db.pending.PendingDatabase
 import one.mixin.android.job.DecryptCallMessage
 import one.mixin.android.job.DecryptMessage
+import one.mixin.android.job.MixinJobManager
+import one.mixin.android.job.RefreshUserJob
 import one.mixin.android.job.pendingMessageStatusMap
+import one.mixin.android.session.Session
 import one.mixin.android.util.FLOOD_THREAD
 import one.mixin.android.util.GsonHelper
 import one.mixin.android.util.PENDING_DB_THREAD
 import one.mixin.android.util.chat.InvalidateFlow
 import one.mixin.android.vo.CallStateLiveData
+import one.mixin.android.vo.Conversation
+import one.mixin.android.vo.ConversationCategory
+import one.mixin.android.vo.ConversationStatus
 import one.mixin.android.vo.MessageStatus
+import one.mixin.android.vo.Participant
+import one.mixin.android.vo.ParticipantSession
 import one.mixin.android.vo.RemoteMessageStatus
+import one.mixin.android.vo.SYSTEM_USER
 import one.mixin.android.vo.isMine
 import one.mixin.android.websocket.BlazeMessageData
 import timber.log.Timber
+import java.io.IOException
 
 class HedwigImp(
     private val mixinDatabase: MixinDatabase,
     private val pendingDatabase: PendingDatabase,
+    private val conversationService: ConversationService,
+    private val circleService: CircleService,
+    private val jobManager: MixinJobManager,
     private val callState: CallStateLiveData,
     private val lifecycleScope: CoroutineScope,
 ) : Hedwig {
@@ -45,6 +61,18 @@ class HedwigImp(
 
     private val conversationDao by lazy {
         mixinDatabase.conversationDao()
+    }
+    private val participantDao by lazy {
+        mixinDatabase.participantDao()
+    }
+    private val participantSessionDao by lazy {
+        mixinDatabase.participantSessionDao()
+    }
+    private val circleConversationDao by lazy {
+        mixinDatabase.circleConversationDao()
+    }
+    private val circleDao by lazy {
+        mixinDatabase.circleDao()
     }
 
     private val conversationExtDao by lazy {
@@ -139,25 +167,23 @@ class HedwigImp(
                 messageDao.insertList(list)
                 pendingDatabase.deletePendingMessageByIds(list.map { it.messageId })
                 list.groupBy { it.conversationId }.forEach { (conversationId, messages) ->
-                    conversationExtDao.increment(conversationId, messages.size)
-                    messages.filter { message ->
-                        !message.isMine() && message.status != MessageStatus.READ.name
-                    }.map { message ->
-                        RemoteMessageStatus(
-                            message.messageId,
-                            message.conversationId,
-                            MessageStatus.DELIVERED.name,
-                        )
-                    }.let { remoteMessageStatus ->
-                        remoteMessageStatusDao.insertList(remoteMessageStatus)
+                    if (conversationId == SYSTEM_USER || conversationId == Session.getAccountId() || checkConversation(conversationId) != null) {
+                        checkConversation(conversationId) ?: return@forEach
+                        conversationExtDao.increment(conversationId, messages.size)
+                        messages.filter { message ->
+                            !message.isMine() && message.status != MessageStatus.READ.name
+                        }.map { message ->
+                            RemoteMessageStatus(message.messageId, message.conversationId, MessageStatus.DELIVERED.name)
+                        }.let { remoteMessageStatus ->
+                            remoteMessageStatusDao.insertList(remoteMessageStatus)
+                        }
+                        messages.last().let { message ->
+                            conversationDao.updateLastMessageId(message.messageId, message.createdAt, message.conversationId)
+                        }
+                        remoteMessageStatusDao.updateConversationUnseen(conversationId)
+                        InvalidateFlow.emit(conversationId)
                     }
-                    messages.last().let { message ->
-                        conversationDao.updateLastMessageId(message.messageId, message.createdAt, message.conversationId)
-                    }
-                    remoteMessageStatusDao.updateConversationUnseen(conversationId)
-                    InvalidateFlow.emit(conversationId)
                 }
-
                 if (list.size == 100) {
                     runPendingJob()
                 }
@@ -165,6 +191,83 @@ class HedwigImp(
                 Timber.e(e)
                 runPendingJob()
             }
+        }
+    }
+
+    private fun checkConversation(conversationId: String): Conversation? {
+        var conversation = conversationDao.findConversationById(conversationId)
+        if (conversation == null) conversation = refreshConversation(conversationId)
+        return conversation
+    }
+
+    private fun refreshConversation(conversationId: String): Conversation? {
+        try {
+            val call = conversationService.getConversation(conversationId).execute()
+            val response = call.body()
+            if (response != null && response.isSuccess) {
+                response.data?.let { conversationData ->
+                    val status = if (conversationData.participants.find { Session.getAccountId() == it.userId } != null) {
+                        ConversationStatus.SUCCESS.ordinal
+                    } else {
+                        ConversationStatus.QUIT.ordinal
+                    }
+                    var ownerId: String = conversationData.creatorId
+                    if (conversationData.category == ConversationCategory.CONTACT.name) {
+                        ownerId = conversationData.participants.find { it.userId != Session.getAccountId() }!!.userId
+                    } else if (conversationData.category == ConversationCategory.GROUP.name) {
+                        jobManager.addJobInBackground(RefreshUserJob(listOf(conversationData.creatorId)))
+                    }
+                    conversationDao.updateConversation(
+                        conversationData.conversationId,
+                        ownerId,
+                        conversationData.category,
+                        conversationData.name,
+                        conversationData.announcement,
+                        conversationData.muteUntil,
+                        conversationData.createdAt,
+                        conversationData.expireIn,
+                        status,
+                    )
+                    val remote = mutableListOf<Participant>()
+                    val conversationUserIds = mutableListOf<String>()
+                    for (p in conversationData.participants) {
+                        remote.add(Participant(conversationId, p.userId, p.role, p.createdAt!!))
+                        conversationUserIds.add(p.userId)
+                    }
+                    participantDao.replaceAll(conversationId, remote)
+
+                    if (conversationUserIds.isNotEmpty()) {
+                        jobManager.addJobInBackground(RefreshUserJob(conversationUserIds, conversationId))
+                    }
+
+                    val sessionParticipants = conversationData.participantSessions?.map {
+                        ParticipantSession(conversationId, it.userId, it.sessionId, publicKey = it.publicKey)
+                    }
+                    sessionParticipants?.let {
+                        participantSessionDao.replaceAll(conversationId, it)
+                    }
+
+                    conversationData.circles?.let { circles ->
+                        circles.forEach {
+                            val circle = circleDao.findCircleById(it.circleId)
+                            if (circle == null) {
+                                val circleResponse = circleService.getCircle(it.circleId).execute().body()
+                                if (circleResponse?.isSuccess == true) {
+                                    circleResponse.data?.let { item ->
+                                        circleDao.insert(item)
+                                    }
+                                }
+                            }
+                            circleConversationDao.insertUpdate(it)
+                        }
+                    }
+                }
+                return conversationDao.findConversationById(conversationId)
+            } else {
+                return null
+            }
+        } catch (_: IOException) {
+            return null
         }
     }
 }
