e50dac233535c53694ea2b69dddf6dd29fc77a2d
touge <touge1027@gmail.com>
2023-01-13 17:21:05 +0800

    Home search asset item show chain label

--- app/src/main/java/one/mixin/android/ui/wallet/adapter/SearchDefaultAdapter.kt
+++ app/src/main/java/one/mixin/android/ui/wallet/adapter/SearchDefaultAdapter.kt
@@ -4,11 +4,9 @@ import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.text.isDigitsOnly
 import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
 import com.timehop.stickyheadersrecyclerview.StickyRecyclerHeadersAdapter
-import one.mixin.android.Constants
 import one.mixin.android.R
 import one.mixin.android.databinding.ItemContactHeaderBinding
 import one.mixin.android.databinding.ItemWalletSearchBinding
@@ -16,6 +14,7 @@ import one.mixin.android.extension.loadImage
 import one.mixin.android.extension.numberFormat2
 import one.mixin.android.extension.priceFormat
 import one.mixin.android.extension.textColorResource
+import one.mixin.android.util.getChainNetwork
 import one.mixin.android.vo.AssetItem
 import one.mixin.android.vo.Fiats
 import one.mixin.android.vo.TopAssetItem
@@ -171,36 +170,6 @@ class TopAssetHolder(binding: ItemWalletSearchBinding) : ItemViewHolder(binding)
             callback?.onAssetClick(asset.assetId)
         }
     }
-}
-
-private val chainNetworks by lazy {
-    mapOf(
-        "43d61dcd-e413-450d-80b8-101d5e903357" to "ERC-20",
-        "17f78d7c-ed96-40ff-980c-5dc62fecbc85" to "BEP-2",
-        "1949e683-6a08-49e2-b087-d6b72398588f" to "BEP-20",
-        "6cfe566e-4aad-470b-8c9a-2fd35b49c68d" to "EOS",
-        "b7938396-3f94-4e0a-9179-d3440718156f" to "Polygon",
-    )
-}
-
-private val bepChains by lazy {
-    arrayOf(
-        "17f78d7c-ed96-40ff-980c-5dc62fecbc85",
-        "1949e683-6a08-49e2-b087-d6b72398588f",
-    )
-}
-
-private fun getChainNetwork(assetId: String, chainId: String, assetKey: String?): String? {
-    if (assetId == chainId && !bepChains.contains(chainId)) return null
-
-    if (chainId == Constants.ChainId.TRON_CHAIN_ID) {
-        return if (assetKey?.isDigitsOnly() == true) {
-            "TRC-10"
-        } else {
-            "TRC-20"
-        }
-    }
-    return chainNetworks[chainId]
 }
 
 interface WalletSearchCallback {
