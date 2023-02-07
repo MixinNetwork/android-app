a8985c3e4c323a8021b394abb9d76f9b2c5a2703
SeniorZhai <developer.zhaitao@gmail.com>
2023-02-07 15:45:16 +0800

    Fts raw sql

--- app/src/main/java/one/mixin/android/job/MessageFtsDeleteJob.kt
+++ app/src/main/java/one/mixin/android/job/MessageFtsDeleteJob.kt
@@ -7,8 +7,6 @@ import one.mixin.android.db.deleteFtsByMessageIds
 class MessageFtsDeleteJob(private val messageIds: List<String>) :
     BaseJob(Params(PRIORITY_LOWER).addTags(GROUP).groupBy("message_delete").persist()) {
 
-    private val TAG = MessageFtsDeleteJob::class.java.simpleName
-
     companion object {
         const val GROUP = "MessageFtsDeleteJob"
         private const val serialVersionUID = 1L
@@ -16,7 +14,7 @@ class MessageFtsDeleteJob(private val messageIds: List<String>) :
 
     override fun onRun() = runBlocking {
         messageIds.chunked(100).forEach {
-            deleteFtsByMessageIds(it)
+            messageFts4Dao.deleteFtsByMessageIds(it)
         }
     }
 }
