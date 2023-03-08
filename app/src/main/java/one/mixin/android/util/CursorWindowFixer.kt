1c52fa4073315fc093a760aaec7cfc4797c479b1
Crossle Song <crosslesong@gmail.com>
2023-03-08 10:14:46 +0400

    fix CursorWindowFixer size

--- app/src/main/java/one/mixin/android/util/CursorWindowFixer.kt
+++ app/src/main/java/one/mixin/android/util/CursorWindowFixer.kt
@@ -1,5 +1,6 @@
 package one.mixin.android.util
 
+import android.annotation.SuppressLint
 import android.app.ActivityManager
 import android.app.ActivityManager.MemoryInfo
 import android.content.Context
@@ -12,21 +13,22 @@ import kotlin.math.min
 
 class CursorWindowFixer {
     companion object {
+        @SuppressLint("DiscouragedPrivateApi")
         fun fix(context: Context) {
             try {
                 val field: Field = CursorWindow::class.java.getDeclaredField("sCursorWindowSize")
                 field.isAccessible = true
-                field.set(null, getCursorWindowSize(context))
+                field.set(null, getCursorWindowSize(context) * 1024 * 1024)
             } catch (e: Exception) {
                 Timber.e(e)
             }
         }
 
         private fun getCursorWindowSize(context: Context): Int {
-            val activityManager = context.getSystemService<ActivityManager>() ?: return 10 * 1024 * 1024
+            val activityManager = context.getSystemService<ActivityManager>() ?: return 10
             val memoryInfo = MemoryInfo()
             activityManager.getMemoryInfo(memoryInfo)
-            val memorySize = max(memoryInfo.totalMem / 1024 / 1024 / 1024, 1)
+            val memorySize = max(memoryInfo.totalMem / 1024 / 1024 / 1024, 2)
             return min(100, (memorySize * 12.5).toInt()) // 8G Memory set window size to 100MB, 1G Memory set to 12.5MB
         }
     }
