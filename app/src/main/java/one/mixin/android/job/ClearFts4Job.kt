bb9e8a6f3a79b6f189f212bcb8b841993279e2de
SeniorZhai <developer.zhaitao@gmail.com>
2023-03-14 18:19:28 +0800

    Disable fts clear

--- app/src/main/java/one/mixin/android/job/ClearFts4Job.kt
+++ app/src/main/java/one/mixin/android/job/ClearFts4Job.kt
@@ -3,6 +3,7 @@ package one.mixin.android.job
 import com.birbit.android.jobqueue.Params
 import kotlinx.coroutines.runBlocking
 import one.mixin.android.util.PropertyHelper
+import one.mixin.android.util.SINGLE_DB_THREAD
 import timber.log.Timber
 
 class ClearFts4Job :
@@ -13,7 +14,7 @@ class ClearFts4Job :
         const val FTS_CLEAR = "fts_clear"
     }
 
-    override fun onRun() = runBlocking {
+    override fun onRun() = runBlocking(SINGLE_DB_THREAD) {
         val count = messageDao.deleteFts()
         if (count > 0) {
             jobManager.addJobInBackground(ClearFts4Job())
