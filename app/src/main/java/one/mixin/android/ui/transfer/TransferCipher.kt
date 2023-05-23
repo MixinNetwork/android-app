dbb9056c5e29a00124a902c10b1995018b398c30
SeniorZhai <developer.zhaitao@gmail.com>
2023-05-23 13:59:07 +0800

    Code clean

--- app/src/main/java/one/mixin/android/ui/transfer/TransferCipher.kt
+++ app/src/main/java/one/mixin/android/ui/transfer/TransferCipher.kt
@@ -4,10 +4,11 @@ import one.mixin.android.crypto.Util
 import org.whispersystems.libsignal.kdf.HKDFv3
 
 object TransferCipher {
+    private val INFO = "Mixin Device Transfer".toByteArray()
     fun generateKey(): ByteArray {
         return HKDFv3().deriveSecrets(
             Util.getSecretBytes(32),
-            "Mixin  Device Transfer".toByteArray(),
+            INFO,
             64
         )
     }
