f738e8f8a62b35125256a0b734f3abffec29f092
SeniorZhai <developer.zhaitao@gmail.com>
2023-06-12 18:44:30 +0800

    Update

--- app/src/main/java/one/mixin/android/ui/transfer/compose/SelectDatePage.kt
+++ app/src/main/java/one/mixin/android/ui/transfer/compose/SelectDatePage.kt
@@ -1,12 +1,13 @@
 package one.mixin.android.ui.transfer.compose
 
+import androidx.annotation.ColorRes
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutVertically
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -14,15 +15,17 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
+import androidx.compose.material.OutlinedButton
 import androidx.compose.material.ProvideTextStyle
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
@@ -35,21 +38,23 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.Layout
-import androidx.compose.ui.layout.Placeable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Constraints
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.zIndex
 import one.mixin.android.R
 import one.mixin.android.ui.setting.ui.compose.MixinTopAppBar
 import one.mixin.android.ui.setting.ui.theme.MixinAppTheme
-import kotlin.math.max
+import org.whispersystems.libsignal.logging.Log
 
 @Composable
 fun SelectDatePage(onClick: () -> Unit) {
@@ -216,67 +221,79 @@ fun YearMothSwitch() {
             maxLines = 1,
         )
         Spacer(modifier = Modifier.width(4.dp))
-        Layout(
-            modifier = Modifier
-                .wrapContentWidth()
-                .clip(RoundedCornerShape(4.dp))
-                .background(MixinAppTheme.colors.shadow)
-                .padding(2.dp)
-                .clickable(
-                    interactionSource = remember { MutableInteractionSource() },
-                    onClick =
-                    {
-                        booleanValue = !booleanValue
-                    },
-                    indication = null,
-                ),
-            content = {
-                Box(
-                    modifier = Modifier
-                        .clip(RoundedCornerShape(4.dp))
-                        .background(MixinAppTheme.colors.shadow),
-                )
-                Text(
-                    modifier = Modifier.padding(horizontal = 4.dp, vertical = 4.dp),
-                    text = stringResource(id = R.string.yrs),
-                    textAlign = TextAlign.Center,
-                    color = MixinAppTheme.colors.textPrimary,
-                )
-                Text(
-                    modifier = Modifier.padding(horizontal = 4.dp, vertical = 4.dp),
-                    text = stringResource(id = R.string.mos),
-                    textAlign = TextAlign.Center,
-                    color = MixinAppTheme.colors.textPrimary,
-                )
-            },
-        ) { measurables, constraints ->
-            val secondPlaceable: Placeable = measurables[1].measure(constraints)
-            val thirdPlaceable: Placeable = measurables[2].measure(constraints)
-            val width = max(secondPlaceable.width, thirdPlaceable.width)
-            val height = max(secondPlaceable.height, thirdPlaceable.height)
-            val itemConstraints = Constraints.fixed(
-                width,
-                height,
-            )
-            val firstPlaceable = measurables[0].measure(itemConstraints)
-            val offset = (width - secondPlaceable.width) / 2
+        val genders2 =
+            listOf(stringResource(id = R.string.Year), stringResource(id = R.string.Month))
+        SegmentedControl(
+            items = genders2,
+            defaultSelectedItemIndex = 0,
+            cornerRadius = 8,
+            color = R.color.colorAccent
+        ) {
+            Log.e("CustomToggle", "Selected item : ${genders2[it]}")
+        }
+    }
+}
 
-            layout(width * 2 + 8.dp.toPx().toInt(), height) {
-                if (booleanValue) {
-                    firstPlaceable.placeRelative(x = width + 8.dp.toPx().toInt(), y = 0)
+@Composable
+fun SegmentedControl(
+    items: List<String>,
+    defaultSelectedItemIndex: Int = 0,
+    cornerRadius: Int = 10,
+    @ColorRes color: Int = R.color.colorAccent,
+    onItemSelection: (selectedItemIndex: Int) -> Unit
+) {
+    val selectedIndex = remember { mutableStateOf(defaultSelectedItemIndex) }
+
+    Row(
+        modifier = Modifier
+    ) {
+        items.forEachIndexed { index, item ->
+            OutlinedButton(
+                modifier = when (index) {
+                    0 -> {
+                        Modifier
+                            .padding(1.dp)
+                            .wrapContentSize()
+                            .offset(0.dp, 0.dp)
+                            .zIndex(if (selectedIndex.value == index) 1f else 0f)
+                    }
+
+                    else -> {
+                        Modifier
+                            .padding(1.dp)
+                            .wrapContentSize()
+                            .offset((-1 * index).dp, 0.dp)
+                            .zIndex(if (selectedIndex.value == index) 1f else 0f)
+                    }
+                },
+                onClick = {
+                    selectedIndex.value = index
+                    onItemSelection(selectedIndex.value)
+                },
+                border = null,
+                shape = RoundedCornerShape(
+                        topStartPercent = cornerRadius,
+                        topEndPercent = cornerRadius,
+                        bottomStartPercent = cornerRadius,
+                        bottomEndPercent = cornerRadius),
+                colors = if (selectedIndex.value == index) {
+                    ButtonDefaults.outlinedButtonColors(
+                        backgroundColor = MixinAppTheme.colors.shadow
+                    )
                 } else {
-                    firstPlaceable.placeRelative(x = 0, y = 0)
-                }
-                if (offset > 0) {
-                    secondPlaceable.placeRelative(x = offset, y = 0)
+                    ButtonDefaults.outlinedButtonColors(backgroundColor = Color.Transparent)
+                },
+            ) {
+                val modifier = if(selectedIndex.value == index){
+                    Modifier.background(color = MixinAppTheme.colors.background, shape = RoundedCornerShape(8.dp))
                 } else {
-                    secondPlaceable.placeRelative(x = 0, y = 0)
+                    Modifier
                 }
-                if (offset < 0) {
-                    thirdPlaceable.placeRelative(x = width + 8.dp.toPx().toInt() - offset, y = 0)
-                } else {
-                    thirdPlaceable.placeRelative(x = width + 8.dp.toPx().toInt(), y = 0)
-                }
+                Text(
+                    modifier = modifier,
+                    text = item,
+                    color = MixinAppTheme.colors.textPrimary,
+                )
             }
         }
     }
@@ -285,6 +302,5 @@ fun YearMothSwitch() {
 @Composable
 @Preview
 fun SelectDatePagePreview() {
-    SelectDatePage(onClick = {
-    })
+    SelectDatePage(onClick = {})
 }
