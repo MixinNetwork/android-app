35da31bc1418c93ff5c2307c401ce8bc94aab1d6
touge <touge1027@gmail.com>
2023-06-13 20:07:26 +0800

    Use go ed25519 for 7.x

--- app/tip-go/crypto/key.go
+++ app/tip-go/crypto/key.go
@@ -11,3 +11,15 @@ func VerifyEd25519(message, sig, pub []byte) bool {
 	pk := ed25519.PublicKey(pub)
 	return ed25519.Verify(pk, message, sig)
 }
+
+func GenerateKey() []byte {
+	_, priv, err := ed25519.GenerateKey(nil)
+	if err != nil {
+		panic(err)
+	}
+	return priv
+}
+
+func NewKeyFromSeed(seed []byte) []byte {
+	return ed25519.NewKeyFromSeed(seed)
+}
