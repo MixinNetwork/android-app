9dcc467df5225605bf48226d8860a3be8cbdaee2
touge <touge1027@gmail.com>
2023-06-20 19:55:40 +0800

    Not use data class

--- app/src/main/java/one/mixin/android/crypto/EdKeyPair.kt
+++ app/src/main/java/one/mixin/android/crypto/EdKeyPair.kt
@@ -1,22 +1,6 @@
 package one.mixin.android.crypto
 
-data class EdKeyPair(
+class EdKeyPair(
     val publicKey: ByteArray,
     val privateKey: ByteArray, // 32 bytes
-) {
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as EdKeyPair
-
-        if (!publicKey.contentEquals(other.publicKey)) return false
-        return privateKey.contentEquals(other.privateKey)
-    }
-
-    override fun hashCode(): Int {
-        var result = publicKey.contentHashCode()
-        result = 31 * result + privateKey.contentHashCode()
-        return result
-    }
-}
+)
