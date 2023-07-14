7dc6b2a70db25fd29a28642919d4fe014e937449
SeniorZhai <developer.zhaitao@gmail.com>
2023-07-14 15:03:18 +0800

    Fix share media

--- app/src/main/java/one/mixin/android/ui/media/SharedMediaViewModel.kt
+++ app/src/main/java/one/mixin/android/ui/media/SharedMediaViewModel.kt
@@ -25,6 +25,7 @@ import one.mixin.android.vo.isEncrypted
 import one.mixin.android.vo.isImage
 import one.mixin.android.vo.isSignal
 import one.mixin.android.vo.isVideo
+import org.threeten.bp.ZonedDateTime
 import javax.inject.Inject
 
 @HiltViewModel
@@ -46,34 +47,33 @@ class SharedMediaViewModel @Inject constructor(
 
     fun getAudioMessages(conversationId: String): LiveData<PagedList<MessageItem>> {
         val dataSource = conversationRepository.getAudioMessages(conversationId)
-        // Todo
-        // val sortedDataSource = dataSource.mapByPage { list ->
-        //     list.sortWith(
-        //         Comparator<MessageItem> { o1, o2 ->
-        //             if (o1 == null || o2 == null) return@Comparator 0
-        //
-        //             val time1 = ZonedDateTime.parse(o1.createdAt)
-        //             val time2 = ZonedDateTime.parse(o2.createdAt)
-        //             val year1 = time1.year
-        //             val year2 = time2.year
-        //             val day1 = time1.dayOfYear
-        //             val day2 = time2.dayOfYear
-        //             if (year1 == year2) {
-        //                 if (day1 == day2) {
-        //                     return@Comparator time1.toOffsetDateTime()
-        //                         .compareTo(time2.toOffsetDateTime())
-        //                 } else {
-        //                     return@Comparator day2 - day1
-        //                 }
-        //             } else {
-        //                 return@Comparator year2 - year1
-        //             }
-        //         },
-        //     )
-        //     return@mapByPage list
-        // }
+        val sortedDataSource = dataSource.mapByPage { list ->
+            list.toMutableList().sortWith(
+                Comparator<MessageItem> { o1, o2 ->
+                    if (o1 == null || o2 == null) return@Comparator 0
+
+                    val time1 = ZonedDateTime.parse(o1.createdAt)
+                    val time2 = ZonedDateTime.parse(o2.createdAt)
+                    val year1 = time1.year
+                    val year2 = time2.year
+                    val day1 = time1.dayOfYear
+                    val day2 = time2.dayOfYear
+                    if (year1 == year2) {
+                        if (day1 == day2) {
+                            return@Comparator time1.toOffsetDateTime()
+                                .compareTo(time2.toOffsetDateTime())
+                        } else {
+                            return@Comparator day2 - day1
+                        }
+                    } else {
+                        return@Comparator year2 - year1
+                    }
+                },
+            )
+            return@mapByPage list
+        }
         return LivePagedListBuilder(
-            dataSource,
+            sortedDataSource,
             PagedList.Config.Builder()
                 .setPrefetchDistance(PAGE_SIZE * 2)
                 .setPageSize(PAGE_SIZE)
