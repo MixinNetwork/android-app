850e23153a49af77516e73add2194562d2eb2e37
SeniorZhai <developer.zhaitao@gmail.com>
2023-07-28 12:09:34 +0800

    Update converter

--- app/src/main/java/one/mixin/android/db/converter/WithdrawalMemoPossibilityConverter.kt
+++ app/src/main/java/one/mixin/android/db/converter/WithdrawalMemoPossibilityConverter.kt
@@ -5,10 +5,14 @@ import one.mixin.android.vo.WithdrawalMemoPossibility
 
 class WithdrawalMemoPossibilityConverter {
     @TypeConverter
-    fun revertDate(value: String): WithdrawalMemoPossibility {
+    fun revertDate(value: String?): WithdrawalMemoPossibility? {
+        if (value == null) return null
         return requireNotNull(WithdrawalMemoPossibility.values().firstOrNull { it.value == value })
     }
 
     @TypeConverter
-    fun converterDate(status: WithdrawalMemoPossibility) = status.value
+    fun converterDate(status: WithdrawalMemoPossibility?): String? {
+        return status?.value
+    }
+
 }
