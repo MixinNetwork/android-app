c204fd46ab0cc7a10b0e13ff1df5462268a182c3
touge <touge1027@gmail.com>
2023-08-01 14:53:50 +0800

    Add fiat list bottom sheet

---
+++ app/src/main/java/one/mixin/android/ui/setting/CurrencyAdapter.kt
@@ -0,0 +1,79 @@
+package one.mixin.android.ui.setting
+
+import android.content.res.Resources
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.view.isInvisible
+import androidx.core.view.isVisible
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import one.mixin.android.R
+import one.mixin.android.databinding.ItemCurrencyBinding
+import one.mixin.android.session.Session
+
+class CurrencyAdapter : ListAdapter<Currency, CurrencyHolder>(Currency.DIFF_CALLBACK) {
+    var currencyListener: OnCurrencyListener? = null
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
+        CurrencyHolder(ItemCurrencyBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+
+    override fun onBindViewHolder(holder: CurrencyHolder, position: Int) {
+        getItem(position)?.let { holder.bind(it, currencyListener) }
+    }
+}
+
+class CurrencyHolder(private val itemBinding: ItemCurrencyBinding) : RecyclerView.ViewHolder(itemBinding.root) {
+    fun bind(currency: Currency, listener: OnCurrencyListener?) {
+        itemBinding.apply {
+            if (currency.name == Session.getFiatCurrency()) {
+                checkIv.isVisible = true
+            } else {
+                checkIv.isInvisible = true
+            }
+            flagIv.setImageResource(currency.flag)
+            name.text = itemView.context.getString(R.string.wallet_setting_currency_desc, currency.name, currency.symbol)
+        }
+        itemView.setOnClickListener { listener?.onClick(currency) }
+    }
+}
+
+interface OnCurrencyListener {
+    fun onClick(currency: Currency)
+}
+
+data class Currency(
+    val name: String,
+    val symbol: String,
+    val flag: Int,
+) {
+    companion object {
+        val DIFF_CALLBACK = object : DiffUtil.ItemCallback<Currency>() {
+            override fun areItemsTheSame(oldItem: Currency, newItem: Currency) =
+                oldItem.name == newItem.name
+
+            override fun areContentsTheSame(oldItem: Currency, newItem: Currency) =
+                oldItem == newItem
+        }
+    }
+}
+
+fun getCurrencyData(resources: Resources): List<Currency> {
+    val names = resources.getStringArray(R.array.currency_names)
+    val symbols = resources.getStringArray(R.array.currency_symbols).map { it.trim() }
+    val flagArray = resources.obtainTypedArray(R.array.currency_flags)
+    val flags = arrayListOf<Int>()
+    for (i in names.indices) {
+        flags.add(flagArray.getResourceId(i, 0))
+    }
+    flagArray.recycle()
+
+    if (flags.size != names.size || flags.size != symbols.size) {
+        return emptyList()
+    }
+    val currencies = arrayListOf<Currency>()
+    for (i in 0 until flags.size) {
+        currencies.add(Currency(names[i], symbols[i], flags[i]))
+    }
+    return currencies
+}
--- app/src/main/java/one/mixin/android/ui/wallet/WalletFragment.kt
+++ app/src/main/java/one/mixin/android/ui/wallet/WalletFragment.kt
@@ -32,7 +32,6 @@ import one.mixin.android.extension.defaultSharedPreferences
 import one.mixin.android.extension.dp
 import one.mixin.android.extension.dpToPx
 import one.mixin.android.extension.mainThread
-import one.mixin.android.extension.navTo
 import one.mixin.android.extension.navigate
 import one.mixin.android.extension.numberFormat2
 import one.mixin.android.extension.numberFormat8
