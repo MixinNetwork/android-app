d672f6d36f688e32e65d305298519e95ca84db3c
SeniorZhai <developer.zhaitao@gmail.com>
2023-08-02 11:35:50 +0800

    Update theme

--- app/src/main/java/one/mixin/android/ui/wallet/CustomPaymentFormTheme.kt
+++ app/src/main/java/one/mixin/android/ui/wallet/CustomPaymentFormTheme.kt
@@ -5,13 +5,13 @@ import com.checkout.frames.style.theme.PaymentFormThemeColors
 
 object CustomPaymentFormTheme {
     private val paymentFormThemeColors = PaymentFormThemeColors(
-        accentColor = 0XFF00CC2D,
-        textColor = 0XFFB1B1B1,
-        errorColor = 0XFFFF0000,
-        backgroundColor = 0xFF17201E,
-        fieldBackgroundColor = 0XFF24302D,
-        enabledButtonColor = 0xFFFFFFFF,
-        disabledButtonColor = 0XFF003300,
+        accentColor = 0XFF333333,
+        textColor = 0XFF333333,
+        errorColor = 0XFFE55541,
+        backgroundColor = 0xFFFFFFFF,
+        fieldBackgroundColor = 0XFFF6F7FA,
+        enabledButtonColor = 0xFF3D75E3,
+        disabledButtonColor = 0XFF9B9B9B,
     )
 
     fun providePaymentFormTheme(): PaymentFormTheme {
