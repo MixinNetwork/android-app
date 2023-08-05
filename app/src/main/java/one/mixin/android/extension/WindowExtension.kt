eb5c4cbe31b4733d946f0e8a52483127c8cb575a
touge <touge1027@gmail.com>
2023-08-05 10:42:43 +0800

    Use deprecated WindowInsets api

--- app/src/main/java/one/mixin/android/extension/WindowExtension.kt
+++ app/src/main/java/one/mixin/android/extension/WindowExtension.kt
@@ -4,21 +4,27 @@ import android.os.Build
 import android.view.WindowInsets
 
 fun WindowInsets.getSystemWindowTop(): Int {
-    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-        getInsetsIgnoringVisibility(WindowInsets.Type.systemBars()).top
-    } else {
-        @Suppress("DEPRECATION")
-        systemWindowInsetTop
-    }
+    @Suppress("DEPRECATION")
+    return systemWindowInsetTop
+
+//    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+//        getInsetsIgnoringVisibility(WindowInsets.Type.systemBars()).top
+//    } else {
+//        @Suppress("DEPRECATION")
+//        systemWindowInsetTop
+//    }
 }
 
 fun WindowInsets.getSystemWindowBottom(): Int {
-    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-        getInsetsIgnoringVisibility(WindowInsets.Type.systemBars()).bottom + getInsets(WindowInsets.Type.ime()).bottom
-    } else {
-        @Suppress("DEPRECATION")
-        systemWindowInsetBottom
-    }
+    @Suppress("DEPRECATION")
+    return systemWindowInsetBottom
+
+//    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+//        getInsetsIgnoringVisibility(WindowInsets.Type.systemBars()).bottom + getInsets(WindowInsets.Type.ime()).bottom
+//    } else {
+//        @Suppress("DEPRECATION")
+//        systemWindowInsetBottom
+//    }
 }
 
 fun WindowInsets.getSystemWindowLeft(): Int {
