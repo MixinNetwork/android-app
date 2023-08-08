0842a99c973b78eee6a7997456acddc9c33116d9
touge <touge1027@gmail.com>
2023-08-08 23:27:39 +0800

    Bugfix/appcenter (#3943)

--- app/src/main/java/one/mixin/android/util/ShareHelper.kt
+++ app/src/main/java/one/mixin/android/util/ShareHelper.kt
@@ -3,6 +3,8 @@ package one.mixin.android.util
 import android.content.Intent
 import android.net.Uri
 import one.mixin.android.extension.getFileName
+import one.mixin.android.extension.getParcelableArrayListCompat
+import one.mixin.android.extension.getParcelableExtraCompat
 import one.mixin.android.vo.ForwardCategory
 import one.mixin.android.vo.ForwardMessage
 import one.mixin.android.vo.ShareCategory
@@ -33,18 +35,18 @@ class ShareHelper {
             if ("text/plain" == type) {
                 val text = intent.getStringExtra(Intent.EXTRA_TEXT)
                 if (text.isNullOrEmpty()) {
-                    intent.getParcelableExtra<Uri>(Intent.EXTRA_STREAM)?.systemMediaToMessage(ForwardCategory.Data)?.addTo(result)
+                    intent.getParcelableExtraCompat(Intent.EXTRA_STREAM, Uri::class.java)?.systemMediaToMessage(ForwardCategory.Data)?.addTo(result)
                 } else {
                     ForwardMessage(ShareCategory.Text, text).addTo(result)
                 }
             } else if (type.startsWith("image/")) {
-                val imageUri = intent.getParcelableExtra<Uri>(Intent.EXTRA_STREAM)
+                val imageUri = intent.getParcelableExtraCompat(Intent.EXTRA_STREAM, Uri::class.java)
                 ForwardMessage(ShareCategory.Image, GsonHelper.customGson.toJson(ShareImageData(imageUri.toString()))).addTo(result)
             } else if (type.startsWith("video/")) {
-                val videoUri = intent.getParcelableExtra<Uri>(Intent.EXTRA_STREAM)
+                val videoUri = intent.getParcelableExtraCompat(Intent.EXTRA_STREAM, Uri::class.java)
                 videoUri?.systemMediaToMessage(ForwardCategory.Video)?.addTo(result)
             } else if (type.startsWith("application/") || type.startsWith("audio/")) {
-                intent.getParcelableExtra<Uri>(Intent.EXTRA_STREAM)?.let {
+                intent.getParcelableExtraCompat(Intent.EXTRA_STREAM, Uri::class.java)?.let {
                     val fileName = it.getFileName()
                     it.systemMediaToMessage(
                         ForwardCategory.Data,
@@ -53,23 +55,23 @@ class ShareHelper {
                     ).addTo(result)
                 }
             } else {
-                val dataUri = intent.getParcelableExtra<Uri>(Intent.EXTRA_STREAM)
+                val dataUri = intent.getParcelableExtraCompat(Intent.EXTRA_STREAM, Uri::class.java)
                 dataUri?.systemMediaToMessage(ForwardCategory.Data, dataUri.getFileName())?.addTo(result)
             }
         } else if (Intent.ACTION_SEND_MULTIPLE == action) {
             when {
                 type.startsWith("image/") -> {
-                    intent.getParcelableArrayListExtra<Uri>(Intent.EXTRA_STREAM)?.forEach { item ->
+                    intent.getParcelableArrayListCompat(Intent.EXTRA_STREAM, Uri::class.java)?.forEach { item ->
                         item.systemMediaToMessage(ShareCategory.Image).addTo(result)
                     }
                 }
                 type.startsWith("video/") -> {
-                    intent.getParcelableArrayListExtra<Uri>(Intent.EXTRA_STREAM)?.forEach { item ->
+                    intent.getParcelableArrayListCompat(Intent.EXTRA_STREAM, Uri::class.java)?.forEach { item ->
                         item.systemMediaToMessage(ForwardCategory.Video).addTo(result)
                     }
                 }
                 else -> {
-                    intent.getParcelableArrayListExtra<Uri>(Intent.EXTRA_STREAM)?.let { list ->
+                    intent.getParcelableArrayListCompat(Intent.EXTRA_STREAM, Uri::class.java)?.let { list ->
                         list.forEach { item ->
                             item.systemMediaToMessage(ForwardCategory.Data, item.getFileName()).addTo(result)
                         }
