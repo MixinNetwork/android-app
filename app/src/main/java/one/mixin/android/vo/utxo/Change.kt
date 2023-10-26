fba9b6297d8425da9367ffa1633bd52686e83637
SeniorZhai <developer.zhaitao@gmail.com>
2023-10-26 14:29:43 +0800

    Fix typo

--- app/src/main/java/one/mixin/android/vo/utxo/Change.kt
+++ app/src/main/java/one/mixin/android/vo/utxo/Change.kt
@@ -13,6 +13,6 @@ class Change(
 fun changeToOutput(change: Utxo, asset: String, createdAt: String): Output {
     val outputId = UUID.nameUUIDFromBytes("${change.hash}:${change.index}".toByteArray()).toString()
     return Output(
-        outputId, change.hash, change.index.toInt(), asset, change.amount, "", emptyList(), 1, emptyList(), "", 1, "", "unspent", createdAt, "", "", ""
+        outputId, change.hash, change.index.toInt(), asset, change.amount, "", emptyList(), 1, emptyList(), "", 1, "unspent", createdAt, "", "", "", ""
     )
 }
