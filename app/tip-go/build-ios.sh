767dd35849474638f1d563a1435b7e3aa7b5671e
touge <touge1027@gmail.com>
2023-11-06 16:20:02 +0800

    workaround load gojni

--- app/tip-go/build-ios.sh
+++ app/tip-go/build-ios.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+
+# go install github.com/mixinnetwork/mobile/cmd/gomobile
+# go install github.com/mixinnetwork/mobile/cmd/gobind
+
 gomobile bind -target=ios,iossimulator/arm64 -o ./tip.xcframework -trimpath -ldflags "-s -w" mixin/tip mixin/kernel
