bad6c586ceaefd4c443618ef1cc2c086257a92eb
SeniorZhai <developer.zhaitao@gmail.com>
2023-11-06 14:06:18 +0800

    Update sql

--- app/src/main/java/one/mixin/android/db/OutputDao.kt
+++ app/src/main/java/one/mixin/android/db/OutputDao.kt
@@ -13,7 +13,7 @@ interface OutputDao : BaseDao<Output> {
     @Query("SELECT * FROM outputs WHERE state = 'unspent' AND asset = :asset ORDER BY sequence ASC LIMIT :limit OFFSET :offset")
     suspend fun findUnspentOutputsByAssetOffset(limit: Int, asset: String, offset: Int): List<Output>
 
-    @Query("SELECT output_id FROM outputs WHERE state = 'signed' AND output_id IN (:ids)")
+    @Query("SELECT output_id FROM outputs WHERE output_id IN (:ids) AND state = 'signed'")
     fun findSignedOutput(ids: List<String>): List<String>
 
     @Query("SELECT sequence FROM outputs ORDER BY sequence DESC LIMIT 1")
