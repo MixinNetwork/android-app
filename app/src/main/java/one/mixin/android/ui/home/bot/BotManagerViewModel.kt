7c9073643c94d65514ee4ae4be1fe4776eca6b20
Crossle Song <crosslesong@gmail.com>
2023-11-17 00:29:20 -0800

    Bugfix/schema (#4160)
    * New schema support return_to
    
    * Check trace has paid
    
    * Code format
    
    * Update layout
    
    * Update api
    
    * Update transaction api
    
    * Update fee bottom padding
    
    ---------
    
    Co-authored-by: touge <touge1027@gmail.com>
    Co-authored-by: SeniorZhai <developer.zhaitao@gmail.com>

--- app/src/main/java/one/mixin/android/ui/home/bot/BotManagerViewModel.kt
+++ app/src/main/java/one/mixin/android/ui/home/bot/BotManagerViewModel.kt
@@ -2,8 +2,8 @@ package one.mixin.android.ui.home.bot
 
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
-import one.mixin.android.repository.UserRepository
 import javax.inject.Inject
+import one.mixin.android.repository.UserRepository
 
 @HiltViewModel
 class BotManagerViewModel
