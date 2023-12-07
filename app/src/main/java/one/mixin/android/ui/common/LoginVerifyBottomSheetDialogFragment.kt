fe7e64624e197030a8a9c251202728fa629cf738
touge <touge1027@gmail.com>
2023-12-07 15:15:05 +0800

    Fix omit check tip counter in login verification (#4211)

--- app/src/main/java/one/mixin/android/ui/common/LoginVerifyBottomSheetDialogFragment.kt
+++ app/src/main/java/one/mixin/android/ui/common/LoginVerifyBottomSheetDialogFragment.kt
@@ -3,15 +3,28 @@ package one.mixin.android.ui.common
 import android.annotation.SuppressLint
 import android.app.Dialog
 import android.content.DialogInterface
-import androidx.core.view.isVisible
+import androidx.lifecycle.lifecycleScope
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import one.mixin.android.R
+import one.mixin.android.RxBus
 import one.mixin.android.api.MixinResponse
 import one.mixin.android.databinding.FragmentLoginVerifyBottomSheetBinding
+import one.mixin.android.event.TipEvent
+import one.mixin.android.job.TipCounterSyncedLiveData
+import one.mixin.android.session.Session
+import one.mixin.android.tip.Tip
 import one.mixin.android.ui.common.biometric.BiometricBottomSheetDialogFragment
 import one.mixin.android.ui.common.biometric.BiometricInfo
+import one.mixin.android.ui.common.biometric.BiometricLayout
+import one.mixin.android.util.reportException
 import one.mixin.android.util.viewBinding
+import one.mixin.android.vo.Account
 import one.mixin.android.widget.BottomSheet
+import timber.log.Timber
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class LoginVerifyBottomSheetDialogFragment : BiometricBottomSheetDialogFragment() {
@@ -22,6 +35,12 @@ class LoginVerifyBottomSheetDialogFragment : BiometricBottomSheetDialogFragment(
     }
 
     private val binding by viewBinding(FragmentLoginVerifyBottomSheetBinding::inflate)
+
+    @Inject
+    lateinit var tip: Tip
+
+    @Inject
+    lateinit var tipCounterSynced: TipCounterSyncedLiveData
 
     @SuppressLint("RestrictedApi")
     override fun setupDialog(
@@ -36,8 +55,61 @@ class LoginVerifyBottomSheetDialogFragment : BiometricBottomSheetDialogFragment(
         }
         setBiometricLayout()
         binding.biometricLayout.apply {
-            biometricTv.isVisible = false
+            pin.isEnabled = false
             measureAllChildren = false
+        }
+        lifecycleScope.launch {
+            checkTipCounter(Session.getAccount()!!)
+        }
+    }
+
+    private suspend fun checkTipCounter(account: Account) {
+        binding.biometricLayout.showPb()
+        try {
+            tip.checkCounter(
+                account.tipCounter,
+                onNodeCounterNotEqualServer = { nodeMaxCounter, failedSigners ->
+                    RxBus.publish(TipEvent(nodeMaxCounter, failedSigners))
+                    withContext(Dispatchers.Main) {
+                        dismiss()
+                    }
+                },
+                onNodeCounterInconsistency = { nodeMaxCounter, failedSigners ->
+                    RxBus.publish(TipEvent(nodeMaxCounter, failedSigners))
+                    withContext(Dispatchers.Main) {
+                        dismiss()
+                    }
+                },
+            ).onSuccess {
+                tipCounterSynced.synced = true
+
+                if (!isAdded) return
+                withContext(Dispatchers.Main) {
+                    binding.biometricLayout.pin.isEnabled = true
+                    binding.biometricLayout.showPin(true)
+                }
+            }
+        } catch (e: Exception) {
+            val msg = "TIP $TAG checkCounter ${e.stackTraceToString()}"
+            Timber.e(msg)
+            reportException(msg, e)
+            showErrorWhenCheckCounterFailed(e.message ?: "checkCounter failed", account)
+        }
+    }
+
+    private fun showErrorWhenCheckCounterFailed(
+        errorString: String,
+        account: Account,
+    ) {
+        if (!isAdded) return
+        binding.biometricLayout.apply {
+            showErrorInfo(errorString, true, errorAction = BiometricLayout.ErrorAction.RetryPin)
+            errorBtn.setOnClickListener {
+                lifecycleScope.launch {
+                    showPin(true)
+                    checkTipCounter(account)
+                }
+            }
         }
     }
 
