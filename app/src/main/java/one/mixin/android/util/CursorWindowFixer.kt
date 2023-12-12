76e23642f11d05747c8e0a54d38f9f9a94cfd43e
touge <touge1027@gmail.com>
2023-12-12 20:38:02 +0800

    Update cursor fixer strategy

--- app/src/main/java/one/mixin/android/util/CursorWindowFixer.kt
+++ app/src/main/java/one/mixin/android/util/CursorWindowFixer.kt
@@ -5,6 +5,7 @@ import android.app.ActivityManager
 import android.app.ActivityManager.MemoryInfo
 import android.content.Context
 import android.database.CursorWindow
+import android.os.Build
 import androidx.core.content.getSystemService
 import timber.log.Timber
 import java.lang.reflect.Field
@@ -15,6 +16,8 @@ class CursorWindowFixer {
     companion object {
         @SuppressLint("DiscouragedPrivateApi")
         fun fix(context: Context) {
+            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) return
+
             try {
                 val field: Field = CursorWindow::class.java.getDeclaredField("sCursorWindowSize")
                 field.isAccessible = true
@@ -25,12 +28,12 @@ class CursorWindowFixer {
         }
 
         private fun getCursorWindowSize(context: Context): Int {
-            val activityManager = context.getSystemService<ActivityManager>() ?: return 10
+            val activityManager = context.getSystemService<ActivityManager>() ?: return 2
             val memoryInfo = MemoryInfo()
             activityManager.getMemoryInfo(memoryInfo)
             val total = memoryInfo.totalMem / 1024 / 1024 / 1024
-            if (total < 6) { // for device memory less than 6GB, use 10MB for cursor window size
-                return 10
+            if (total < 8) { // for device memory less than 8GB, use 2MB for cursor window size
+                return 2
             }
             val memorySize = max(total, 2)
             return min(100, (memorySize * 12.5).toInt()) // 8G Memory set window size to 100MB, 1G Memory set to 12.5MB
