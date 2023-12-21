06f97727f8265fcfa53c39c8a77eab01f5c2c6ca
SeniorZhai <developer.zhaitao@gmail.com>
2023-12-21 08:02:06 +0800

    Disable drag and drop

--- app/src/main/java/one/mixin/android/ui/home/bot/BotManagerAdapter.kt
+++ app/src/main/java/one/mixin/android/ui/home/bot/BotManagerAdapter.kt
@@ -1,24 +1,17 @@
 package one.mixin.android.ui.home.bot
 
 import android.annotation.SuppressLint
-import android.content.ClipData
-import android.os.Build
-import android.view.DragEvent
 import android.view.LayoutInflater
 import android.view.View
-import android.view.View.DRAG_FLAG_OPAQUE
-import android.view.View.DragShadowBuilder
 import android.view.ViewGroup
-import androidx.core.content.res.ResourcesCompat
 import androidx.recyclerview.widget.RecyclerView
 import one.mixin.android.R
 import one.mixin.android.databinding.ItemBotManagerBinding
-import one.mixin.android.extension.clickVibrate
 import one.mixin.android.extension.notEmptyWithElse
 import one.mixin.android.vo.App
 import one.mixin.android.vo.BotInterface
 
-class BotManagerAdapter(private val botCallBack: (BotInterface) -> Unit) : RecyclerView.Adapter<BotManagerAdapter.ListViewHolder>(), View.OnLongClickListener {
+class BotManagerAdapter(private val botCallBack: (BotInterface) -> Unit) : RecyclerView.Adapter<BotManagerAdapter.ListViewHolder>() {
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int,
@@ -62,7 +55,6 @@ class BotManagerAdapter(private val botCallBack: (BotInterface) -> Unit) : Recyc
                 botCallBack.invoke(app)
             }
             binding.avatar.tag = position
-            binding.avatar.setOnLongClickListener(this)
         }
     }
 
@@ -72,25 +64,4 @@ class BotManagerAdapter(private val botCallBack: (BotInterface) -> Unit) : Recyc
 
     class ListViewHolder(itemView: View?) : RecyclerView.ViewHolder(itemView!!)
 
-    override fun onLongClick(v: View): Boolean {
-        v.background = null
-        val data = ClipData.newPlainText("", "")
-        val shadowBuilder = DragShadowBuilder(v)
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            v.startDragAndDrop(data, shadowBuilder, v, DRAG_FLAG_OPAQUE)
-        } else {
-            @Suppress("DEPRECATION")
-            v.startDrag(data, shadowBuilder, v, 0)
-        }
-        v.setOnDragListener { view, event ->
-            if (event.action == DragEvent.ACTION_DRAG_ENDED) {
-                val ctx = view.context
-                view.background = ResourcesCompat.getDrawable(ctx.resources, R.drawable.mixin_ripple_large, ctx.theme)
-            }
-            return@setOnDragListener true
-        }
-        v.alpha = 0.2f
-        v.context.clickVibrate()
-        return false
-    }
 }
--- app/src/main/java/one/mixin/android/widget/bot/BotDock.kt
+++ app/src/main/java/one/mixin/android/widget/bot/BotDock.kt
@@ -1,26 +1,19 @@
 package one.mixin.android.widget.bot
 
 import android.annotation.SuppressLint
-import android.content.ClipData
 import android.content.Context
-import android.os.Build
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.collection.arraySetOf
-import androidx.core.view.ViewCompat
-import androidx.core.view.ViewPropertyAnimatorCompat
-import androidx.core.view.ViewPropertyAnimatorListener
 import androidx.core.view.isInvisible
 import androidx.core.view.isVisible
 import one.mixin.android.R
 import one.mixin.android.databinding.ItemDockBinding
-import one.mixin.android.extension.clickVibrate
 import one.mixin.android.vo.BotInterface
 import kotlin.math.max
 
-class BotDock : ViewGroup, View.OnLongClickListener {
+class BotDock : ViewGroup {
     constructor(context: Context) : this(context, null)
 
     constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
@@ -93,27 +86,6 @@ class BotDock : ViewGroup, View.OnLongClickListener {
         }
     }
 
-    override fun setOnDragListener(l: OnDragListener?) {
-        super.setOnDragListener(l)
-        for (i in 1 until childCount) {
-            val child = getChildAt(i)
-            child.setOnDragListener(l)
-        }
-    }
-
-    override fun onLongClick(v: View): Boolean {
-        val data = ClipData.newPlainText("", "")
-        val shadowBuilder = DragShadowBuilder(v)
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            v.startDragAndDrop(data, shadowBuilder, v, View.DRAG_FLAG_OPAQUE)
-        } else {
-            @Suppress("DEPRECATION")
-            v.startDrag(data, shadowBuilder, v, 0)
-        }
-        v.context.clickVibrate()
-        v.alpha = 0f
-        return false
-    }
 
     override fun generateDefaultLayoutParams(): MarginLayoutParams {
         return MarginLayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT)
@@ -148,7 +120,6 @@ class BotDock : ViewGroup, View.OnLongClickListener {
                 val app = apps[i - 1]
                 avatar.renderApp(app)
                 avatar.tag = app
-                avatar.setOnLongClickListener(this@BotDock)
                 avatar.setOnClickListener {
                     onDockListener?.onDockClick(app)
                 }
