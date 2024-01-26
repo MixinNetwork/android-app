46ee733f291a23df7fd68871460dc172f1f3eaba
SeniorZhai <developer.zhaitao@gmail.com>
2024-01-26 16:49:47 +0800

    Restore logs

--- app/src/main/java/one/mixin/android/job/RestoreTransactionJob.kt
+++ app/src/main/java/one/mixin/android/job/RestoreTransactionJob.kt
@@ -49,12 +49,17 @@ class RestoreTransactionJob : BaseJob(
                 val feeTraceId = uniqueObjectId(transaction.requestId, "FEE")
                 val feeTransaction = rawTransactionDao.findRawTransaction(feeTraceId, RawTransactionType.FEE.value)
                 try {
+                    Timber.e("Restore Transaction(${transaction.requestId}): Get Transaction")
                     val response = utxoService.getTransactionsById(transaction.requestId)
                     if (response.isSuccess) {
+                        Timber.e("Restore Transaction(${transaction.requestId}): db begin")
                         runInTransaction {
+                            Timber.e("Restore Transaction(${transaction.requestId}): update raw transaction ${transaction.requestId}")
                             rawTransactionDao.updateRawTransaction(transaction.requestId, OutputState.signed.name)
+                            Timber.e("Restore Transaction(${transaction.requestId}): update raw transaction $feeTraceId")
                             rawTransactionDao.updateRawTransaction(feeTraceId, OutputState.signed.name)
                         }
+                        Timber.e("Restore Transaction(${transaction.requestId}): db end")
                         if (feeTransaction == null) {
                             val data = response.data!!
                             if (transaction.receiverId.isNotBlank()) {
@@ -62,6 +67,7 @@ class RestoreTransactionJob : BaseJob(
                             }
                         }
                     } else if (response.errorCode == 404) {
+                        Timber.e("Restore Transaction(${transaction.requestId}): Post Transaction")
                         val transactionRsp =
                             utxoService.transactions(
                                 if (feeTransaction != null) {
@@ -71,29 +77,38 @@ class RestoreTransactionJob : BaseJob(
                                 },
                             )
                         if (transactionRsp.error == null) {
+                            Timber.e("Restore Transaction(${transaction.requestId}): Post Transaction Success")
                             val transactionResponse = transactionRsp.data!!.first()
+                            Timber.e("Restore Transaction(${transaction.requestId}): db begin")
                             runInTransaction {
+                                Timber.e("Restore Transaction(${transaction.requestId}): update raw transaction ${transaction.requestId}")
                                 rawTransactionDao.updateRawTransaction(transaction.requestId, OutputState.signed.name)
+                                Timber.e("Restore Transaction(${transaction.requestId}): update raw transaction $feeTraceId")
                                 rawTransactionDao.updateRawTransaction(feeTraceId, OutputState.signed.name)
                             }
+                            Timber.e("Restore Transaction(${transaction.requestId}): db end")
                             if (feeTransaction == null && transaction.receiverId.isNotBlank()) {
                                 insertSnapshotMessage(transactionResponse, transaction.receiverId)
                             }
                         } else {
+                            Timber.e("Restore Transaction(${transaction.requestId}): Post Transaction Error ${transactionRsp.errorDescription}")
                             reportException(e = Throwable("Transaction Error ${transactionRsp.errorDescription}"))
                             rawTransactionDao.updateRawTransaction(transaction.requestId, OutputState.signed.name)
                             rawTransactionDao.updateRawTransaction(feeTraceId, OutputState.signed.name)
                         }
                         jobManager.addJobInBackground(SyncOutputJob())
                     } else if (response.errorCode >= 500) {
+                        Timber.e("Restore Transaction(${transaction.requestId}): GET Transaction Server Error")
                         reportException(Exception("Restore Transaction Error: ${transaction.requestId} - ${response.errorCode}"))
                         delay(3000)
                     } else {
+                        Timber.e("Restore Transaction(${transaction.requestId}): GET Transaction Error ${response.errorDescription}")
                         reportException(Exception("Restore Transaction Error: ${transaction.requestId} - ${response.errorCode}"))
                         // do nothing, waiting cycle
                         delay(3000)
                     }
                 } catch (e: Exception) {
+                    Timber.e("Restore Transaction(${transaction.requestId}): Throwable ${e.message}")
                     Timber.e(e)
                     reportException(e)
                     delay(3000)
