b9c4cd642da2952d68746bd92cdd74816fd61ee0
SeniorZhai <developer.zhaitao@gmail.com>
2024-03-13 15:44:38 +0800

    Update address label span

--- app/src/main/java/one/mixin/android/widget/linktext/RoundBackgroundColorSpan.kt
+++ app/src/main/java/one/mixin/android/widget/linktext/RoundBackgroundColorSpan.kt
@@ -35,16 +35,16 @@ class RoundBackgroundColorSpan(private val bgColor: Int, private val textColor: 
         canvas.drawRoundRect(
             RectF(
                 x,
-                (top + paint.textSize * 0.05).toFloat(),
+                (top + paint.textSize * 0.02).toFloat(),
                 x + (paint.measureText(text, start, end).toInt() + 8.dp),
-                (bottom - paint.textSize * 0.05).toFloat(),
+                (bottom - paint.textSize * 0.02).toFloat(),
             ),
             4.dp.toFloat(),
             4.dp.toFloat(),
             paint,
         )
         paint.color = textColor
-        canvas.drawText(text, start, end, (x + 4.dp), y.toFloat(), paint)
+        canvas.drawText(text, start, end, (x + 4.dp), (y + paint.textSize * 0.02).toFloat(), paint)
         paint.color = originalColor
     }
 }
