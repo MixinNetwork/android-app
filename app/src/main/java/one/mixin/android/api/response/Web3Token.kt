cde980cf9221216ee2cc0dc2650144b660a8a89c
SeniorZhai <developer.zhaitao@gmail.com>
2024-04-28 18:50:02 +0800

    Update eth web token

--- app/src/main/java/one/mixin/android/api/response/Web3Token.kt
+++ app/src/main/java/one/mixin/android/api/response/Web3Token.kt
@@ -49,6 +49,9 @@ class Web3Token(
 fun Web3Token.getChainFromName(): Chain {
     return when {
         chainId.equals("ethereum", true) -> Chain.Ethereum
+        chainId.equals("base", true) -> Chain.Ethereum
+        chainId.equals("arbitrum", true) -> Chain.Ethereum
+        chainId.equals("optimism", true) -> Chain.Ethereum
         chainId.equals("polygon", true) -> Chain.Polygon
         chainId.equals("binance-smart-chain", true) -> Chain.BinanceSmartChain
         else -> throw IllegalArgumentException("Not support: $chainId")
@@ -58,6 +61,9 @@ fun Web3Token.getChainFromName(): Chain {
 fun Web3Token.getChainIdFromName(): String {
     return when {
         chainId.equals("ethereum", true) -> Constants.ChainId.ETHEREUM_CHAIN_ID
+        chainId.equals("base", true) -> Constants.ChainId.ETHEREUM_CHAIN_ID
+        chainId.equals("arbitrum", true) -> Constants.ChainId.ETHEREUM_CHAIN_ID
+        chainId.equals("optimism", true) -> Constants.ChainId.ETHEREUM_CHAIN_ID
         chainId.equals("polygon", true) -> Constants.ChainId.Polygon
         chainId.equals("binance-smart-chain", true) -> Constants.ChainId.BinanceSmartChain
         else -> throw IllegalArgumentException("Not support: $chainId")
@@ -66,6 +72,9 @@ fun Web3Token.getChainIdFromName(): String {
 
 fun Web3Token.getChainAssetKey(): String {
     return if (chainId.equals("ethereum", true)) "0x0000000000000000000000000000000000000000"
+    else if (chainId.equals("base", true)) "0x0000000000000000000000000000000000000000"
+    else if (chainId.equals("arbitrum", true)) "0x0000000000000000000000000000000000000000"
+    else if (chainId.equals("optimism", true)) "0x0000000000000000000000000000000000000000"
     else if (chainId.equals("polygon", true)) "0x0000000000000000000000000000000000001010"
     else if (chainId.equals("binance-smart-chain", true)) "0x0000000000000000000000000000000000000000"
     else throw IllegalArgumentException("Not support: $chainId")
@@ -73,7 +82,7 @@ fun Web3Token.getChainAssetKey(): String {
 
 fun Web3Token.supportDeposit(): Boolean {
     return when (chainId.lowercase(Locale.US)) {
-        "ethereum", "polygon", "binance-smart-chain" -> true
+        "ethereum", "base", "arbitrum", "optimism", "polygon", "binance-smart-chain" -> true
         else -> false
     }
 }
@@ -88,6 +97,9 @@ fun Web3Token.findChainToken(tokens: List<Web3Token>): Web3Token? {
 fun Web3Token.buildTransaction(fromAddress: String, toAddress: String, v: String): JsSignMessage {
     val transaction =
         if ((chainId.equals("ethereum", true) && assetKey == "0x0000000000000000000000000000000000000000") ||
+            (chainId.equals("base", true) && assetKey == "0x0000000000000000000000000000000000000000") ||
+            (chainId.equals("arbitrum", true) && assetKey == "0x0000000000000000000000000000000000000000") ||
+            (chainId.equals("optimism", true) && assetKey == "0x0000000000000000000000000000000000000000") ||
             (chainId.equals("polygon", true) && assetKey == "0x0000000000000000000000000000000000001010") ||
             (chainId.equals("binance-smart-chain", true) && assetKey == "0x0000000000000000000000000000000000000000")
         ) {
