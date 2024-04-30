0ec22722720bcd3a9ce4310d6d536413e53b8b24
touge <touge1027@gmail.com>
2024-04-30 23:16:01 +0800

    Fix set chain (#4542)
    * Fix set chain
    
    * tweak generate address

--- app/src/main/java/one/mixin/android/tip/wc/internal/Chain.kt
+++ app/src/main/java/one/mixin/android/tip/wc/internal/Chain.kt
@@ -95,6 +95,22 @@ internal fun String?.getChainSymbol(): String? {
     }
 }
 
+internal fun getChainByChainId(chainId: String?): Chain? {
+    if (chainId == null) return null
+
+    return when (chainId) {
+        Chain.Ethereum.chainId -> Chain.Ethereum
+        Chain.Base.chainId -> Chain.Base
+        Chain.Arbitrum.chainId -> Chain.Arbitrum
+        Chain.Optimism.chainId -> Chain.Optimism
+        Chain.BinanceSmartChain.chainId -> Chain.BinanceSmartChain
+        Chain.Polygon.chainId -> Chain.Polygon
+        Chain.Avalanche.chainId -> Chain.Avalanche
+        Chain.Solana.chainId -> Chain.Solana
+        else -> null
+    }
+}
+
 val walletConnectChainIdMap =
     mapOf(
         Chain.Ethereum.symbol to ETHEREUM_CHAIN_ID,
