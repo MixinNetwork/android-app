9781ae806e1a2252c68c91b871fa9c28c601dae0
SeniorZhai <developer.zhaitao@gmail.com>
2024-05-31 17:52:31 +0800

    Hide radio group (#4661)

--- app/src/main/java/one/mixin/android/web3/swap/SwapTokenListBottomSheetDialogFragment.kt
+++ app/src/main/java/one/mixin/android/web3/swap/SwapTokenListBottomSheetDialogFragment.kt
@@ -3,6 +3,7 @@ package one.mixin.android.web3.swap
 import android.annotation.SuppressLint
 import android.app.Dialog
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.core.view.updateLayoutParams
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
@@ -68,6 +69,7 @@ class SwapTokenListBottomSheetDialogFragment : MixinBottomSheetDialogFragment() 
         }
 
         binding.apply {
+            radio.isVisible = true
             assetRv.adapter = adapter
             adapter.tokens = tokens!!
             searchEt.et.setHint(R.string.search_swap_token)
