2c8b693b9913b542bec230338aaf9f84a536920d
touge <touge1027@gmail.com>
2024-06-27 17:41:05 +0800

    Save historical salts (#4565)
    * Save historical salts
    
    * Debug support reveal all salts
    
    ---------
    
    Co-authored-by: SeniorZhai <developer.zhaitao@gmail.com>

--- app/src/main/java/one/mixin/android/ui/setting/SafeDebugFragment.kt
+++ app/src/main/java/one/mixin/android/ui/setting/SafeDebugFragment.kt
@@ -8,17 +8,22 @@ import androidx.lifecycle.lifecycleScope
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 import one.mixin.android.Constants.Tip.ALIAS_SPEND_SALT
+import one.mixin.android.Constants.Tip.SPEND_SALT
 import one.mixin.android.MixinApplication
 import one.mixin.android.R
 import one.mixin.android.crypto.newKeyPairFromSeed
 import one.mixin.android.databinding.FragmentSafeDebugBinding
 import one.mixin.android.extension.alert
+import one.mixin.android.extension.defaultSharedPreferences
 import one.mixin.android.extension.getClipboardManager
 import one.mixin.android.extension.indeterminateProgressDialog
+import one.mixin.android.extension.putString
 import one.mixin.android.extension.toHex
 import one.mixin.android.extension.toast
 import one.mixin.android.tip.Tip
 import one.mixin.android.tip.deleteKeyByAlias
+import one.mixin.android.tip.deleteLatestSalts
+import one.mixin.android.tip.readAllEncryptedSalts
 import one.mixin.android.ui.common.BaseFragment
 import one.mixin.android.ui.common.PinInputBottomSheetDialogFragment
 import one.mixin.android.util.viewBinding
@@ -50,9 +55,20 @@ class SafeDebugFragment : BaseFragment(R.layout.fragment_safe_debug) {
         binding.publicKey.setOnClickListener {
             reveal()
         }
+        binding.allPublicKey.setOnClickListener {
+            revealAll()
+        }
         binding.delete.setOnClickListener {
-            deleteKeyByAlias(ALIAS_SPEND_SALT)
-            toast("Delete Salt")
+            requireContext().alert("Are you sure to delete latest salt", "Delete Latest Salt")
+                .setPositiveButton(R.string.Delete) { v, _ ->
+                    if (deleteLatestSalts(MixinApplication.appContext)) {
+                        toast("Latest salt deleted!")
+                    }
+                    v.dismiss()
+                }
+                .setNegativeButton(R.string.Cancel) { v, _ ->
+                    v.dismiss()
+                }.show()
         }
     }
 
@@ -70,14 +86,44 @@ class SafeDebugFragment : BaseFragment(R.layout.fragment_safe_debug) {
                 val pkHex = keyPair.publicKey.toHex()
                 dialog.dismiss()
                 requireContext().alert(pkHex, "Reveal Public Key")
-                    .setPositiveButton(android.R.string.copy) { dialog, _ ->
+                    .setPositiveButton(android.R.string.copy) { v, _ ->
                         requireContext().getClipboardManager().setPrimaryClip(
                             ClipData.newPlainText(
                                 null,
                                 pkHex,
                             ),
                         )
-                        dialog.dismiss()
+                        v.dismiss()
+                    }
+                    .show()
+            }
+        }.showNow(parentFragmentManager, PinInputBottomSheetDialogFragment.TAG)
+    }
+
+    private fun revealAll() {
+        PinInputBottomSheetDialogFragment.newInstance("Input PIN to Reveal All").setOnPinComplete { pin ->
+            lifecycleScope.launch {
+                val dialog =
+                    indeterminateProgressDialog(message = R.string.Please_wait_a_bit).apply {
+                        setCancelable(false)
+                    }
+                dialog.show()
+                val tipPriv = tip.getOrRecoverTipPriv(MixinApplication.appContext, pin).getOrThrow()
+                val pks = readAllEncryptedSalts(MixinApplication.appContext).map {
+                    val spendKey = tip.getSpendPrivFromEncryptedSalt(it, pin, tipPriv)
+                    val keyPair = newKeyPairFromSeed(spendKey)
+                    keyPair.publicKey.toHex()
+                }.joinToString("\n")
+                dialog.dismiss()
+                requireContext().alert(pks, "Reveal All Public Key")
+                    .setPositiveButton(android.R.string.copy) { v, _ ->
+                        requireContext().getClipboardManager().setPrimaryClip(
+                            ClipData.newPlainText(
+                                null,
+                                pks,
+                            ),
+                        )
+                        v.dismiss()
                     }
                     .show()
             }
