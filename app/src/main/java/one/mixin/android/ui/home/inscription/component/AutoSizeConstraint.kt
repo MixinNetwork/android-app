d2a07b7e0e636851fa8c032418d4d26782417535
SeniorZhai <developer.zhaitao@gmail.com>
2024-07-03 13:20:17 +0800

    Owner compose (#4766)
    * Owner compose
    
    * Update
    
    * Update title

--- app/src/main/java/one/mixin/android/ui/home/inscription/component/AutoSizeConstraint.kt
+++ app/src/main/java/one/mixin/android/ui/home/inscription/component/AutoSizeConstraint.kt
@@ -3,11 +3,14 @@ package one.mixin.android.ui.home.inscription.component
 import android.util.Log
 import androidx.compose.material.LocalTextStyle
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawWithContent
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
@@ -17,7 +20,6 @@ import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.sp
-import timber.log.Timber
 
 sealed class AutoSizeConstraint(open val min: TextUnit = TextUnit.Unspecified) {
     data class Width(override val min: TextUnit = TextUnit.Unspecified): AutoSizeConstraint(min)
