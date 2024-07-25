1c6aef4d988637bd4336928ddd2ee70447c2c4de
SeniorZhai <developer.zhaitao@gmail.com>
2024-07-25 13:00:03 +0800

    Remove unnecessary string formatting

--- app/src/main/java/one/mixin/android/widget/WebTokenHeader.kt
+++ app/src/main/java/one/mixin/android/widget/WebTokenHeader.kt
@@ -10,7 +10,6 @@ import one.mixin.android.R
 import one.mixin.android.api.response.Web3Token
 import one.mixin.android.databinding.ViewWeb3TokenHeaderBinding
 import one.mixin.android.extension.loadImage
-import one.mixin.android.extension.numberFormat
 import one.mixin.android.extension.numberFormat2
 import one.mixin.android.vo.Fiats
 import java.math.BigDecimal
@@ -57,13 +56,13 @@ class Web3TokenHeader : ConstraintLayout {
         _binding.avatar.badge.loadImage(token.chainIconUrl, R.drawable.ic_avatar_place_holder)
         _binding.totalTv.text =
             try {
-                if (token.balance.numberFormat().toFloat() == 0f) {
+                if (token.balance.toFloat() == 0f) {
                     "0.00"
                 } else {
-                    token.balance.numberFormat()
+                    token.balance
                 }
-            } catch (ignored: NumberFormatException) {
-                token.balance.numberFormat()
+            } catch (ignored: Exception) {
+                token.balance
             }
         _binding.value.text = "≈ ${Fiats.getSymbol()}${(BigDecimal(token.price).multiply(BigDecimal(token.balance)).multiply(BigDecimal(Fiats.getRate())).numberFormat2())}"
         _binding.symbol.text = token.symbol
