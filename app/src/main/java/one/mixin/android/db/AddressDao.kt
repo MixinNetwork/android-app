4ee3bae8ee3ebe1d8f7e7c91a241dd48e42d470a
SeniorZhai <developer.zhaitao@gmail.com>
2024-07-30 13:03:23 +0800

    Update address item

--- app/src/main/java/one/mixin/android/db/AddressDao.kt
+++ app/src/main/java/one/mixin/android/db/AddressDao.kt
@@ -11,7 +11,7 @@ interface AddressDao : BaseDao<Address> {
     @Query("SELECT * FROM addresses WHERE asset_id = :id ORDER BY updated_at DESC")
     fun addresses(id: String): LiveData<List<Address>>
 
-    @Query("SELECT a.address_id, t.icon_url, a.label, a.destination, a.tag FROM addresses a LEFT JOIN tokens t ON t.asset_id = a.asset_id ORDER BY updated_at DESC")
+    @Query("SELECT a.address_id, t.icon_url, c.icon_url as chain_icon_url, a.label, a.destination, a.tag FROM addresses a LEFT JOIN tokens t ON t.asset_id = a.asset_id LEFT JOIN tokens c ON c.asset_id = t.chain_id ORDER BY updated_at DESC")
     fun allAddresses(): LiveData<List<AddressItem>>
 
     @Query("DELETE FROM addresses WHERE address_id = :id")
