92354e2ab67beb602cc5e0ed5403093a1df7033a
SeniorZhai <developer.zhaitao@gmail.com>
2024-08-12 17:03:15 +0800

    Fix typo file name (#4870)

--- app/src/main/java/one/mixin/android/job/SyncInscriptionsJob.kt
+++ app/src/main/java/one/mixin/android/job/SyncInscriptionsJob.kt
@@ -4,26 +4,45 @@ import com.birbit.android.jobqueue.Params
 import kotlinx.coroutines.runBlocking
 import timber.log.Timber
 
-class SyncInscriptionCollectionJob(val hash: String) : BaseJob(
+class SyncInscriptionsJob(val hash: List<String>) : BaseJob(
     Params(PRIORITY_UI_HIGH).addTags(TAG).groupBy(TAG).persist().requireNetwork(),
 ) {
     companion object {
         private const val serialVersionUID = 1L
-        const val TAG = "SyncInscriptionJob"
+        const val TAG = "SyncInscriptionsJob"
     }
 
     override fun onRun() =
         runBlocking {
-            syncInscriptionCollection(hash)
+            syncInscriptions()
         }
 
+    private suspend fun syncInscriptions() {
+        val local = inscriptionDao.getExitsHash(hash)
+        (hash - local.toSet()).forEach {
+            val response = tokenService.getInscriptionItem(it)
+            if (response.isSuccess) {
+                val inscription = response.data ?: return@forEach
+                inscriptionDao.insert(inscription)
+                syncInscriptionCollection(inscription.collectionHash)
+            } else {
+                Timber.e(response.errorDescription)
+            }
+        }
+        inscriptionDao.getInscriptionCollectionIds(hash).forEach {
+            syncInscriptionCollection(it)
+        }
+    }
+
     private suspend fun syncInscriptionCollection(collectionHash: String) {
-        val collectionResponse = tokenService.getInscriptionCollection(collectionHash)
-        if (collectionResponse.isSuccess) {
-            val inscriptionCollection = collectionResponse.data ?: return
-            inscriptionCollectionDao.insert(inscriptionCollection)
-        } else {
-            Timber.e(collectionResponse.errorDescription)
+        if (inscriptionCollectionDao.exits(collectionHash) == null) {
+            val collectionResponse = tokenService.getInscriptionCollection(collectionHash)
+            if (collectionResponse.isSuccess) {
+                val inscriptionCollection = collectionResponse.data ?: return
+                inscriptionCollectionDao.insert(inscriptionCollection)
+            } else {
+                Timber.e(collectionResponse.errorDescription)
+            }
         }
     }
 }
