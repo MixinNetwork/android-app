9718d93bbf5f200eeb18561f9e1b128e53c9b84c
SeniorZhai <developer.zhaitao@gmail.com>
2024-09-11 15:00:44 +0800

    Market rank

--- app/src/main/java/one/mixin/android/ui/wallet/MarketDetailsFragment.kt
+++ app/src/main/java/one/mixin/android/ui/wallet/MarketDetailsFragment.kt
@@ -245,6 +245,7 @@ class MarketDetailsFragment : BaseFragment(R.layout.fragment_details_market) {
         walletViewModel.marketByCoinId(marketItem.coinId).observe(this.viewLifecycleOwner) { info ->
             if (info != null) {
                 binding.apply {
+                    rank.text = "#${marketItem.marketCapRank}"
                     currentPrice = priceFormat(info.currentPrice)
                     priceValue.text = currentPrice
                     marketHigh.text = priceFormat(info.high24h)
