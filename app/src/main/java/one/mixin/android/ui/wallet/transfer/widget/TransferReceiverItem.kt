0a99f289a06bad4c3345335f446600150795b38c
SeniorZhai <developer.zhaitao@gmail.com>
2024-10-10 19:27:57 +0800

    Feature/transfer bottom (#5015)
    * Update Android Gradle Plugin to 8.7.0-alpha09
    
    * Display signer state
    
    * Update layout

--- app/src/main/java/one/mixin/android/ui/wallet/transfer/widget/TransferReceiverItem.kt
+++ app/src/main/java/one/mixin/android/ui/wallet/transfer/widget/TransferReceiverItem.kt
@@ -8,13 +8,17 @@ import android.view.Gravity
 import android.view.LayoutInflater
 import android.widget.LinearLayout
 import androidx.appcompat.content.res.AppCompatResources
+import androidx.core.view.isVisible
+import androidx.core.view.updateLayoutParams
+import one.mixin.android.R
 import one.mixin.android.databinding.ItemReceiverBinding
 import one.mixin.android.extension.dp
 import one.mixin.android.vo.User
 
 class TransferReceiverItem : LinearLayout {
     private val _binding: ItemReceiverBinding
-    private val dp8 = 8.dp
+    private val dp6 = 6.dp
+    private val dp4 = 4.dp
 
     constructor(context: Context) : this(context, null)
     constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
@@ -24,13 +28,36 @@ class TransferReceiverItem : LinearLayout {
         _binding = ItemReceiverBinding.inflate(LayoutInflater.from(context), this)
         val outValue = TypedValue()
         getContext().theme.resolveAttribute(android.R.attr.selectableItemBackground, outValue, true)
-        foreground = AppCompatResources.getDrawable(context, outValue.resourceId)
+        _binding.name.foreground = AppCompatResources.getDrawable(context, outValue.resourceId)
+    }
+
+    override fun setOnClickListener(l: OnClickListener?) {
+        _binding.name.setOnClickListener(l)
+        _binding.userAvatar.setOnClickListener(l)
     }
 
     @SuppressLint("SetTextI18n")
-    fun setContent(user: User) {
+    fun setContent(user: User, signers: List<String>? = null) {
         _binding.apply {
-            name.setName(user,"${user.fullName} (${user.identityNumber})")
+            if (signers != null) {
+                userSelect.isVisible = true
+                userSelect.setImageResource(
+                    if (signers.contains(user.userId)) {
+                        R.drawable.ic_ceceive_selected
+                    } else {
+                        R.drawable.ic_ceceive_unselected
+                    }
+                )
+                userAvatar.updateLayoutParams<MarginLayoutParams> {
+                    marginEnd = dp6
+                }
+            } else {
+                userSelect.isVisible = false
+                userAvatar.updateLayoutParams<MarginLayoutParams> {
+                    marginEnd = dp4
+                }
+            }
+            name.setName(user, "${user.fullName} (${user.identityNumber})")
             userAvatar.setInfo(user.fullName, user.avatarUrl, user.identityNumber)
         }
     }
