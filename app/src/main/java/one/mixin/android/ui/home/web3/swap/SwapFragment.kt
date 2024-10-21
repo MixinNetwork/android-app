d014b0a5724b4cdf74ac195de9a5019d37ade628
SeniorZhai <developer.zhaitao@gmail.com>
2024-10-21 14:13:36 +0800

    Code clean

--- app/src/main/java/one/mixin/android/ui/home/web3/swap/SwapFragment.kt
+++ app/src/main/java/one/mixin/android/ui/home/web3/swap/SwapFragment.kt
@@ -248,7 +248,7 @@ class SwapFragment : BaseFragment() {
                                     val inputMint = fromToken?.getUnique() ?: return@launch
                                     val outputMint = toToken?.getUnique() ?: return@launch
 
-                                    if (quoteJob?.isActive == true) quoteJob?.cancel()
+                                    quoteJob?.takeIf { it.isActive }?.cancel()
                                     isLoading = true
                                     val swapResult =
                                         handleMixinResponse(
@@ -549,7 +549,7 @@ class SwapFragment : BaseFragment() {
     }
 
     private fun refreshQuote(text: String) {
-        if (quoteJob?.isActive == true) quoteJob?.cancel()
+        quoteJob?.takeIf { it.isActive }?.cancel()
         if (text.isBlank()) {
             outputText = "0"
         } else {
