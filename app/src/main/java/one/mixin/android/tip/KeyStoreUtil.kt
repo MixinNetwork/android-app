f5110e4b0b09ca5451698e9500a42bff2f78c17c
SeniorZhai <developer.zhaitao@gmail.com>
2024-10-22 14:33:59 +0800

    Compatible method

--- app/src/main/java/one/mixin/android/tip/KeyStoreUtil.kt
+++ app/src/main/java/one/mixin/android/tip/KeyStoreUtil.kt
@@ -1,16 +1,24 @@
 package one.mixin.android.tip
 
+import android.content.Context
 import android.os.Build
 import android.security.keystore.KeyGenParameterSpec
 import android.security.keystore.KeyProperties
 import android.security.keystore.KeyProperties.AUTH_BIOMETRIC_STRONG
 import android.security.keystore.KeyProperties.AUTH_DEVICE_CREDENTIAL
+import androidx.security.crypto.EncryptedSharedPreferences
+import androidx.security.crypto.MasterKey
+import com.google.protobuf.Mixin
+import one.mixin.android.MixinApplication
+import one.mixin.android.extension.base64Encode
+import one.mixin.android.extension.decodeBase64
 import one.mixin.android.util.reportException
 import java.security.KeyStore
 import javax.crypto.Cipher
 import javax.crypto.KeyGenerator
 import javax.crypto.SecretKey
 import javax.crypto.spec.IvParameterSpec
+import javax.crypto.spec.SecretKeySpec
 
 fun getEncryptCipher(alias: String): Cipher {
     val cipher = Cipher.getInstance("AES/CBC/PKCS7Padding")
@@ -45,17 +53,23 @@ private fun getKeyByAlias(
     alias: String,
     userAuthenticationRequired: Boolean = false,
 ): SecretKey? {
-    val ks: KeyStore =
-        KeyStore.getInstance("AndroidKeyStore").apply {
-            load(null)
-        }
-    var key: SecretKey? = null
+    var key: SecretKey?
+    var keyStoreCrash = false
     try {
+        val ks: KeyStore =
+            KeyStore.getInstance("AndroidKeyStore").apply {
+                load(null)
+            }
         key = ks.getKey(alias, null) as? SecretKey
         if (key != null) {
             return key
         }
     } catch (e: Exception) {
+        keyStoreCrash = true
+        key = getAesKeyFromEncryptedPreferences(MixinApplication.appContext, alias)
+        if (key != null) {
+            return key
+        }
         reportException("getKeyByAlias", e)
     }
     try {
@@ -83,8 +97,38 @@ private fun getKeyByAlias(
         }
         keyGenerator.init(builder.build())
         key = keyGenerator.generateKey()
+        if (keyStoreCrash) {
+            storeAesKeyInEncryptedPreferences(MixinApplication.appContext, alias, key)
+        }
     } catch (e: Exception) {
         reportException("getKeyByAlias", e)
     }
     return key
+}
+
+private fun storeAesKeyInEncryptedPreferences(context: Context, alias: String, key: SecretKey) {
+    val encryptedPrefs = EncryptedSharedPreferences.create(
+        context,
+        alias,
+        MasterKey.Builder(context).setKeyScheme(MasterKey.KeyScheme.AES256_GCM).build(),
+        EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+        EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+    )
+
+    val encodedKey = key.encoded.base64Encode()
+    encryptedPrefs.edit().putString(alias, encodedKey).apply()
+}
+
+private fun getAesKeyFromEncryptedPreferences(context: Context, alias: String): SecretKey? {
+    val encryptedPrefs = EncryptedSharedPreferences.create(
+        context,
+        alias,
+        MasterKey.Builder(context).setKeyScheme(MasterKey.KeyScheme.AES256_GCM).build(),
+        EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+        EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+    )
+
+    val encodedKey = encryptedPrefs.getString(alias, null) ?: return null
+    val decodedKey = encodedKey.decodeBase64()
+    return SecretKeySpec(decodedKey, "AES")
 }
