9052a560981590088a20ee3622d27e7fe74287a4
SeniorZhai <developer.zhaitao@gmail.com>
2024-11-15 18:20:53 +0800

    Update style

--- app/src/main/java/one/mixin/android/ui/wallet/TransactionsFragment.kt
+++ app/src/main/java/one/mixin/android/ui/wallet/TransactionsFragment.kt
@@ -124,7 +124,6 @@ class TransactionsFragment : BaseFragment(R.layout.fragment_transactions), OnSna
             }
         }
         binding.apply {
-            sendReceiveView.enableSwap()
             sendReceiveView.swap.setOnClickListener {
                 lifecycleScope.launch {
                     val assets = walletViewModel.allAssetItems()
