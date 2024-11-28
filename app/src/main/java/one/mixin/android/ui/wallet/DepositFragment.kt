df0b32e68b2ef3f8852c4b60c7d5b3cf8dd8d07d
Crossle Song <crosslesong@gmail.com>
2024-11-28 15:02:09 +0400

    update usdc versions

--- app/src/main/java/one/mixin/android/ui/wallet/DepositFragment.kt
+++ app/src/main/java/one/mixin/android/ui/wallet/DepositFragment.kt
@@ -54,7 +54,6 @@ class DepositFragment : BaseFragment() {
     private val usdcAssets =
         mapOf(
             "9b180ab6-6abe-3dc0-a13f-04169eb34bfa" to "ERC-20",
-            "fe26b981-29e9-3032-a0e9-b24d619e987e" to "TRC-20",
             "de6fa523-c596-398e-b12f-6d6980544b59" to "Solana",
             "2f845564-3898-3d17-8c24-3275e96235b5" to "Base",
             "5fec1691-561d-339f-8819-63d54bf50b52" to "Polygon",
