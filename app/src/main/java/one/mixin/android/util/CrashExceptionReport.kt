7adfd60732639a53b7510c69ba473ef1a454a736
SeniorZhai <developer.zhaitao@gmail.com>
2024-12-09 14:51:53 +0800

    More report

--- app/src/main/java/one/mixin/android/util/CrashExceptionReport.kt
+++ app/src/main/java/one/mixin/android/util/CrashExceptionReport.kt
@@ -5,12 +5,15 @@ import androidx.collection.ArrayMap
 import androidx.media3.common.PlaybackException
 import androidx.media3.datasource.HttpDataSource
 import com.google.firebase.crashlytics.FirebaseCrashlytics
+import io.sentry.Sentry
+import io.sentry.SentryLevel
 import one.mixin.android.BuildConfig
 import one.mixin.android.extension.getStackTraceString
 import one.mixin.android.session.Session
 
 fun reportException(e: Throwable) {
     FirebaseCrashlytics.getInstance().recordException(e)
+    Sentry.captureException(e)
 }
 
 fun reportException(
@@ -18,9 +21,11 @@ fun reportException(
     e: Throwable,
 ) {
     FirebaseCrashlytics.getInstance().log(msg + e.getStackTraceString())
+    Sentry.captureMessage(msg + e.getStackTraceString())
 }
 
 fun reportEvent(msg: String) {
+    Sentry.captureMessage(msg, SentryLevel.DEBUG)
 }
 
 fun reportExoPlayerException(
