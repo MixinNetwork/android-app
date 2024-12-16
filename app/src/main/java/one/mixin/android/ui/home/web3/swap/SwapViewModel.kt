b85e8689c0913cda4a08508b9df57be7df321f8b
SeniorZhai <developer.zhaitao@gmail.com>
2024-12-16 14:09:55 +0800

    Refactor/swap (#5175)
    * Refactor Compose parameters to improve component passing
    
    * Inner request
    
    * Remember
    
    * Update price info
    
    * Display error message
    
    * Update text style
    
    * Update default loading
    
    * Update layout
    
    * Check if respond quote range
    
    * Code clean
    
    ---------
    
    Co-authored-by: Crossle Song <crosslesong@gmail.com>
    Co-authored-by: touge <touge1027@gmail.com>

--- app/src/main/java/one/mixin/android/ui/home/web3/swap/SwapViewModel.kt
+++ app/src/main/java/one/mixin/android/ui/home/web3/swap/SwapViewModel.kt
@@ -1,18 +1,20 @@
 package one.mixin.android.ui.home.web3.swap
 
+import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import one.mixin.android.Constants.RouteConfig.ROUTE_BOT_USER_ID
+import one.mixin.android.R
 import one.mixin.android.api.MixinResponse
 import one.mixin.android.api.handleMixinResponse
 import one.mixin.android.api.request.RelationshipAction
 import one.mixin.android.api.request.RelationshipRequest
 import one.mixin.android.api.request.web3.SwapRequest
 import one.mixin.android.api.response.Web3Token
-import one.mixin.android.api.response.web3.QuoteResponse
+import one.mixin.android.api.response.web3.QuoteResult
 import one.mixin.android.api.response.web3.SwapResponse
 import one.mixin.android.api.response.web3.SwapToken
 import one.mixin.android.api.service.Web3Service
@@ -21,6 +23,8 @@ import one.mixin.android.job.UpdateRelationshipJob
 import one.mixin.android.repository.TokenRepository
 import one.mixin.android.repository.UserRepository
 import one.mixin.android.ui.oldwallet.AssetRepository
+import one.mixin.android.util.ErrorHandler.Companion.INVALID_QUOTE_AMOUNT
+import one.mixin.android.util.getMixinErrorStringByCode
 import one.mixin.android.vo.safe.TokenItem
 import javax.inject.Inject
 
@@ -44,13 +48,58 @@ class SwapViewModel
             amount: String,
             slippage: String,
             source: String,
-        ): MixinResponse<QuoteResponse> = assetRepository.web3Quote(inputMint, outputMint, amount, slippage, source)
+        ): MixinResponse<QuoteResult> = assetRepository.web3Quote(inputMint, outputMint, amount, slippage, source)
 
         suspend fun web3Swap(
             swapRequest: SwapRequest,
         ): MixinResponse<SwapResponse> {
             addRouteBot()
             return assetRepository.web3Swap(swapRequest)
+        }
+
+        suspend fun quote(
+            context: Context,
+            symbol: String,
+            inputMint: String?,
+            outputMint: String?,
+            amount: String,
+            slippage: String,
+            source: String,
+        ) : Result<QuoteResult?> {
+            return if (amount.isNotBlank() && inputMint != null && outputMint != null) {
+                runCatching {
+                    val response = web3Quote(
+                        inputMint = inputMint,
+                        outputMint = outputMint,
+                        amount = amount,
+                        slippage = slippage,
+                        source = source,
+                    )
+                    return if (response.isSuccess) {
+                        Result.success(requireNotNull(response.data))
+                    } else if (response.errorCode == INVALID_QUOTE_AMOUNT) {
+                        val extra = response.error?.extra?.asJsonObject?.get("data")?.asJsonObject
+                        val errorMessage = when {
+                            extra != null -> {
+                                val min = extra.get("min")?.asString
+                                val max = extra.get("max")?.asString
+                                when {
+                                    !min.isNullOrBlank() && !max.isNullOrBlank() -> context.getString(R.string.single_transaction_should_be_between, min, symbol, max, symbol)
+                                    !min.isNullOrBlank() -> context.getString(R.string.single_transaction_should_be_greater_than, min, symbol)
+                                    !max.isNullOrBlank() -> context.getString(R.string.single_transaction_should_be_less_than, max, symbol)
+                                    else -> context.getMixinErrorStringByCode(response.errorCode, response.errorDescription)
+                                }
+                            }
+                            else -> context.getMixinErrorStringByCode(response.errorCode, response.errorDescription)
+                        }
+                        return Result.failure(Throwable(errorMessage))
+                    } else {
+                        Result.failure(Throwable(context.getMixinErrorStringByCode(response.errorCode, response.errorDescription)))
+                    }
+                }
+            } else {
+                 Result.success(null)
+            }
         }
 
         suspend fun searchTokens(query: String) = assetRepository.searchTokens(query)
