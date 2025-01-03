8beaa7cfd95357bcd098773cbdaf7be8a7ba5671
SeniorZhai <developer.zhaitao@gmail.com>
2025-01-03 15:47:54 +0800

    Update transaction closure

--- app/src/main/java/one/mixin/android/job/SyncOutputJob.kt
+++ app/src/main/java/one/mixin/android/job/SyncOutputJob.kt
@@ -2,7 +2,6 @@ package one.mixin.android.job
 
 import com.birbit.android.jobqueue.Params
 import kotlinx.coroutines.runBlocking
-import one.mixin.android.db.insertUnspentOutputs
 import one.mixin.android.session.Session
 import one.mixin.android.session.buildHashMembers
 import timber.log.Timber
