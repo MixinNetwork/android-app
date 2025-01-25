2e62011ac4ba18df6df44d67829c7321970762c9
SeniorZhai <developer.zhaitao@gmail.com>
2025-01-25 10:37:41 +0800

    Clear paint shader

--- app/src/main/java/one/mixin/android/widget/ConfirmationBgView.kt
+++ app/src/main/java/one/mixin/android/widget/ConfirmationBgView.kt
@@ -2,22 +2,16 @@ package one.mixin.android.widget
 
 import android.content.Context
 import android.graphics.Canvas
-import android.graphics.Outline
+import android.graphics.Color
+import android.graphics.LinearGradient
 import android.graphics.Paint
+import android.graphics.Shader
 import android.util.AttributeSet
-import android.view.View
-import android.view.ViewOutlineProvider
 import android.widget.RelativeLayout
 import one.mixin.android.R
 import one.mixin.android.extension.colorFromAttribute
-import one.mixin.android.extension.dpToPx
-import android.graphics.Color
-import android.graphics.LinearGradient
-import android.graphics.Shader
 import one.mixin.android.extension.dp
-import kotlin.math.max
 import kotlin.math.min
-
 
 class ConfirmationBgView : RelativeLayout {
     private val colorWhite by lazy { context.colorFromAttribute(R.attr.bg_white) }
@@ -35,12 +29,16 @@ class ConfirmationBgView : RelativeLayout {
         setWillNotDraw(false)
     }
 
+    init {
+        paint.color = colorWhite
+    }
+
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
         val w = width.toFloat()
         val h = height.toFloat()
         if (all == 0 || cur == 0) {
-            paint.color = colorWhite
+            paint.shader = null // clear shader
             canvas.drawRect(0f, 0f, w, h, paint)
         } else {
             val gradient = LinearGradient(
@@ -61,28 +59,5 @@ class ConfirmationBgView : RelativeLayout {
     ) {
         this.all = all
         this.cur = cur
-    }
-
-    fun roundBottom(round: Boolean) {
-        if (round) {
-            outlineProvider =
-                object : ViewOutlineProvider() {
-                    override fun getOutline(
-                        view: View,
-                        outline: Outline,
-                    ) {
-                        outline.setRoundRect(
-                            0,
-                            0,
-                            view.width,
-                            view.height,
-                            context.dpToPx(8f).toFloat(),
-                        )
-                    }
-                }
-            clipToOutline = true
-        } else {
-            clipToOutline = false
-        }
     }
 }
