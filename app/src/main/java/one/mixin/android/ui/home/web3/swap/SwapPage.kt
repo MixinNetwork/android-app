85d3fc93d0b9e45bbd6b6e60e02920392364474d
SeniorZhai <developer.zhaitao@gmail.com>
2025-02-28 13:47:53 +0800

    Disable max handle

--- app/src/main/java/one/mixin/android/ui/home/web3/swap/SwapPage.kt
+++ app/src/main/java/one/mixin/android/ui/home/web3/swap/SwapPage.kt
@@ -115,7 +115,6 @@ fun SwapPage(
     var quoteResult by remember { mutableStateOf<QuoteResult?>(null) }
     var errorInfo by remember { mutableStateOf<String?>(null) }
     var quoteMin by remember { mutableStateOf<String?>(null) }
-    var quoteMax by remember { mutableStateOf<String?>(null) }
 
     var inputText by remember { mutableStateOf(initialAmount ?: "") }
     LaunchedEffect(lastOrderTime) {
@@ -150,7 +149,6 @@ fun SwapPage(
                             isLoading = true
                             errorInfo = null
                             quoteMin = null
-                            quoteMax = null
                             val amount = if (source == "") from.toLongAmount(text).toString() else text
                             viewModel.quote(context, from.symbol, from.getUnique(), to.getUnique(), amount, slippageBps.toString(), source)
                                 .onSuccess { value ->
@@ -163,7 +161,6 @@ fun SwapPage(
                                     if (exception is CancellationException) return@onFailure
                                     if (exception is AmountException) {
                                         quoteMin = exception.min
-                                        quoteMax = exception.max
                                     }
                                     errorInfo = exception.message
                                     quoteResult = null
@@ -173,7 +170,6 @@ fun SwapPage(
                             errorInfo = null
                             quoteResult = null
                             quoteMin = null
-                            quoteMax = null
                             isLoading = false
                         }
                     }
@@ -334,12 +330,8 @@ fun SwapPage(
                                             text = errorInfo ?: "",
                                             modifier = Modifier
                                                 .clickable {
-                                                    if (quoteMax != null || quoteMin != null) {
-                                                        if (quoteMax != null && runCatching { BigDecimal(inputText) }.getOrDefault(BigDecimal.ZERO) > runCatching { BigDecimal(quoteMax!!) }.getOrDefault(BigDecimal.ZERO)) {
-                                                            inputText = quoteMax!!
-                                                        } else if (quoteMin != null) {
-                                                            inputText = quoteMin!!
-                                                        }
+                                                    if (quoteMin != null && runCatching { BigDecimal(quoteMin) }.getOrDefault(BigDecimal.ZERO) > BigDecimal.ZERO) {
+                                                        inputText = quoteMin!!
                                                     }
                                             },
                                             style = TextStyle(
