46b335547bb76ee65cb1cbd81d1839867f0eb9aa
SeniorZhai <developer.zhaitao@gmail.com>
2025-03-18 18:15:18 +0800

    Hide progress

--- app/src/main/java/one/mixin/android/ui/home/web3/BrowserPage.kt
+++ app/src/main/java/one/mixin/android/ui/home/web3/BrowserPage.kt
@@ -26,6 +26,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.hapticfeedback.HapticFeedbackType
@@ -283,6 +284,7 @@ fun BrowserPage(
                             modifier =
                             Modifier
                                 .size(40.dp)
+                                .alpha(if (step == WalletConnectBottomSheetDialogFragment.Step.Sending) 0f else 1f)
                                 .align(Alignment.CenterHorizontally),
                             color = MixinAppTheme.colors.accent,
                         )
