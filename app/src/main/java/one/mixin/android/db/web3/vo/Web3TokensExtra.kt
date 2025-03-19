e9d61179202508902a2bde32b00f6907ff5b2727
SeniorZhai <developer.zhaitao@gmail.com>
2025-03-19 15:32:06 +0800

    Update scheme

--- app/src/main/java/one/mixin/android/db/web3/vo/Web3TokensExtra.kt
+++ app/src/main/java/one/mixin/android/db/web3/vo/Web3TokensExtra.kt
@@ -5,14 +5,15 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(
-    tableName = "tokens_extra", primaryKeys = ["asset_id", "wallet_id"]
+    tableName = "tokens_extra", primaryKeys = ["wallet_id", "asset_id"]
 )
 data class Web3TokensExtra(
-    @ColumnInfo(name = "asset_id")
-    val assetId: String,
 
     @ColumnInfo(name = "wallet_id")
     val walletId: String,
+
+    @ColumnInfo(name = "asset_id")
+    val assetId: String,
 
     @ColumnInfo(name = "hidden")
     val hidden: Boolean? = null,
