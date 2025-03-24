e90bb808eefcf6c1f1bdc0e731573cf6c6316f9d
SeniorZhai <developer.zhaitao@gmail.com>
2025-03-24 21:50:37 +0800

    Distinct and display

--- app/src/main/java/one/mixin/android/ui/address/AddressViewModel.kt
+++ app/src/main/java/one/mixin/android/ui/address/AddressViewModel.kt
@@ -4,7 +4,6 @@ import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.map
 import one.mixin.android.job.MixinJobManager
-import one.mixin.android.job.RefreshAddressJob
 import one.mixin.android.repository.TokenRepository
 import javax.inject.Inject
 
@@ -16,6 +15,8 @@ class AddressViewModel
         private val jobManager: MixinJobManager,
     ) : ViewModel() {
         fun addressesFlow(chainId: String) = tokenRepository.addressesFlow(chainId).map {
-            list -> list.distinctBy { it.destination }
+            list -> list.distinctBy { address ->
+                Pair(address.destination, address.tag)
+            }
         }
     }
