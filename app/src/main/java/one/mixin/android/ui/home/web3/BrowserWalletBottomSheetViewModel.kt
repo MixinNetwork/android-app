d56fa530f0cb999b2cb681f2e7fc4367fe89ccc0
SeniorZhai <developer.zhaitao@gmail.com>
2025-04-18 16:41:27 +0800

    Feature/commen (#5471)
    * Update market layout
    
    * Fix web3 local search
    
    * Update search
    
    * Update evm nonce
    
    * Update
    
    * Update
    
    ---------
    
    Co-authored-by: Crossle Song <crosslesong@gmail.com>

--- app/src/main/java/one/mixin/android/ui/home/web3/BrowserWalletBottomSheetViewModel.kt
+++ app/src/main/java/one/mixin/android/ui/home/web3/BrowserWalletBottomSheetViewModel.kt
@@ -8,7 +8,6 @@ import one.mixin.android.Constants.RouteConfig.ROUTE_BOT_USER_ID
 import one.mixin.android.api.handleMixinResponse
 import one.mixin.android.api.request.web3.EstimateFeeRequest
 import one.mixin.android.api.request.web3.EstimateFeeResponse
-import one.mixin.android.api.request.web3.ParseTxRequest
 import one.mixin.android.api.request.web3.Web3RawTransactionRequest
 import one.mixin.android.api.response.web3.ParsedTx
 import one.mixin.android.api.response.web3.SwapToken
@@ -70,7 +69,7 @@ class BrowserWalletBottomSheetViewModel
             var meet401 = false
             var parsedTx: ParsedTx? = null
             handleMixinResponse(
-                invokeNetwork = { assetRepo.simulateWeb3Tx(ParseTxRequest(tx, chainId, from)) },
+                invokeNetwork = { assetRepo.simulateWeb3Tx(Web3RawTransactionRequest(chainId, tx,from)) },
                 successBlock = { parsedTx = it.data },
                 failureBlock = {
                     if (it.errorCode == ErrorHandler.SIMULATE_TRANSACTION_FAILED) {
