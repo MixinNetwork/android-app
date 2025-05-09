fd65fd8d7bcb1faf179e30aa710d40a6c7fa674f
SeniorZhai <developer.zhaitao@gmail.com>
2025-05-09 13:34:39 +0800

    Refresh data

--- app/src/main/java/one/mixin/android/ui/home/web3/market/MarketDiffCallback.kt
+++ app/src/main/java/one/mixin/android/ui/home/web3/market/MarketDiffCallback.kt
@@ -5,9 +5,7 @@ import one.mixin.android.vo.market.MarketItem
 
 class MarketDiffCallback : DiffUtil.ItemCallback<MarketItem>() {
     override fun areItemsTheSame(oldItem: MarketItem, newItem: MarketItem): Boolean {
-        return oldItem.coinId == newItem.coinId && oldItem.priceChangePercentage7D == newItem.priceChangePercentage7D
-            && oldItem.currentPrice == newItem.currentPrice
-            && oldItem.priceChangePercentage24H == newItem.priceChangePercentage24H
+        return oldItem.coinId == newItem.coinId
     }
 
     override fun areContentsTheSame(oldItem: MarketItem, newItem: MarketItem): Boolean {
