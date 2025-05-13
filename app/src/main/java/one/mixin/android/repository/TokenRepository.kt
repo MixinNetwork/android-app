6b2e3dcdf6135116d2b146d0244d4e5ac4ccfbe0
SeniorZhai <developer.zhaitao@gmail.com>
2025-05-13 10:26:48 +0800

    Update transaction configured

--- app/src/main/java/one/mixin/android/repository/TokenRepository.kt
+++ app/src/main/java/one/mixin/android/repository/TokenRepository.kt
@@ -1026,25 +1026,22 @@ class TokenRepository
                 raw.simulateTx?.balanceChanges?.forEach { bc ->
                     val amt = bc.amount.toBigDecimalOrNull()
                     if (amt != null) {
-                        if (amt > BigDecimal.ZERO) {
-                            receivers.add(
-                                AssetChange(
-                                    assetId = bc.assetId,
-                                    amount = amt.toPlainString(),
-                                    from = bc.from,
-                                    to = bc.to
-                                )
+                        receivers.add(
+                            AssetChange(
+                                assetId = bc.assetId,
+                                amount = amt.abs().toPlainString(),
+                                from = bc.from,
+                                to = bc.to
                             )
-                        } else if (amt < BigDecimal.ZERO) {
-                            senders.add(
-                                AssetChange(
-                                    assetId = bc.assetId,
-                                    amount = amt.toPlainString(),
-                                    from = bc.from,
-                                    to = bc.to,
-                                )
+                        )
+                        senders.add(
+                            AssetChange(
+                                assetId = bc.assetId,
+                                amount = amt.toPlainString(),
+                                from = bc.from,
+                                to = bc.to,
                             )
-                        }
+                        )
                     }
                 }
 
