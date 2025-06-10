1ab657864f4c2f0ababeedab2573102060711dab
SeniorZhai <developer.zhaitao@gmail.com>
2025-06-10 13:43:11 +0800

    Refresh pending order (#5580)
    * Refresh pending order
    
    * fix typo
    
    ---------
    
    Co-authored-by: Crossle Song <crosslesong@gmail.com>

--- app/src/main/java/one/mixin/android/db/OrderDao.kt
+++ app/src/main/java/one/mixin/android/db/OrderDao.kt
@@ -34,7 +34,7 @@ interface OrderDao : BaseDao<SwapOrder> {
     )
     fun getOrderById(orderId: String): Flow<SwapOrderItem?>
 
-    @Query("SELECT * FROM swap_orders WHERE state = 'pending'")
+    @Query("SELECT * FROM swap_orders WHERE state = 'pending' ORDER BY created_at ASC")
     suspend fun getPendingOrders(): List<SwapOrder>
 
     @Query(
