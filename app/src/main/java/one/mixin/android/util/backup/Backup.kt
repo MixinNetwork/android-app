e74d452b980fd16e12dec7830963fef372d415d4
SeniorZhai <developer.zhaitao@gmail.com>
2025-06-23 12:26:24 +0800

    Lost delete code

--- app/src/main/java/one/mixin/android/util/backup/Backup.kt
+++ app/src/main/java/one/mixin/android/util/backup/Backup.kt
@@ -100,6 +100,8 @@ suspend fun backup(
         db.execSQL("DELETE FROM jobs")
         db.execSQL("DELETE FROM flood_messages")
         db.execSQL("DELETE FROM offsets")
+        db.execSQL("DELETE FROM outputs")
+        db.execSQL("DELETE FROM tokens_extra")
     } catch (ignored: Exception) {
         Timber.e(ignored)
     } finally {
