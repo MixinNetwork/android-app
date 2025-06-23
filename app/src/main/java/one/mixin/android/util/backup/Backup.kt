861e866ae8f06d98ce6d5207ab2b33b62cd41550
SeniorZhai <developer.zhaitao@gmail.com>
2025-06-23 12:23:32 +0800

    Delete tokens extra

--- app/src/main/java/one/mixin/android/util/backup/Backup.kt
+++ app/src/main/java/one/mixin/android/util/backup/Backup.kt
@@ -229,6 +229,7 @@ suspend fun backupApi29(
                 db.execSQL("DELETE FROM flood_messages")
                 db.execSQL("DELETE FROM offsets")
                 db.execSQL("DELETE FROM outputs")
+                db.execSQL("DELETE FROM tokens_extra")
             } catch (ignored: Exception) {
             } finally {
                 db.close()
