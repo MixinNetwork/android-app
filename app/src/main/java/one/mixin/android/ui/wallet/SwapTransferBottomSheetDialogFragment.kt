cd72fdaae0b651355c6863185a002a8bc2463e53
SeniorZhai <developer.zhaitao@gmail.com>
2025-07-18 15:21:44 +0800

    Fix web3 chain check

--- app/src/main/java/one/mixin/android/ui/wallet/SwapTransferBottomSheetDialogFragment.kt
+++ app/src/main/java/one/mixin/android/ui/wallet/SwapTransferBottomSheetDialogFragment.kt
@@ -802,8 +802,7 @@ class SwapTransferBottomSheetDialogFragment : BottomSheetDialogFragment() {
                         }
                         buildTipGas(chain.chainId, r.data!!)
                     } ?: return@onEach
-                    // todo
-                    chainToken = bottomViewModel.web3TokenItemById("",token?.chainId ?: "")
+                    chainToken = bottomViewModel.web3TokenItemById(JsSigner.currentWalletId,token?.chainId ?: "")
                     insufficientGas = checkGas(token, chainToken, tipGas, transaction.value, transaction.maxFeePerGas)
                     if (insufficientGas) {
                         handleException(IllegalArgumentException(requireContext().getString(R.string.insufficient_gas, chainToken?.symbol ?: chain.symbol)))
