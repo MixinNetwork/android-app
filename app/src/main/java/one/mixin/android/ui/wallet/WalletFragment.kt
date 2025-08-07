bae35c4953999650773649508baba5620356945d
SeniorZhai <developer.zhaitao@gmail.com>
2025-08-07 16:52:23 +0800

    Remove unused code

--- app/src/main/java/one/mixin/android/ui/wallet/WalletFragment.kt
+++ app/src/main/java/one/mixin/android/ui/wallet/WalletFragment.kt
@@ -112,44 +112,7 @@ class WalletFragment : BaseFragment(R.layout.fragment_wallet) {
         } ?: WalletDestination.Privacy
 
         selectedWalletDestination = initialWalletDestination
-        lifecycleScope.launch {
-            val finalWalletDestination = when (val dest = initialWalletDestination) {
-                is WalletDestination.Classic -> {
-                    val wallet = walletViewModel.findWalletById(dest.walletId)
-                    if (wallet != null) dest else WalletDestination.Privacy
-                }
-                is WalletDestination.Import -> {
-                    val wallet = walletViewModel.findWalletById(dest.walletId)
-                    if (wallet != null) {
-                        if (dest.category != wallet.category) {
-                            WalletDestination.Import(dest.walletId, wallet.category)
-                        } else {
-                            dest
-                        }
-                    } else {
-                        WalletDestination.Privacy
-                    }
-                }
-                is WalletDestination.Watch -> {
-                    val wallet = walletViewModel.findWalletById(dest.walletId)
-                    if (wallet != null) {
-                        if (dest.category != wallet.category) {
-                            WalletDestination.Watch(dest.walletId, wallet.category)
-                        } else {
-                            dest
-                        }
-                    } else {
-                        WalletDestination.Privacy
-                    }
-                }
-                is WalletDestination.Privacy -> dest
-            }
-
-            Timber.e("Final wallet destination: $finalWalletDestination, $initialWalletDestination")
-            if (initialWalletDestination != finalWalletDestination) {
-                selectedWalletDestination = finalWalletDestination
-            }
-        }
+        Timber.e("Loaded selected wallet destination: $selectedWalletDestination")
     }
 
     private fun saveSelectedWalletDestination(destination: WalletDestination) {
