2cd65b1bbf8d6ffdc4faa74bf77c66ad0b022f18
SeniorZhai <developer.zhaitao@gmail.com>
2025-09-02 10:30:40 +0800

    Update

--- app/src/main/java/one/mixin/android/ui/common/ReceiveQrActivity.kt
+++ app/src/main/java/one/mixin/android/ui/common/ReceiveQrActivity.kt
@@ -1,12 +1,16 @@
 package one.mixin.android.ui.common
 
+import android.Manifest
 import android.content.ClipData
+import android.content.Intent
 import android.graphics.Bitmap
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.core.view.drawToBitmap
+import androidx.fragment.app.DialogFragment
 import androidx.lifecycle.lifecycleScope
 import com.uber.autodispose.android.lifecycle.autoDispose
+import com.uber.autodispose.autoDispose
 import dagger.hilt.android.AndroidEntryPoint
 import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
@@ -19,12 +23,21 @@ import one.mixin.android.databinding.ActivityReceiveQrBinding
 import one.mixin.android.extension.generateQRCode
 import one.mixin.android.extension.getClipboardManager
 import one.mixin.android.extension.heavyClickVibrate
+import one.mixin.android.extension.isExternalTransferUrl
+import one.mixin.android.extension.isLightningUrl
+import one.mixin.android.extension.openPermissionSetting
 import one.mixin.android.extension.toast
 import one.mixin.android.session.Session
+import one.mixin.android.ui.conversation.link.LinkBottomSheetDialogFragment
+import one.mixin.android.ui.home.MainActivity
+import one.mixin.android.ui.home.MainActivity.Companion.SCAN
+import one.mixin.android.ui.qr.CaptureActivity
+import one.mixin.android.ui.qr.CaptureActivity.Companion.ARGS_SHOW_SCAN
 import one.mixin.android.ui.wallet.AssetListBottomSheetDialogFragment
 import one.mixin.android.ui.wallet.AssetListBottomSheetDialogFragment.Companion.TYPE_FROM_RECEIVE
 import one.mixin.android.ui.wallet.BackupMnemonicPhraseWarningBottomSheetDialogFragment
 import one.mixin.android.ui.wallet.DepositShareActivity
+import one.mixin.android.util.rxpermission.RxPermissions
 import one.mixin.android.widget.BadgeCircleImageView
 import timber.log.Timber
 
@@ -46,10 +59,51 @@ class ReceiveQrActivity : BaseActivity() {
 
     private val userId: String by lazy { intent.getStringExtra(ARGS_USER_ID)!! }
 
+    private val getScanResult =
+        registerForActivityResult(CaptureActivity.CaptureContract()) { data ->
+            if (data != null) {
+                handlerCode(data)
+            }
+        }
+
+    fun showCapture(scan: Boolean) {
+        getScanResult.launch(Pair(ARGS_SHOW_SCAN, scan))
+    }
+
+    private var bottomSheet: DialogFragment? = null
+    private fun handlerCode(intent: Intent) {
+        if (intent.hasExtra(SCAN)) {
+            val scan = intent.getStringExtra(SCAN)!!
+            bottomSheet?.dismiss()
+            showScanBottom(scan)
+            clearCodeAfterConsume(intent, SCAN)
+        }
+    }
+
+    private fun showScanBottom(scan: String) {
+        if (scan.isLightningUrl() || scan.isExternalTransferUrl()) {
+            LinkBottomSheetDialogFragment.newInstance(scan).show(
+                supportFragmentManager,
+                LinkBottomSheetDialogFragment.TAG
+            )
+        } else {
+            bottomSheet = QrScanBottomSheetDialogFragment.newInstance(scan)
+            bottomSheet?.showNow(supportFragmentManager, QrScanBottomSheetDialogFragment.TAG)
+        }
+    }
+
+    private fun clearCodeAfterConsume(
+        intent: Intent,
+        code: String,
+    ) {
+        intent.removeExtra(code)
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-
+        binding.titleView.setSubTitle(getString(R.string.Receive_Money), getString(R.string.Privacy_Wallet), R.drawable.ic_wallet_privacy)
         binding.titleView.leftIb.setOnClickListener {
             finish()
         }
@@ -61,7 +115,14 @@ class ReceiveQrActivity : BaseActivity() {
             } else {
                 binding.apply {
                     scan.setOnClickListener {
-
+                        RxPermissions(this@ReceiveQrActivity).request(Manifest.permission.CAMERA)
+                            .autoDispose(stopScope).subscribe { granted ->
+                                if (granted) {
+                                    showCapture(true)
+                                } else {
+                                    this@ReceiveQrActivity.openPermissionSetting()
+                                }
+                            }
                     }
                     amount.setOnClickListener {
                         if (!Session.saltExported() && Session.isAnonymous()) {
@@ -132,7 +193,6 @@ class ReceiveQrActivity : BaseActivity() {
                         }
                     }
                     this.onShareClick = { amount, address ->
-                        Timber.e("$amount $address")
                         this@ReceiveQrActivity.lifecycleScope.launch {
                             val shareView = binding.container
                             val bitmap = shareView.drawToBitmap()
