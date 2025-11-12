894ecb93dc9f73324481b869b3deedfe0142f959
SeniorZhai <developer.zhaitao@gmail.com>
2025-11-12 11:13:18 +0800

    Remove background

--- app/src/main/java/one/mixin/android/ui/call/CallBottomSheetDialogFragment.kt
+++ app/src/main/java/one/mixin/android/ui/call/CallBottomSheetDialogFragment.kt
@@ -14,6 +14,7 @@ import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
 import androidx.appcompat.app.AlertDialog
 import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.core.view.doOnPreDraw
@@ -125,6 +126,7 @@ class CallBottomSheetDialogFragment : BottomSheetDialogFragment() {
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         return MixinBottomSheetDialog(requireContext(), theme).apply {
             dismissWithAnimation = true
+            window?.clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND)
         }
     }
 
