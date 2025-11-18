aa7aa9fb63025360191cc64dd4fef0d7c01c7d22
weifangc <weifangcheng@outlook.jp>
2025-11-18 16:11:26 +0800

    chore: remove repetitive word in comment
    Signed-off-by: weifangc <weifangcheng@outlook.jp>

--- app/src/main/java/androidx/compose/ui/platform/LocalSoftwareKeyboardController.kt
+++ app/src/main/java/androidx/compose/ui/platform/LocalSoftwareKeyboardController.kt
@@ -4,7 +4,7 @@ import androidx.compose.runtime.Composable
 
 // Used by Stripe Android SDK
 // Work around for https://github.com/stripe/stripe-android/issues/7184
-// This existed as Experimental in Compose 1.5, but was moved to a val in in Compose 1.6
+// This existed as Experimental in Compose 1.5, but was moved to a val in Compose 1.6
 @Suppress("unused")
 object LocalSoftwareKeyboardController {
     val current
