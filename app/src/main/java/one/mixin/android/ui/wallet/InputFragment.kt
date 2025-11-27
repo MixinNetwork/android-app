11d54787300d46bbe6460ba8476e0b8a14b947cf
SeniorZhai <developer.zhaitao@gmail.com>
2025-11-27 15:21:39 +0800

    Prevent gas fee from being refreshed once calculated

--- app/src/main/java/one/mixin/android/ui/wallet/InputFragment.kt
+++ app/src/main/java/one/mixin/android/ui/wallet/InputFragment.kt
@@ -43,6 +43,7 @@ import one.mixin.android.extension.viewDestroyed
 import one.mixin.android.job.MixinJobManager
 import one.mixin.android.job.SyncOutputJob
 import one.mixin.android.session.Session
+import one.mixin.android.tip.wc.internal.TipGas
 import one.mixin.android.ui.address.ReceiveSelectionBottom.OnReceiveSelectionClicker
 import one.mixin.android.ui.address.TransferDestinationInputFragment
 import one.mixin.android.ui.common.BaseFragment
@@ -521,6 +522,7 @@ class InputFragment : BaseFragment(R.layout.fragment_input), OnReceiveSelectionC
                                     toUser = user,
                                     chainToken = chainToken,
                                     isFeeWaived = isFeeWaived,
+                                    tipGas = tipGas,
                                     onTxhash = { _, serializedTx ->
                                     },
                                     onDismiss = { isDone->
@@ -1034,6 +1036,7 @@ class InputFragment : BaseFragment(R.layout.fragment_input), OnReceiveSelectionC
     }
 
     private var gas: BigDecimal? = null
+    private var tipGas: TipGas? = null
 
     private suspend fun refreshFee(t: TokenItem) {
         val toAddress = toAddress?: return
@@ -1220,7 +1223,9 @@ class InputFragment : BaseFragment(R.layout.fragment_input), OnReceiveSelectionC
           delay(3000)
           refreshGas(t)
         } else if (isAdded) {
-            gas = web3ViewModel.calcFee(t, transaction, fromAddress)
+            val feeResult = web3ViewModel.calcFeeWithTipGas(t, transaction, fromAddress)
+            gas = feeResult.first
+            tipGas = feeResult.second
             if (chainToken?.assetId == t.assetId) {
                 val balance = runCatching {
                     tokenBalance.toBigDecimalOrNull()?.subtract(gas ?: BigDecimal.ZERO)
