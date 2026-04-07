013fb1a66a63e9f725de4c1160835961a117b4b1
SeniorZhai <developer.zhaitao@gmail.com>
2026-04-07 16:33:48 +0800

    Remove unused layout import from open position item

--- app/src/main/java/one/mixin/android/ui/home/web3/trade/perps/OpenPositionItem.kt
+++ app/src/main/java/one/mixin/android/ui/home/web3/trade/perps/OpenPositionItem.kt
@@ -2,7 +2,6 @@ package one.mixin.android.ui.home.web3.trade.perps
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
