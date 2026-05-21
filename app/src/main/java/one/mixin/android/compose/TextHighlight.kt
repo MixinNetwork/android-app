a795a6764708d775300384e16e1b16b8e89d9b16
SeniorZhai <developer.zhaitao@gmail.com>
2026-05-21 11:40:06 +0800

    fix(compose): add missing Column import in TextHighlight preview

--- app/src/main/java/one/mixin/android/compose/TextHighlight.kt
+++ app/src/main/java/one/mixin/android/compose/TextHighlight.kt
@@ -1,5 +1,6 @@
 package one.mixin.android.compose
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
