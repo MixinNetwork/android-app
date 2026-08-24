2faec2ae3548391b69c56ff11b369ae364f615e7
SeniorZhai <developer.zhaitao@gmail.com>
2026-08-24 15:24:39 +0800

    fix(market): align stock sub tabs

--- app/src/main/java/one/mixin/android/ui/home/web3/market/MarketPage.kt
+++ app/src/main/java/one/mixin/android/ui/home/web3/market/MarketPage.kt
@@ -219,7 +219,7 @@ private fun SubTabs(
     LazyRow(
         state = listState,
         modifier = Modifier.fillMaxWidth(),
-        contentPadding = PaddingValues(start = 6.dp, end = 20.dp),
+        contentPadding = PaddingValues(start = if (topTab == MarketTopTab.STOCK) 12.dp else 6.dp, end = 20.dp),
         horizontalArrangement = Arrangement.spacedBy(10.dp),
         verticalAlignment = Alignment.CenterVertically,
     ) {
