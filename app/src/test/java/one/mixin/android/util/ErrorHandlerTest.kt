f397976bb71625e2ed9c30a01fd354627223e3d9
SeniorZhai <developer.zhaitao@gmail.com>
2026-09-21 09:19:12 +0800

    fix(perps): sync minimum margin and position errors

--- app/src/test/java/one/mixin/android/util/ErrorHandlerTest.kt
+++ app/src/test/java/one/mixin/android/util/ErrorHandlerTest.kt
@@ -20,21 +20,25 @@ import kotlin.test.assertNull
 @Config(application = Application::class, manifest = Config.NONE, sdk = [28])
 class ErrorHandlerTest {
     @Test
-    fun mapsPerpsOrderValueBelowMinimumErrors() {
-        assertEquals(R.string.error_perps_order_value_too_small, perpsOrderValueErrorResource(10650))
-        assertEquals(R.string.error_perps_order_value_too_small, perpsOrderValueErrorResource(10654))
-        assertNull(perpsOrderValueErrorResource(10653))
+    fun distinguishesPerpsMarginAndPositionMinimumErrors() {
+        assertEquals(R.string.error_perps_margin_too_small, perpsMinimumValueErrorResource(10650))
+        assertEquals(R.string.error_perps_margin_too_small_value, perpsMinimumValueErrorResource(10650, hasMinimum = true))
+        assertEquals(R.string.error_perps_position_size_too_small, perpsMinimumValueErrorResource(10654))
+        assertEquals(R.string.error_perps_position_size_too_small_value, perpsMinimumValueErrorResource(10654, hasMinimum = true))
+        assertNull(perpsMinimumValueErrorResource(10653))
     }
 
     @Test
-    fun showMinimumOrderValueWhenProvided() {
+    fun showsMinimumValueWithCurrencyOrFallsBackToTheMatchingError() {
         val application = ApplicationProvider.getApplicationContext<Context>()
         val resources =
             object : Resources(application.assets, application.resources.displayMetrics, application.resources.configuration) {
                 override fun getString(id: Int): String =
                     when (id) {
-                        R.string.error_perps_order_value_too_small -> "ERROR %1\$d: Position size is too small."
-                        R.string.error_perps_order_value_minimum -> "ERROR %1\$d: Order value must be at least %2\$s USD."
+                        R.string.error_perps_margin_too_small -> "ERROR 10650: Margin is too small"
+                        R.string.error_perps_margin_too_small_value -> "ERROR 10650: Margin must have minimum value of %1\$s"
+                        R.string.error_perps_position_size_too_small -> "ERROR 10654: Position size is too small"
+                        R.string.error_perps_position_size_too_small_value -> "ERROR 10654: Position must have minimum size of %1\$s"
                         else -> error("Unexpected string resource: $id")
                     }
 
@@ -52,12 +56,15 @@ class ErrorHandlerTest {
             for ((extra, minimum) in listOf(
                 """{"field":"amount","min_order_value":"10","reason":"order value must be at least 10"}""" to "10",
                 """{"min_order_value":"0.20"}""" to "0.2",
+                """{"min_order_value":"0.00000001"}""" to "0.00000001",
                 """{"min_order_value":10}""" to "10",
                 """{"min_order_value":"0"}""" to null,
                 """{"min_order_value":"-10"}""" to null,
                 """{"min_order_value":"invalid"}""" to null,
                 """{"min_order_value":{}}""" to null,
                 """{"min_order_value":null}""" to null,
+                """{"min_order_value":true}""" to null,
+                """{"min_order_value":"1e10"}""" to null,
                 "{}" to null,
                 "null" to null,
             )) {
@@ -67,12 +74,17 @@ class ErrorHandlerTest {
                         ResponseError::class.java,
                     )
                 val response = MixinResponse<Any>(error)
-                val expected =
-                    minimum?.let { "ERROR $code: Order value must be at least $it USD." }
-                        ?: "ERROR $code: Position size is too small."
+                val expected = if (code == 10650) {
+                    minimum?.let { "ERROR 10650: Margin must have minimum value of \$$it" }
+                        ?: "ERROR 10650: Margin is too small"
+                } else {
+                    minimum?.let { "ERROR 10654: Position must have minimum size of \$$it" }
+                        ?: "ERROR 10654: Position size is too small"
+                }
                 assertEquals(expected, context.getMixinErrorStringByCode(response.errorCode, response.errorDescription))
             }
         }
-        assertEquals("ERROR 10654: Position size is too small.", context.getMixinErrorStringByCode(10654, "Invalid extra\n{broken"))
+        assertEquals("ERROR 10650: Margin is too small", context.getMixinErrorStringByCode(10650, "Invalid extra\n{broken"))
+        assertEquals("ERROR 10654: Position size is too small", context.getMixinErrorStringByCode(10654, "Invalid extra\n{broken"))
     }
 }
